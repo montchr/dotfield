24f67cf3f42b9eb1ea796e2d91ca614ce7a74d69
Chris Montgomery <chris@alley.co>
2020-07-23 00:09:55 -0400

    Change some emacs packages around

--- init.el
+++ init.el
@@ -39,11 +39,11 @@
        minimap           ; show a map of the code on the side
        modeline          ; snazzy, Atom-inspired modeline, plus API
        nav-flash         ; blink cursor line after big motions
-       neotree           ; a project drawer, like NERDTree for vim
+       ;; neotree           ; a project drawer, like NERDTree for vim
        ophints           ; highlight the region an operation acts on
        (popup +defaults)   ; tame sudden yet inevitable temporary windows
-       pretty-code       ; ligatures or substitute text with pretty symbols
-       ;; tabs              ; an tab bar for Emacs
+       ;; pretty-code       ; ligatures or substitute text with pretty symbols
+       tabs              ; an tab bar for Emacs
        treemacs          ; a project drawer, like neotree but cooler
        unicode           ; extended unicode support for various languages
        vc-gutter         ; vcs diff in the fringe
