24bc73b795fb94ca631014c59a956cd56d6bf39d
Chris Montgomery <chris@alley.co>
2020-07-23 01:00:15 -0400

    Configure Centaur Tabs

--- config.el
+++ config.el
@@ -48,8 +48,8 @@
 ;; Simple settings.
 ;; https://tecosaur.github.io/emacs-config/config.html#simple-settings
 (setq undo-limit 80000000
-  evil-want-fine-undo nil
-  truncate-string-ellipsis "…")
+      evil-want-fine-undo nil
+      truncate-string-ellipsis "…")
 
 ;; Allow the default macOS ~alt~ behavior for special keyboard chars.
 (setq ns-right-alternate-modifier 'none)
@@ -90,18 +90,18 @@
     (org-map-entries 'org-archive-subtree "/DONE" 'file))
   (require 'find-lisp)
   (setq cdom/org-agenda-directory "~/org/gtd"
-    org-agenda-files (find-lisp-find-files cdom/org-agenda-directory "\.org$")
-    org-log-refile 'time))
+        org-agenda-files (find-lisp-find-files cdom/org-agenda-directory "\.org$")
+        org-log-refile 'time))
 
 (after! doct
   (setq org-capture-templates
-  (doct '(("Tasks"
-            :keys "t"
-            :file (concat cdom/org-agenda-directory "inbox.org")
-            :prepend t
-            :template "* %{todo-state} %^{Description}")))
+        (doct '(("Tasks"
+                 :keys "t"
+                 :file (concat cdom/org-agenda-directory "inbox.org")
+                 :prepend t
+                 :template "* %{todo-state} %^{Description}")))
+        )
   )
-)
 
 (setq deft-directory "~/org"
       deft-recursive t)
@@ -111,11 +111,11 @@
   (setq org-roam-directory "~/org")
   :config
   (setq org-roam-dailies-capture-templates
-    '(("d" "daily" plain #'org-roam-capture--get-point
-        ""
-        :immediate-finish t
-        :file-name "%<%Y-%m-%d>"
-        :head "#+title: %<%A, %d %B %Y>")))
+        '(("d" "daily" plain #'org-roam-capture--get-point
+           ""
+           :immediate-finish t
+           :file-name "%<%Y-%m-%d>"
+           :head "#+title: %<%A, %d %B %Y>")))
   (setq +org-roam-open-buffer-on-find-file nil))
 
 ;; Configure org-journal for compatability with org-roam-dailies
@@ -126,6 +126,64 @@
         org-journal-dir cdom/org-agenda-directory
         org-journal-date-format "%A, %d %B %Y"
         org-journal-enable-agenda-integration t))
+
+;; https://github.com/ema2159/centaur-tabs#my-personal-configuration
+(use-package! centaur-tabs
+  :config
+  (setq centaur-tabs-style "bar"
+	      centaur-tabs-height 32
+	      centaur-tabs-set-icons t
+	      centaur-tabs-set-modified-marker t
+	      centaur-tabs-show-navigation-buttons t
+	      centaur-tabs-set-bar 'under
+	      x-underline-at-descent-line t)
+  (centaur-tabs-headline-match)
+  ;; (setq centaur-tabs-gray-out-icons 'buffer)
+  ;; (centaur-tabs-enable-buffer-reordering)
+  ;; (setq centaur-tabs-adjust-buffer-order t)
+  (centaur-tabs-mode t)
+  (setq uniquify-separator "/")
+  (setq uniquify-buffer-name-style 'forward)
+  (defun centaur-tabs-buffer-groups ()
+    "`centaur-tabs-buffer-groups' control buffers' group rules.
+
+ Group centaur-tabs with mode if buffer is derived from `eshell-mode' `emacs-lisp-mode' `dired-mode' `org-mode' `magit-mode'.
+ All buffer name start with * will group to \"Emacs\".
+ Other buffer group by `centaur-tabs-get-group-name' with project name."
+    (list
+     (cond
+	    ;; ((not (eq (file-remote-p (buffer-file-name)) nil))
+	    ;; "Remote")
+	    ((or (string-equal "*" (substring (buffer-name) 0 1))
+	         (memq major-mode '(magit-process-mode
+				                      magit-status-mode
+				                      magit-diff-mode
+				                      magit-log-mode
+				                      magit-file-mode
+				                      magit-blob-mode
+				                      magit-blame-mode
+				                      )))
+	     "Emacs")
+	    ((derived-mode-p 'prog-mode)
+	     "Editing")
+	    ((derived-mode-p 'dired-mode)
+	     "Dired")
+	    ((memq major-mode '(helpful-mode
+			                    help-mode))
+	     "Help")
+	    ((memq major-mode '(org-mode
+			                    org-agenda-clockreport-mode
+			                    org-src-mode
+			                    org-agenda-mode
+			                    org-beamer-mode
+			                    org-indent-mode
+			                    org-bullets-mode
+			                    org-cdlatex-mode
+			                    org-agenda-log-mode
+			                    diary-mode))
+	     "OrgMode")
+	    (t
+	     (centaur-tabs-get-group-name (current-buffer)))))))
 
 
 ;; [BROKEN] Archive items to an archive sibling instead of a separate file
