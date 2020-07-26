76ac68f85d068cceb96c91aae54cf1aa5455ef70
Chris Montgomery <chrismontgomery@montalley.myfiosgateway.com>
2020-07-26 16:45:50 -0400

    indentation

--- config.el
+++ config.el
@@ -143,18 +143,18 @@
 (use-package! fish-mode
   :config
   (setq fish-indent-offset 2
-    fish-enable-auto-indent t))
+        fish-enable-auto-indent t))
 
 ;; https://github.com/ema2159/centaur-tabs#my-personal-configuration
 (use-package! centaur-tabs
   :config
   (setq centaur-tabs-style "bar"
-	      centaur-tabs-height 32
-	      centaur-tabs-set-icons t
-	      centaur-tabs-set-modified-marker t
-	      centaur-tabs-show-navigation-buttons t
-	      centaur-tabs-set-bar 'under
-	      x-underline-at-descent-line t)
+        centaur-tabs-height 32
+        centaur-tabs-set-icons t
+        centaur-tabs-set-modified-marker t
+        centaur-tabs-show-navigation-buttons t
+        centaur-tabs-set-bar 'under
+        x-underline-at-descent-line t)
   (centaur-tabs-headline-match)
   ;; (setq centaur-tabs-gray-out-icons 'buffer)
   ;; (centaur-tabs-enable-buffer-reordering)
@@ -170,39 +170,42 @@
  Other buffer group by `centaur-tabs-get-group-name' with project name."
     (list
      (cond
-	    ;; ((not (eq (file-remote-p (buffer-file-name)) nil))
-	    ;; "Remote")
-	    ((or (string-equal "*" (substring (buffer-name) 0 1))
-	         (memq major-mode '(magit-process-mode
-				                      magit-status-mode
-				                      magit-diff-mode
-				                      magit-log-mode
-				                      magit-file-mode
-				                      magit-blob-mode
-				                      magit-blame-mode
-				                      )))
-	     "Emacs")
-	    ((derived-mode-p 'prog-mode)
-	     "Editing")
-	    ((derived-mode-p 'dired-mode)
-	     "Dired")
-	    ((memq major-mode '(helpful-mode
-			                    help-mode))
-	     "Help")
-	    ((memq major-mode '(org-mode
-			                    org-agenda-clockreport-mode
-			                    org-src-mode
-			                    org-agenda-mode
-			                    org-beamer-mode
-			                    org-indent-mode
-			                    org-bullets-mode
-			                    org-cdlatex-mode
-			                    org-agenda-log-mode
-			                    diary-mode))
-	     "OrgMode")
-	    (t
-	     (centaur-tabs-get-group-name (current-buffer)))))))
+      ;; ((not (eq (file-remote-p (buffer-file-name)) nil))
+      ;; "Remote")
+      ((or (string-equal "*" (substring (buffer-name) 0 1))
+           (memq major-mode '(magit-process-mode
+                              magit-status-mode
+                              magit-diff-mode
+                              magit-log-mode
+                              magit-file-mode
+                              magit-blob-mode
+                              magit-blame-mode
+                              )))
+       "Emacs")
+      ((derived-mode-p 'prog-mode)
+       "Editing")
+      ((derived-mode-p 'dired-mode)
+       "Dired")
+      ((memq major-mode '(helpful-mode
+                          help-mode))
+       "Help")
+      ((memq major-mode '(org-mode
+                          org-agenda-clockreport-mode
+                          org-src-mode
+                          org-agenda-mode
+                          org-beamer-mode
+                          org-indent-mode
+                          org-bullets-mode
+                          org-cdlatex-mode
+                          org-agenda-log-mode
+                          diary-mode))
+       "OrgMode")
+      (t
+       (centaur-tabs-get-group-name (current-buffer)))))))
 
 
 ;; [BROKEN] Archive items to an archive sibling instead of a separate file
 (setq org-archive-default-command 'org-archive-to-archive-sibling)
+
+(setq +doom-quit-messages ("(setq nothing t everything 'permitted)"
+                           "Hey! Hey, M-x listen!"))
