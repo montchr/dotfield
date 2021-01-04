251a98e44188dc98fffb6f4d1954c4a36c1bafcf
Chris Montgomery <chris@montchr.io>
2021-01-04 12:30:25 -0500

    (zsh): disable broken git aliases

--- zsh/.zinit/plugins/_local---config-files/config-files.plugin.zsh
+++ zsh/.zinit/plugins/_local---config-files/config-files.plugin.zsh
@@ -143,54 +143,55 @@ alias reload="exec $SHELL -l"
 # Git
 #
 
-alias gs='scmpuff_status'
+# @TODO: many of these need to be functions
+# alias gs='scmpuff_status'
 
 # commits / index
-alias ga='git add'
-alias gaa="git add --all"
-alias gap="git add -p"
-alias gdt="git difftool"
-alias gc="git commit"
-alias gca="git commit --amend"
-# amend commit without editing message
-alias gcam="git commit --amend -C HEAD"
-alias gd='git diff'
-alias grs='git reset'
+# alias ga="git add"
+# alias gaa="git add --all"
+# alias gap="git add -p"
+# alias gdt="git difftool"
+# alias gc="git commit"
+# alias gca="git commit --amend"
+# # amend commit without editing message
+# alias gcam="git commit --amend -C HEAD"
+# alias gd='git diff'
+# alias grs='git reset'
 
 # branches / refs
-alias gsw='git switch'
-alias gco='git checkout'
-alias gcob="git checkout -b"
-alias gb="git branch"
-alias gm="git merge --no-edit"
-alias gmt="git mergetool"
-alias gcp="git cherry-pick"
+# alias gsw='git switch'
+# alias gco='git checkout'
+# alias gcob="git checkout -b"
+# alias gb="git branch"
+# alias gm="git merge --no-edit"
+# alias gmt="git mergetool"
+# alias gcp="git cherry-pick"
 
 # remotes
-alias gpl="git pull"
-alias gplo='git pull origin'
-alias gpls="git pull && gsumo"
-alias gps="git push"
-alias gpsu="git push -u"
-alias gpso="git push origin"
-alias gpss="git push && gsumo"
+# alias gpl="git pull"
+# alias gplo='git pull origin'
+# alias gpls="git pull && gsumo"
+# alias gps="git push"
+# alias gpsu="git push -u"
+# alias gpso="git push origin"
+# alias gpss="git push && gsumo"
 
 # stashes
-alias gsh="git stash"
-alias gsha="git stash apply"
-alias gshp="git stash pop"
-alias gshl="git stash list"
+# alias gsh="git stash"
+# alias gsha="git stash apply"
+# alias gshp="git stash pop"
+# alias gshl="git stash list"
 
 # Show list of files changed in a commit
 # Follow with commit hash
-alias gdl="git diff-tree --no-commit-id --name-only -r $1"
+# alias gdl="git diff-tree --no-commit-id --name-only -r $1"
 
 # logs
-alias gl="git log --oneline --decorate -20"
+# alias gl="git log --oneline --decorate -20"
 # List all the commits on the current branch ahead of master
-alias glb="git log --oneline --decorate \$GIT_PRIMARY_BRANCH.."
+# alias glb="git log --oneline --decorate \$GIT_PRIMARY_BRANCH.."
 # Based on https://stackoverflow.com/questions/1057564/pretty-git-branch-graphs
-alias gla="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(green)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(bold cyan)%s%C(reset) %C(dim white)- %an%C(reset)%n''' --all"
+# alias gla="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(green)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(bold cyan)%s%C(reset) %C(dim white)- %an%C(reset)%n''' --all"
 
 alias gsumo="git submodule update --init --recursive"
 
