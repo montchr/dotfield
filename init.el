eac3c08a1da7f983096d01dc7c0b0b0b9cb2e711
Chris Montgomery <chris@cdom.io>
2021-01-13 18:47:30 -0500

    tweak

--- init.el
+++ init.el
@@ -3,19 +3,8 @@
 ;; This file controls what Doom modules are enabled and what order they load
 ;; in. Remember to run 'doom sync' after modifying it!
 
-;; NOTE Press 'SPC h d h' (or 'C-h d h' for non-vim users) to access Doom's
-;;      documentation. There you'll find information about all of Doom's
-;;      modules and what flags they support.
-
-;; NOTE Move your cursor over a module's name (or its flags) and press 'K' (or
-;;      'C-c g k' for non-vim users) to view its documentation. This works on
-;;      flags as well (those symbols that start with a plus).
-;;
-;;      Alternatively, press 'gd' (or 'C-c g d') on a module to browse its
-;;      directory (for easy access to its source code).
-
 ;; Respect visual line mode
-(setq evil-respect-visual-line-mode t)
+(setq! evil-respect-visual-line-mode t)
 
 (doom! :input
   ;;chinese
