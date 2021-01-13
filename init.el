ba240e2b340b4c39dbfb6d905f33529f923a6245
Chris Montgomery <chris@cdom.io>
2021-01-13 18:44:36 -0500

    disable company childframe to prevent crashes

--- init.el
+++ init.el
@@ -22,7 +22,7 @@
   ;;japanese
 
   :completion
-  (company +childframe)          ; the ultimate code completion backend
+  (company)          ; the ultimate code completion backend
   ;;helm                         ; the *other* search engine for love and life
   ;;ido                          ; the other *other* search engine...
   (ivy +fuzzy +prescient +icons) ; a search engine for love and life
