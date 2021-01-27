5888e6d2813c7fe92659eea0e1378697336dc956
Chris Montgomery <chris@cdom.io>
2021-01-27 15:32:17 -0500

    (gpg): point to correct startup script path

--- gpg/bin/startup-gpg-agent.sh
+++ gpg/bin/startup-gpg-agent.sh
@@ -1,9 +1,11 @@
+#!/usr/bin/env zsh
+
 # Ensure that gpg can find the agent when needed
 if [ -f ~/.gnupg/.gpg-agent-info ] && [ -n "$(pgrep gpg-agent)" ]; then
-    source ~/.gnupg/.gpg-agent-info
-    export GPG_AGENT_INFO
+  source ${HOME}/.gnupg/.gpg-agent-info
+  export GPG_AGENT_INFO
 else
-    eval $(gpg-agent --daemon --write-env-file ~/.gnupg/.gpg-agent-info)
+  eval $(gpg-agent --daemon --write-env-file ~/.gnupg/.gpg-agent-info)
 fi
 
 # This line is important for GUI tools to also find it
