12fa78dd0222fcda195e033b11e623aa33f0b621
Chris Montgomery <chris@cdom.io>
2021-02-04 00:57:05 -0500

    (doom-modeline): hide utf-8 encoding

--- config.el
+++ config.el
@@ -24,6 +24,15 @@
     (custom-set-faces!
       '((mode-line mode-line-inactive) :family "Iosevka Term"))))
 
+;; Hide 'UTF-8' encoding from the modeline, since it's the default.
+;; https://tecosaur.github.io/emacs-config/config.html
+(defun +doom-modeline-conditional-buffer-encoding ()
+  "We expect the encoding to be LF UTF-8, so only show the modeline when this is not the case"
+  (setq-local doom-modeline-buffer-encoding
+              (unless (or (eq buffer-file-coding-system 'utf-8-unix)
+                          (eq buffer-file-coding-system 'utf-8)))))
+(add-hook 'after-change-major-mode-hook #'+doom-modeline-conditional-buffer-encoding)
+
 ;; Default indent by 2 spaces
 (setq! evil-shift-width 2)
 
