597f05b4593ee69e09f11dc82ccffd32988bd548
Chris Montgomery <chris@cdom.io>
2021-02-22 19:34:32 -0500

    (doom): enable data module

--- emacs/dot-doom.d/init.el
+++ emacs/dot-doom.d/init.el
@@ -110,7 +110,7 @@
   ;;coq                ; proofs-as-programs
   ;;crystal            ; ruby at the speed of c
   ;;csharp             ; unity, .NET, and mono shenanigans
-  ;;data               ; config/data formats
+  data               ; config/data formats
   ;;(dart +flutter)    ; paint ui and not much else
   ;;elixir             ; erlang done right
   ;;elm                ; care for a cup of TEA?
