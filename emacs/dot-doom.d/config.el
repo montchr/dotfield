6d021f988fd7f64d93cfdd20f5c4fcf067b25450
Chris Montgomery <chris@cdom.io>
2021-02-22 19:34:14 -0500

    (projectile): ignore emacs.d

--- emacs/dot-doom.d/config.el
+++ emacs/dot-doom.d/config.el
@@ -144,9 +144,9 @@
 (setq-default history-length 1000)
 (setq-default prescient-history-length 1000)
 
-;; Prevent projectile from adding documentation directories as projects.
+;; Prevent projectile from adding unwanted directories as projects.
 ;; https://tecosaur.github.io/emacs-config/config.html#projectile
-(setq! projectile-ignored-projects '("~/" "/tmp" "~/.emacs.d/.local/straight/repos/"))
+(setq! projectile-ignored-projects '("~/" "/tmp" "~/.emacs.d" "~/.emacs.d/.local/straight/repos/"))
 (defun projectile-ignored-project-function (filepath)
   "Return t if FILEPATH is within any of `projectile-ignored-projects'"
   (or (mapcar (lambda (p) (s-starts-with-p p filepath)) projectile-ignored-projects)))
