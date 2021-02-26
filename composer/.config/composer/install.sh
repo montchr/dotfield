0e0aed9e570f36d6099d3611e5461fb15efe3ae6
Chris Montgomery <chris@cdom.io>
2021-02-26 12:19:50 -0500

    (php): manage composer configuration with stow

---
+++ composer/.config/composer/install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+EXPECTED_CHECKSUM="$(php -r 'copy("https://composer.github.io/installer.sig", "php://stdout");')"
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+
+if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
+then
+    >&2 echo 'ERROR: Invalid installer checksum'
+    rm composer-setup.php
+    exit 1
+fi
+
+php composer-setup.php \
+  --install-dir="${XDG_BIN_HOME:-${HOME}/.local/bin}" \
+  --filename="composer"
+RESULT=$?
+rm composer-setup.php
+exit $RESULT
