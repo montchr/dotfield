cecfac330a2a8f40cf5639ea8390d73053f2182b
Chris Montgomery <chris@cdom.io>
2021-02-28 23:33:25 -0500

    (emacs): reinstall parinfer with rust flag

--- emacs/dot-doom.d/init.el
+++ emacs/dot-doom.d/init.el
@@ -51,7 +51,7 @@
   ;;lispy            ; vim for lisp, for people who don't like vim
   ;;multiple-cursors   ; editing in many places at once
   ;;objed            ; text object editing for the innocent
-  ;;parinfer           ; turn lisp into python, sort of
+  (parinfer +rust)           ; turn lisp into python, sort of
   rotate-text        ; cycle region at point between text candidates
   snippets           ; my elves. They type so I don't have to
   word-wrap          ; soft wrapping with language-aware indent
