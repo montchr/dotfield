61a9090ec6461b26b2c91937000f8286b3898d94
Chris Montgomery <chris@cdom.io>
2021-03-16 22:21:58 -0400

    fix(emacs): disable parinfer
    it throws annoyingg prompts every time an org file is saved

--- emacs/dot-doom.d/init.el
+++ emacs/dot-doom.d/init.el
@@ -53,7 +53,8 @@
   ;;lispy            ; vim for lisp, for people who don't like vim
   ;;multiple-cursors   ; editing in many places at once
   ;;objed            ; text object editing for the innocent
-  (parinfer +rust)           ; turn lisp into python, sort of
+  ;; @TODO disabled parinfer due to annoying prompts saving org files
+  ;;(parinfer +rust)           ; turn lisp into python, sort of
   rotate-text        ; cycle region at point between text candidates
   snippets           ; my elves. They type so I don't have to
   word-wrap          ; soft wrapping with language-aware indent
