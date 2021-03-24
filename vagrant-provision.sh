3864003274b023a38cedcf0226bc9d3301a6dce6
Chris Montgomery <chris@cdom.io>
2021-03-24 00:17:20 -0400

    refactor(vagrant): call bootstrap for root and new user
    however, the environment variables passed down from the Vagrantfile
    aren't available to the `su` login shells. the variables need to be
    passed through somehow...

--- vagrant-provision.sh
+++ vagrant-provision.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 
-cd "${HOME}" || return 1
+set -e
 
-cp /vagrant/bootstrap "${HOME}/bootstrap"
-bash -c "${HOME}/bootstrap"
+# @TODO how to pass the required env vars to the other users?
+function .do_bootstrap() {
+  local user="$1"
+  sudo su - "${user}" -c "
+    cd ${HOME}
+    cp /vagrant/bootstrap ${HOME}/bootstrap
+    bash -c ${HOME}/bootstrap
+  "
+}
+
+.do_bootstrap root
+.do_bootstrap "${CDOM_INIT_NEW_USER_NAME}"
