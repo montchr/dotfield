59dee5c6a7d8bc569bdf6982432e61f4a1a493d9
Chris Montgomery <chris@cdom.io>
2021-03-25 23:21:27 -0400

    build(emacs): disable minimap

--- emacs/dot-doom.d/init.el
+++ emacs/dot-doom.d/init.el
@@ -23,13 +23,13 @@
   doom                         ; what makes DOOM look the way it does
   doom-dashboard               ; a nifty splash screen for Emacs
   doom-quit                    ; DOOM quit-message prompts when you quit Emacs
-  ;; fill-column               ; a `fill-column' indicator
   (emoji +unicode)     ; 🙂
+  ;; fill-column               ; a `fill-column' indicator
   hl-todo                      ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
   ;; hydra
   ;; indent-guides             ; highlighted indent columns
   (ligatures +extra)  ; ligatures and symbols
-  minimap                   ; show a map of the code on the side
+  ;; minimap                   ; show a map of the code on the side
   modeline                     ; snazzy, Atom-inspired modeline, plus API
   nav-flash                    ; blink cursor line after big motions
   ;; neotree                   ; a project drawer, like NERDTree for vim
