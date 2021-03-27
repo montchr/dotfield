0b3f7f4b3926a9eef97186efb8b0719191a65dc3
Chris Montgomery <chris@cdom.io>
2021-03-27 15:48:42 -0400

    build(emacs): add bats-mode

--- emacs/dot-doom.d/packages.el
+++ emacs/dot-doom.d/packages.el
@@ -58,6 +58,7 @@
            :repo "ideasman42/emacs-scroll-on-jump"))
 
 ;; Languages
+(package! bats-mode)                 ; mode for editing and running BATS tests
 (package! doct)                      ; _d_eclarative _o_rg _c_apture _t_emplates
 (package! literate-calc-mode)        ; like soulver, but in emacs
 (package! neon-mode
