986cf342ce28d32d484967ea980cc1cf30827e0d
Chris Montgomery <chris@cdom.io>
2021-04-13 12:51:18 -0400

    style(dotfield): no stupid braces

--- lib/utils.sh
+++ lib/utils.sh
@@ -24,68 +24,64 @@ set +u
   && emulate -L bash
 set -u
 
-{
-
-  if [[ ! -v USER ]]; then
-    USER="$(whoami)"
-  fi
+if [[ ! -v USER ]]; then
+  USER="$(whoami)"
+fi
 
 
-  if [[ ! -v KERNEL_NAME ]]; then
-    KERNEL_NAME="$( uname -s | tr '[:upper:]' '[:lower:]')"
-  fi
+if [[ ! -v KERNEL_NAME ]]; then
+  KERNEL_NAME="$( uname -s | tr '[:upper:]' '[:lower:]')"
+fi
 
-  if [[ ! -v OS_NAME || ! -v OS_VERSION ]]; then
-    case "${KERNEL_NAME}" in
-      darwin)
-        OS_NAME="macos"
-        OS_VERSION="$(sw_vers -productVersion)"
-        ;;
-      linux)
-        # shellcheck disable=1091
-        OS_NAME="$(
-          . /etc/os-release
-          printf "%s" "${ID}"
-        )"
-        # shellcheck disable=1091
-        OS_VERSION="$(
-          . /etc/os-release
-          printf "%s" "${VERSION_ID}"
-        )"
-        ;;
-      *) OS_NAME="unknown" ;;
-    esac
-  fi
+if [[ ! -v OS_NAME || ! -v OS_VERSION ]]; then
+  case "${KERNEL_NAME}" in
+    darwin)
+      OS_NAME="macos"
+      OS_VERSION="$(sw_vers -productVersion)"
+      ;;
+    linux)
+      # shellcheck disable=1091
+      OS_NAME="$(
+        . /etc/os-release
+        printf "%s" "${ID}"
+      )"
+      # shellcheck disable=1091
+      OS_VERSION="$(
+        . /etc/os-release
+        printf "%s" "${VERSION_ID}"
+      )"
+      ;;
+    *) OS_NAME="unknown" ;;
+  esac
+fi
 
-  PATH="$HOME/.local/bin:$PATH"
+PATH="$HOME/.local/bin:$PATH"
 
-  XDG_CONFIG_HOME="${DOTFIELD:-${HOME}/.config}"
-  XDG_CONFIG_CACHE="$HOME/.cache"
-  XDG_DATA_HOME="$HOME/.local/share"
-  XDG_CACHE_HOME="$HOME/.cache"
-  XDG_BIN_HOME="${HOME}/.local/bin"
+XDG_CONFIG_HOME="${DOTFIELD:-${HOME}/.config}"
+XDG_CONFIG_CACHE="$HOME/.cache"
+XDG_DATA_HOME="$HOME/.local/share"
+XDG_CACHE_HOME="$HOME/.cache"
+XDG_BIN_HOME="${HOME}/.local/bin"
 
-  DEVELOPER="${HOME}/Developer"
-  if [[ "$USER" != "$QUERENT" ]]; then
-    DEVELOPER="${HOME}/Developer/99-personal"
-  fi
-  
-  export \
-    DEVELOPER \
-    KERNEL_NAME \
-    KERNEL_RELEASE \
-    OS_NAME \
-    OS_VERSION \
-    PATH \
-    QUERENT \
-    USER \
-    XDG_BIN_HOME \
-    XDG_CACHE_HOME \
-    XDG_CONFIG_CACHE \
-    XDG_CONFIG_HOME \
-    XDG_DATA_HOME
+DEVELOPER="${HOME}/Developer"
+if [[ "$USER" != "$QUERENT" ]]; then
+  DEVELOPER="${HOME}/Developer/99-personal"
+fi
 
-}
+export \
+  DEVELOPER \
+  KERNEL_NAME \
+  KERNEL_RELEASE \
+  OS_NAME \
+  OS_VERSION \
+  PATH \
+  QUERENT \
+  USER \
+  XDG_BIN_HOME \
+  XDG_CACHE_HOME \
+  XDG_CONFIG_CACHE \
+  XDG_CONFIG_HOME \
+  XDG_DATA_HOME
 
 
 #========================================
