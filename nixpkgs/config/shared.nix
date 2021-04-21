cbcc456e5dc3dcc094231c928aaf4c2cd01b09c7
Chris Montgomery <chris@cdom.io>
2021-04-21 00:55:12 -0400

    feat(nix): use cachix

--- nixpkgs/config/shared.nix
+++ nixpkgs/config/shared.nix
@@ -5,13 +5,12 @@
     extraOptions = "experimental-features = nix-command flakes";
     binaryCaches = [
       "https://cache.nixos.org/"
-      "https://hardselius.cachix.org"
+      "https://montchr.cachix.org"
       "https://hydra.iohk.io"
       "https://iohk.cachix.org"
     ];
     binaryCachePublicKeys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      # "hardselius.cachix.org-1:PoN90aQw2eVMwfAy0MS6V9T2exWlgtHOUBBSnthXAl4="
       "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
       "iohk.cachix.org-1:DpRUyj7h7V830dp/i6Nti+NEO2/nhblbov/8MW7Rqoo="
     ];
