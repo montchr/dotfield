3e61dd21a6d5751738516d21d34496d60b76967b
Chris Montgomery <chris@cdom.io>
2021-04-21 14:59:24 -0400

    feat(fonts): add pragmata pro support to doom

--- emacs/init.el
+++ emacs/init.el
@@ -31,7 +31,9 @@
   hl-todo                      ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
   hydra
   ;; indent-guides             ; highlighted indent columns
-  (ligatures +extra)  ; ligatures and symbols
+  (ligatures
+    +extra
+    +pragmata-pro)             ; ligatures and symbols
   ;; minimap                   ; show a map of the code on the side
   modeline                     ; snazzy, Atom-inspired modeline, plus API
   nav-flash                    ; blink cursor line after big motions
