90cdd94f5be76ff7ff34347c3fdbc4bd03d83c02
Chris Montgomery <chris@cdom.io>
2021-05-02 16:49:33 -0400

    (nix): fix `home` reference

--- dotfield/modules/shared/xdg.nix
+++ dotfield/modules/shared/xdg.nix
@@ -8,17 +8,16 @@
 
 { config, home-manager, lib, ... }:
 {
-  home-manager = {
-    users.${config.user.name}.xdg.enable = true;
-    home.sessionVariables = {
-      # These are the defaults, and xdg.enable does set them, but due to load
-      # order, they're not set before environment.variables are set, which could
-      # cause race conditions.
-      XDG_CACHE_HOME = "$HOME/.cache";
-      XDG_CONFIG_HOME = "$HOME/.config";
-      XDG_DATA_HOME = "$HOME/.local/share";
-      XDG_BIN_HOME = "$HOME/.local/bin";
-    };
+  home-manager.users.${config.user.name}.xdg.enable = true;
+
+  home.sessionVariables = {
+    # These are the defaults, and xdg.enable does set them, but due to load
+    # order, they're not set before environment.variables are set, which could
+    # cause race conditions.
+    XDG_CACHE_HOME = "$HOME/.cache";
+    XDG_CONFIG_HOME = "$HOME/.config";
+    XDG_DATA_HOME = "$HOME/.local/share";
+    XDG_BIN_HOME = "$HOME/.local/bin";
   };
 
   environment = {
