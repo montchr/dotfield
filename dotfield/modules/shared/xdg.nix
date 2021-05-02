e20b096b46f92105f94717ce6ade0148a80d7e9e
Chris Montgomery <chris@cdom.io>
2021-05-02 17:00:35 -0400

    (nix): home again

--- dotfield/modules/shared/xdg.nix
+++ dotfield/modules/shared/xdg.nix
@@ -10,14 +10,16 @@
 {
   home-manager.users.${config.user.name}.xdg.enable = true;
 
-  home.sessionVariables = {
-    # These are the defaults, and xdg.enable does set them, but due to load
-    # order, they're not set before environment.variables are set, which could
-    # cause race conditions.
-    XDG_CACHE_HOME = "$HOME/.cache";
-    XDG_CONFIG_HOME = "$HOME/.config";
-    XDG_DATA_HOME = "$HOME/.local/share";
-    XDG_BIN_HOME = "$HOME/.local/bin";
+  config = {
+    home.sessionVariables = {
+      # These are the defaults, and xdg.enable does set them, but due to load
+      # order, they're not set before environment.variables are set, which could
+      # cause race conditions.
+      XDG_CACHE_HOME = "$HOME/.cache";
+      XDG_CONFIG_HOME = "$HOME/.config";
+      XDG_DATA_HOME = "$HOME/.local/share";
+      XDG_BIN_HOME = "$HOME/.local/bin";
+    };
   };
 
   environment = {
