c469109f1cda30de5cd0d630a94aff7023c2bda4
Chris Montgomery <chris@cdom.io>
2021-05-09 23:24:59 -0400

    (nix): add flake-utils

--- flake.nix
+++ flake.nix
@@ -1,43 +1,11 @@
 # Thank you to https://github.com/ahmedelgabri/dotfiles/
 
-# As a first step, I will try to symlink my configs as much as possible then
-# migrate the configs to Nix
-#
-# https://nixcloud.io/ for Nix syntax
-# https://nix.dev/
-# https://nix-community.github.io/awesome-nix/
-# https://discourse.nixos.org/t/home-manager-equivalent-of-apt-upgrade/8424/3
-# https://www.reddit.com/r/NixOS/comments/jmom4h/new_neofetch_nixos_logo/gayfal2/
-# https://www.youtube.com/user/elitespartan117j27/videos?view=0&sort=da&flow=grid
-# https://www.youtube.com/playlist?list=PLRGI9KQ3_HP_OFRG6R-p4iFgMSK1t5BHs
-# https://www.reddit.com/r/NixOS/comments/k9xwht/best_resources_for_learning_nixos/
-# https://www.reddit.com/r/NixOS/comments/k8zobm/nixos_preferred_packages_flow/
-# https://www.reddit.com/r/NixOS/comments/j4k2zz/does_anyone_use_flakes_to_manage_their_entire/
-#
-# Sample repos
-# https://github.com/teoljungberg/dotfiles/tree/master/nixpkgs (contains custom hammerspoon & vim )
-# https://github.com/jwiegley/nix-config (nice example)
-# https://github.com/hardselius/dotfiles (good readme on steps to do for install)
-# https://github.com/nuance/dotfiles
-#
-# With flakes
-# https://github.com/hlissner/dotfiles
-# https://github.com/mjlbach/nix-dotfiles
-# https://github.com/thpham/nix-configs/blob/e46a15f69f/default.nix (nice example of how to build)
-# https://github.com/sandhose/nixconf
-# https://github.com/cideM/dotfiles (darwin, NixOS, home-manager)
-# https://github.com/monadplus/nixconfig
-# https://github.com/jamesottaway/dotfiles (Darwin, NixOS, home-manager)
-# https://github.com/malob/nixpkgs (Darwin, home-manager, homebrew in nix & PAM, this is a great example)
-# https://github.com/kclejeune/system (nice example)
-# https://github.com/mrkuz/nixos
-#
-
 {
   description = "Dotfield";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
 
     home-manager = {
       url = "github:nix-community/home-manager";
@@ -56,7 +24,7 @@
   };
 
   # N.B. including nixpkgs here may cause yabai overlay to break!
-  outputs = { self, ... }@inputs:
+  outputs = { self, flake-utils, ... }@inputs:
     let
       sharedHostsConfig = { config, pkgs, lib, options, ... }: {
         nix = {
@@ -127,6 +95,24 @@
           '';
         });
       });
+
+      # TODO: SAD.
+      # flake-utils.lib.eachDefaultSystem ({
+      #   # Nix flakes REPL
+      #   # https://github.com/NixOS/nix/issues/3803#issuecomment-748612294
+      #   #
+      #   # Usage:
+      #   #   nix run .#repl
+      #   apps.repl = flake-utils.lib.mkApp {
+      #     drv = inputs.pkgs.writeShellScriptBin "repl" ''
+      #       confnix=$(mktemp)
+      #       echo "builtins.getFlake (toString $(git rev-parse --show-toplevel))" >$confnix
+      #       trap "rm $confnix" EXIT
+      #       nix repl $confnix
+      #     '';
+      #   };
+      # });
+
 
       darwinConfigurations = {
         "hodgepodge" = inputs.darwin.lib.darwinSystem {
