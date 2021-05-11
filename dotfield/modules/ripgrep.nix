d927abe732d7ec704c2df6d47f842e9a0103c81c
Chris Montgomery <chris@cdom.io>
2021-05-11 12:10:11 -0400

    (nix): generate empty ripgrep config to prevent error

--- dotfield/modules/ripgrep.nix
+++ dotfield/modules/ripgrep.nix
@@ -1,10 +1,8 @@
 { pkgs, lib, config, ... }:
 
 let
-
   cfg = config.my.modules.ripgrep;
-  # cfgDir = "${config.dotfield.configDir}/ripgrep";
-
+  cfgDir = "${config.dotfield.configDir}/ripgrep";
 in {
   options = with lib; {
     my.modules.ripgrep = {
@@ -16,15 +14,14 @@ in {
 
   config = with lib;
     mkIf cfg.enable {
-      my.env = { RIPGREP_CONFIG_PATH = "$XDG_CONFIG_HOME/ripgrep/config"; };
+      my = {
+        env = { RIPGREP_CONFIG_PATH = "$XDG_CONFIG_HOME/ripgrep/config"; };
 
-      my.user = { packages = with pkgs; [ ripgrep ]; };
+        user = { packages = with pkgs; [ ripgrep ]; };
 
-      # my.hm.configFile = {
-      #   "ripgrep" = {
-      #     recursive = true;
-      #     source = builtins.toPath /. cfgDir;
-      #   };
-      # };
+        hm.configFile = {
+          "ripgrep/config" = "";
+        };
+      };
     };
 }
