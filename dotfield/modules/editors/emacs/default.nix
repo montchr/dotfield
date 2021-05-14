65fb028a19e93d189fa20dbf0259ee16fa34b496
Chris Montgomery <chris@cdom.io>
2021-05-14 12:00:49 -0400

    (emacs/nix): update module

--- dotfield/modules/editors/emacs/default.nix
+++ dotfield/modules/editors/emacs/default.nix
@@ -1,25 +1,20 @@
-{ config, lib, pkgs, inputs, ... }:
+{ config, lib, pkgs, emacs, inputs, ... }:
 
 with lib;
-# with lib.my;
 let
   cfg = config.my.modules.editors.emacs;
   configDir = config.dotfiles.configDir;
 in
 {
-  options.modules.editors.emacs = {
-    enable = mkBoolOpt false;
-    doom = {
-      enable = mkBoolOpt true;
+  options = with lib; {
+    my.modules.editors.emacs = {
+      enable = mkEnableOption false;
+      doom.enable = mkEnableOption true;
     };
   };
 
   config = mkIf cfg.enable {
-    nixpkgs.overlays = [ inputs.emacs.overlay ];
-
-    programs.emacs.enable = true;
-
-    user.packages = with pkgs; [
+    my.user.packages = with pkgs; [
       ## Emacs itself
       binutils # native-comp needs 'as', provided by this
       # emacsPgtkGcc # 28 + pgtk + native-comp
@@ -45,20 +40,14 @@ in
       # :checkers grammar
       languagetool
       # :tools editorconfig
-      editorconfig-core-c # per-project style config
+      editorconfig-core-c
       # :tools lookup & :lang org +roam
       sqlite
-      # :lang cc
-      # ccls
       # :lang javascript
       nodePackages.javascript-typescript-langserver
     ];
 
-    # Doom Emacs bin.
-    # TODO: ensure this doesn't overwrite existing PATH!
-    # my.env.PATH = [ "$XDG_CONFIG_HOME/emacs/bin" ];
-
-    my.modules.zsh.rcFiles = [ "${configDir}/emacs/aliases.zsh" ];
+    # my.modules.zsh.rcFiles = [ "${configDir}/emacs/aliases.zsh" ];
 
     fonts.fonts = [ pkgs.emacs-all-the-icons-fonts ];
 
