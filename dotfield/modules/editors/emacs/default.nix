f5aa966c9bc6209549d598dbf635d4d4ff1119b1
Chris Montgomery <chris@cdom.io>
2021-05-14 00:05:03 -0400

    (emacs): updates from cmacrae/emacs

--- dotfield/modules/editors/emacs/default.nix
+++ dotfield/modules/editors/emacs/default.nix
@@ -2,35 +2,40 @@
 
 with lib;
 # with lib.my;
-let cfg = config.my.modules.editors.emacs;
-    configDir = config.dotfiles.configDir;
-in {
+let
+  cfg = config.my.modules.editors.emacs;
+  configDir = config.dotfiles.configDir;
+in
+{
   options.modules.editors.emacs = {
     enable = mkBoolOpt false;
     doom = {
-      enable  = mkBoolOpt true;
+      enable = mkBoolOpt true;
     };
   };
 
   config = mkIf cfg.enable {
-    nixpkgs.overlays = [ inputs.emacs-overlay.overlay ];
+    nixpkgs.overlays = [ inputs.emacs.overlay ];
+
+    programs.emacs.enable = true;
 
     user.packages = with pkgs; [
       ## Emacs itself
-      binutils       # native-comp needs 'as', provided by this
-      emacsPgtkGcc   # 28 + pgtk + native-comp
+      binutils # native-comp needs 'as', provided by this
+      # emacsPgtkGcc # 28 + pgtk + native-comp
 
       ## Doom dependencies
-      git
-      (ripgrep.override {withPCRE2 = true;})
-      gnutls              # for TLS connectivity
+      (ripgrep.override { withPCRE2 = true; })
+      gnutls # for TLS connectivity
 
       ## Optional dependencies
-      fd                  # faster projectile indexing
-      imagemagick         # for image-dired
-      (mkIf (config.programs.gnupg.agent.enable)
-        pinentry_emacs)   # in-emacs gnupg prompts
-      zstd                # for undo-fu-session/undo-tree compression
+      fd # faster projectile indexing
+      imagemagick # for image-dired
+      (
+        mkIf (config.programs.gnupg.agent.enable)
+          pinentry_emacs
+      ) # in-emacs gnupg prompts
+      zstd # for undo-fu-session/undo-tree compression
 
       ## Module dependencies
       # :checkers spell
@@ -57,10 +62,10 @@ in {
 
     fonts.fonts = [ pkgs.emacs-all-the-icons-fonts ];
 
-    system.activationScripts.postUserActivation.text = mkIf cfg.doom.enable ''
-      if [[ ! -d $HOME/.config/emacs ]]; then
-        git clone https://github.com/hlissner/doom-emacs $HOME/.config/emacs
-      fi
-    '';
+    # system.activationScripts.postUserActivation.text = mkIf cfg.doom.enable ''
+    #   if [[ ! -d $HOME/.config/emacs ]]; then
+    #     git clone https://github.com/hlissner/doom-emacs $HOME/.config/emacs
+    #   fi
+    # '';
   };
 }
