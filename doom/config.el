7621c53f0cfc30c16c038594dc83f7b4d90802b0
Chris Montgomery <chris@cdom.io>
2021-06-05 00:36:22 -0400

    (emacs): use projectile native indexing

--- doom/config.el
+++ doom/config.el
@@ -347,6 +347,7 @@
 
 (use-package! projectile
   :config
+  (setq! projectile-indexing-method 'native)
   (appendq! projectile-globally-ignored-directories '("client-mu-plugins/vendor")))
 
 (use-package! treemacs
