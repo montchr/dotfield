8ab339aa9fe82f509c612485dadb5948ad079354
Chris Montgomery <chris@cdom.io>
2021-06-05 00:34:48 -0400

    (emacs): disable ivy-posframe

--- doom/init.el
+++ doom/init.el
@@ -17,7 +17,6 @@
   ;;helm                         ; the *other* search engine for love and life
   ;;ido                          ; the other *other* search engine...
   (ivy                           ; a search engine for love and life
-    +childframe
     +prescient
     +icons)
 
@@ -52,7 +51,7 @@
   (evil +everywhere) ; come to the dark side, we have cookies
   file-templates     ; auto-snippets for empty files
   fold               ; (nigh) universal code folding
-  format   ; automated prettiness
+  format             ; automated prettiness
   ;;god              ; run Emacs commands without modifier keys
   ;;lispy            ; vim for lisp, for people who don't like vim
   ;;multiple-cursors   ; editing in many places at once
