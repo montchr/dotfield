519e2d0de0e4db741ae4e1cc4de201c34475eb8c
Chris Montgomery <chris@cdom.io>
2021-06-07 00:07:44 -0400

    (shell): move bootstrap helper vars out of utils

--- utils.sh
+++ utils.sh
@@ -20,8 +20,6 @@
 #
 # ============= TABLE OF CONTENTS ============ #
 #                                              #
-# 0. The World            ::     [[ world ]]   #
-#                                              #
 # 1. String Manipulation  ::       [[ str ]]   #
 #                                              #
 # 2. Messages + Prompts   ::       [[ msg ]]   #
@@ -56,109 +54,9 @@
 readonly UTILS_LOADED="true"
 
 
-USER="${USER:-}"
-if [[ -z "${USER}" ]]; then
-  USER="$(whoami)"
-fi
-
-KERNEL_NAME="${KERNEL_NAME:-}"
-if [[ -z "${KERNEL_NAME}" ]]; then
-  KERNEL_NAME="$( uname -s | tr '[:upper:]' '[:lower:]')"
-fi
-
-OS_NAME="${OS_NAME:-}"
-OS_VERSION="${OS_VERSION:-}"
-if [[ -z "${OS_NAME}" || -z "${OS_VERSION}" ]]; then
-  case "${KERNEL_NAME}" in
-    darwin)
-      OS_NAME="macos"
-      OS_VERSION="$(sw_vers -productVersion)"
-      ;;
-    linux)
-      # shellcheck disable=1091
-      OS_NAME="$(
-        . /etc/os-release
-        printf "%s" "${ID}"
-      )"
-      # shellcheck disable=1091
-      OS_VERSION="$(
-        . /etc/os-release
-        printf "%s" "${VERSION_ID}"
-      )"
-      ;;
-    *) OS_NAME="unknown" ;;
-  esac
-fi
-
-PATH="$HOME/.local/bin:$PATH"
-
-XDG_CONFIG_HOME="${HOME}/.config"
-XDG_DATA_HOME="$HOME/.local/share"
-XDG_CACHE_HOME="$HOME/.cache"
-XDG_BIN_HOME="${HOME}/.local/bin"
-
-DEVELOPER="${HOME}/Developer"
-if [[ "$USER" != "$QUERENT" ]]; then
-  DEVELOPER="${HOME}/Developer/99-personal"
-fi
-
-export \
-  DEVELOPER \
-  KERNEL_NAME \
-  KERNEL_RELEASE \
-  OS_NAME \
-  OS_VERSION \
-  PATH \
-  QUERENT \
-  USER \
-  XDG_BIN_HOME \
-  XDG_CACHE_HOME \
-  XDG_CONFIG_HOME \
-  XDG_DATA_HOME
-
-
+# Settings
 readonly MSG__INDENT="    "  # 4 spaces
 readonly MSG__COL__GAP="  "  # 2 spaces
-
-
-#====\\\===\\===\\\===\\===\\\===\\===\\\===\\===\\\===\\===\\\===\\===\\\===>
-#:
-#:    ==>   THE WORLD  [[ world ]]
-#:
-#====///===//===///===//===///===//===///===//===///===//===///===//===///===>
-
-
-#========================================
-# Print basic information about the known world.
-#
-# Usage:
-#   world::info
-# Globals:
-#   BASH_VERSION
-#   KERNEL_NAME
-#   KERNEL_RELEASE
-#   OS_NAME
-#   OS_VERSION
-#   USER
-#   XDG_BIN_HOME
-#   XDG_CONFIG_HOME
-# Outputs:
-#   Values of various key global variables.
-#========================================
-function world::info {
-
-msg::stream::info <<END
-Kernel name:         ${KERNEL_NAME}
-Kernel release:      ${KERNEL_RELEASE}
-Operating system:    ${OS_NAME}
-OS version:          ${OS_VERSION}
-Bash version:        ${BASH_VERSION}
-User:                ${USER}
-XDG_CONFIG_HOME:     ${XDG_CONFIG_HOME}
-XDG_BIN_HOME:        ${XDG_BIN_HOME}
-END
-
-}
 
 
 #====\\\===\\===\\\===\\===\\\===\\===\\\===\\===\\\===\\===\\\===\\===\\\===>
