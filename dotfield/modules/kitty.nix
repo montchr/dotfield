70be06b7e5b1443e3cc25f5a81705dfb2bafd9c5
Chris Montgomery <chris@cdom.io>
2021-07-04 13:22:46 -0400

    refactor(kitty): move get window script to nix config

--- dotfield/modules/kitty.nix
+++ dotfield/modules/kitty.nix
@@ -1,10 +1,16 @@
 { pkgs, lib, config, options, ... }:
 
 let
-
   cfg = config.my.modules.kitty;
-  # cfgDir = "${config.dotfield.configDir}/kitty";
+  configDir = "${config.dotfield.configDir}/kitty";
 
+  scripts = with pkgs; {
+    kittyGetWindow = (writeShellScriptBin "kitty-get-window-by-platform-id" ''
+      kitty @ --to $KITTY_SOCKET ls \
+        | ${pkgs.jq}/bin/jq -r --argjson id "$1" \
+          '.[] | select(.platform_window_id==$id)'
+    '');
+  };
 in {
   options = with lib; {
     my.modules.kitty = {
@@ -16,23 +22,12 @@ in {
 
   config = with lib;
     mkIf cfg.enable (mkMerge [
-      (if (builtins.hasAttr "homebrew" options) then {
-        homebrew.casks = [ "kitty" ];
-      } else {
-        my.user = { packages = with pkgs; [ kitty ]; };
-      })
-
       {
+        homebrew.casks = [ "kitty" ];
+        my.user.packages = (map (key: getAttr key scripts) (attrNames scripts));
         environment.variables = {
           TERMINFO_DIRS = "${pkgs.kitty.terminfo.outPath}/share/terminfo";
         };
-
-        # my.hm.configFile = {
-        #   "kitty" = {
-        #     recursive = true;
-        #     source = cfgDir;
-        #   };
-        # };
       }
     ]);
 }
