644be9c451810ca380fc75397133f9cb67d81241
Chris Montgomery <chris@cdom.io>
2021-07-04 15:15:20 -0400

    (dotfield): delete some unnecessary configs

--- dotfield/modules/node.nix
+++ dotfield/modules/node.nix
@@ -46,6 +46,31 @@ in {
               init-version=0.0.1
             '';
           };
+
+          ".prettierrc" = ''
+            # ${nix_managed}
+            ---
+            arrowParens: 'always'
+            bracketSpacing: true
+            jsxBracketSameLine: false
+            jsxSingleQuote: false
+            printWidth: 80
+            proseWrap: 'never'
+            quoteProps: 'as-needed'
+            semi: true
+            singleQuote: true
+            tabWidth: 2
+            trailingComma: 'all'
+            useTabs: false
+            overrides:
+              - files: '*.php'
+                options:
+                  braceStyle: '1tbs'
+                  phpVersion: '7.4'
+                  tabWidth: 4
+                  trailingCommaPHP: true
+                  useTabs: true
+            '';
         };
       };
     };
