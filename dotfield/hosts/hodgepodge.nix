efa17cf257b7e92a1132251bcdee74cb1d7a30cd
Chris Montgomery <chris@cdom.io>
2021-07-06 22:36:49 -0400

    (nix): update hodgepodge user info

--- dotfield/hosts/hodgepodge.nix
+++ dotfield/hosts/hodgepodge.nix
@@ -1,4 +1,10 @@
 { config, inputs, ... }: {
   imports = [ ../modules/darwin ];
   networking.hostName = "HodgePodge";
+
+  my = {
+    username = "cdom";
+    email = "chris@cdom.io";
+    website = "https://github.com/montchr/";
+  };
 }
