8476b8bf2a8ea849c7a18386ae672a37dad8e78a
Chris Montgomery <chris@cdom.io>
2021-07-12 02:14:46 -0400

    refactor(php): move composer config into flake

--- dotfield/modules/php.nix
+++ dotfield/modules/php.nix
@@ -1,10 +1,18 @@
 { config, pkgs, lib, ... }:
 with lib;
-let cfg = config.my.modules.php;
+let
+  cfg = config.my.modules.php;
+  composerConfigDir = "${config.dotfield.configDir}/composer";
 in {
   options = with lib; { my.modules.php = { enable = mkEnableOption false; }; };
 
   config = mkIf cfg.enable {
     my.user.packages = with pkgs; [ php php74Packages.composer ];
+
+    my.hm.configFile = {
+      "composer/composer.json" = "${composerConfigDir}/composer.json";
+      "composer/composer.lock" = "${composerConfigDir}/composer.lock";
+      };
+    };
   };
 }
