bca5d22c5cf45ea4514446c0566464884c44fa53
Chris Montgomery <chris@cdom.io>
2021-07-16 22:02:06 -0400

    (php): remove composer links

--- dotfield/modules/php.nix
+++ dotfield/modules/php.nix
@@ -2,21 +2,10 @@
 with lib;
 let
   cfg = config.my.modules.php;
-  composerConfigDir = "${config.my.xdgPaths.config}/composer";
 in {
   options = { my.modules.php = { enable = mkEnableOption false; }; };
 
   config = mkIf cfg.enable {
     my.user.packages = with pkgs; [ php php74Packages.composer ];
-
-    my.hm.configFile = with config.my.hm.lib.file; {
-      "composer/composer.json" = {
-        source = (mkOutOfStoreSymlink "${composerConfigDir}/composer.json");
-      };
-
-      "composer/composer.lock" = {
-        source = (mkOutOfStoreSymlink "${composerConfigDir}/composer.lock");
-      };
-    };
   };
 }
