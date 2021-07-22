687bf7fae761a7c0cb292e59267c478d0c0026f1
Chris Montgomery <chris@cdom.io>
2021-07-22 17:11:19 -0400

    (emacs): disable vi-tilde-fringe

--- doom/init.el
+++ doom/init.el
@@ -42,7 +42,7 @@
   (treemacs +lsp)              ; a project drawer, like neotree but cooler
   ;; unicode                   ; extended unicode support for various languages
   vc-gutter                    ; vcs diff in the fringe
-  vi-tilde-fringe              ; fringe tildes to mark beyond EOB
+  ;; vi-tilde-fringe              ; fringe tildes to mark beyond EOB
   ;; window-select             ; visually switch windows
   workspaces                   ; tab emulation, persistence & separate workspaces
   zen                          ; distraction-free coding or writing
