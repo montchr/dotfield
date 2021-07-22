794ba1930d41103d59261aa5e7d2f87c074eee5f
Chris Montgomery <chris@cdom.io>
2021-07-21 21:54:02 -0400

    fix(bash): load bash 5 in user environment

--- dotfield/modules/bash.nix
+++ dotfield/modules/bash.nix
@@ -5,6 +5,7 @@ let
   configDir = config.dotfield.configDir;
   home = config.my.user.home;
   cfg = config.my.modules.bash;
+  bash = pkgs.bashInteractive_5;
 in {
   options = with lib; {
     my.modules.bash = {
@@ -24,6 +25,8 @@ in {
     my = {
       env = { BASH_COMPLETION_USER_FILE = "$XDG_DATA_HOME/bash/completion"; };
 
+      user.packages = [ bash ];
+
       hm = {
         configFile = {
 
@@ -37,12 +40,9 @@ in {
       };
     };
 
-    # List packages installed in system profile. To search by name, run:
-    # $ nix-env -qaP | grep wget
     environment = {
-      shells = with pkgs; [ bashInteractive_5 ];
-
-      systemPackages = with pkgs; [ bashInteractive_5 ];
+      shells = [ bash ];
+      systemPackages = [ bash ];
     };
   };
 }
