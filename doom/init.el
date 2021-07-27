51b5cce4cde35eee86642d0a67b1db15a2776426
Chris Montgomery <chris@cdom.io>
2021-07-27 13:01:48 -0400

    feat(emacs): drop ivy, use vertico!

--- doom/init.el
+++ doom/init.el
@@ -16,9 +16,8 @@
   (company +childframe)          ; the ultimate code completion backend
   ;;helm                         ; the *other* search engine for love and life
   ;;ido                          ; the other *other* search engine...
-  (ivy                           ; a search engine for love and life
-    +prescient
-    +icons)
+  ;;(ivy +prescient +icons)      ; a search engine for love and life
+  (vertico +icons)               ; the search engine of the future
 
   :ui
   deft                      ; notational velocity for Emacs
