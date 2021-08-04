b97faee8fd8b940729af61985cd73784c7c7214d
Chris Montgomery <chris@cdom.io>
2021-08-04 12:08:56 -0400

    refactor(php): move config into flake

--- dotfield/modules/php.nix
+++ dotfield/modules/php.nix
@@ -1,11 +1,17 @@
 { config, pkgs, lib, ... }:
 with lib;
-let
-  cfg = config.my.modules.php;
+let cfg = config.my.modules.php;
 in {
   options = { my.modules.php = { enable = mkEnableOption false; }; };
 
   config = mkIf cfg.enable {
-    my.user.packages = with pkgs; [ php php74Packages.composer ];
+    my.user.packages = with pkgs; [
+      php
+      php74Packages.composer
+
+      # Get the current `php` executable's version number.
+      (writeShellScriptBin "php-version" "php -v | awk '/^PHP/{print $2}'")
+    ];
+
   };
 }
