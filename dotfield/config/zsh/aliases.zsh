2f20d8540ad7e565d75bc4141b3e0ae9f9335b6b
Chris Montgomery <chris@alley.co>
2021-08-09 09:58:47 -0400

    (zsh): add some aliases for npm and vagrant

--- dotfield/config/zsh/aliases.zsh
+++ dotfield/config/zsh/aliases.zsh
@@ -141,3 +141,18 @@ alias flushdns="dscacheutil -flushcache"
 # Empty the Trash on all mounted volumes and the main HDD
 # Also, clear Apple’s System Logs to improve shell startup speed
 alias emptytrash="sudo rm -rfv /Volumes/*/.Trashes; sudo rm -rfv ~/.Trash; sudo rm -rfv /private/var/log/asl/*.asl"
+
+
+# -------------------------------------
+#  NPM
+# -------------------------------------
+
+alias nrb="npm run build"
+alias nrd="npm run dev"
+
+
+# -------------------------------------
+#  VAGRANT
+# -------------------------------------
+
+alias vup="vagrant up"
