c532aac132630f0e2607f9f933ffe4be182f07ce
Chris Montgomery <chris@cdom.io>
2021-09-06 12:30:42 -0400

    fix: point default dotfield dir to its own xdg config dir

--- modules/settings.nix
+++ modules/settings.nix
@@ -32,8 +32,12 @@ in {
     in rec {
       configDir = mkOpt t "${config.dotfield.dir}/config";
       dir = mkOpt t (toString ../.);
-      # FIXME: this points to an arbitrary location which will vary per system
-      path = mkOpt t "${config.my.user.home}/Developer/dotfield";
+      # FIXME: This points to an arbitrary location which may vary per system.
+      # Instead, it should be determined programmatically based on the flake's
+      # actual location. Note, however, that its currently only useful for
+      # out-of-store symlinks, which are generally discouraged as they do not
+      # adhere to the Nix principle of immutable configuration.
+      path = mkOpt t "${config.my.user.home}/.config/dotfield";
       binDir = mkOpt t "${config.dotfield.dir}/bin";
       libDir = mkOpt t "${config.dotfield.dir}/lib";
       modulesDir = mkOpt t "${config.dotfield.dir}/modules";
