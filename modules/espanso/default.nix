a974c94330e86d7c9a8bc00b288ee153eb7b75d3
Chris Montgomery <chris@alley.co>
2021-09-06 14:17:44 -0400

    fix(espanso): no symlinking at all

--- modules/espanso/default.nix
+++ modules/espanso/default.nix
@@ -6,8 +6,7 @@ let
   configDirPath = "${config.dotfield.configDir}/espanso";
   snippetsDir = "$XDG_CONFIG_HOME/espanso/user";
   plugins = [ "greek-letters-alt" ];
-  # secrets = (map (s: "${configDirPath}/user/${s}") [ "personal.yml" "work.yml" ]);
-  secrets = [ ];
+  secrets = (map (s: "${configDirPath}/user/${s}") [ "personal.yml" "work.yml" ]);
 in {
   options = with lib; {
     my.modules.espanso = { enable = mkEnableOption false; };
@@ -35,8 +34,9 @@ in {
         system.activationScripts.postUserActivation.text = ''
           # Link decrypted secret configs to the Espanso snippets directory,
           # bypassing Nix so secrets don't pass through the store.
-          mkdir -p "${snippetsDir}"
-          ln -sfv ${toString secrets} ${snippetsDir}
+	  # FIXME: bring these back
+          # mkdir -p "${snippetsDir}"
+          # ln -sfv ${toString secrets} ${snippetsDir}
 
           # Restart the daemon.
           ${cmd} restart
