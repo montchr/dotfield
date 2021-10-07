889b6b1e41bcb3bb0c852ddd2c371a89f93d8e76
Chris Montgomery <chris@alley.co>
2021-10-07 00:49:58 -0400

    tweak(emacs): enable osc keycodes in tty module

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -103,7 +103,7 @@
 
   :os
   (:if IS-MAC macos)  ; improve compatibility with macOS
-  tty                 ; improve the terminal Emacs experience
+  (tty +osc)          ; improve the terminal Emacs experience
 
   :lang
   ;;agda               ; types of types of types of types...
