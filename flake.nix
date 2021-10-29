956379bbf3c8d98d4e474a8ca0dc5ad723094c1c
Chris Montgomery <chris@alley.co>
2021-10-29 12:36:25 -0400

    fix: syntax

--- flake.nix
+++ flake.nix
@@ -129,7 +129,7 @@
           };
           ghaDarwin = utils.lib.mergeAny darwinHostDefaults {
             modules = [ ./hosts/gha-darwin.nix ];
-          }
+          };
         };
 
       # Shortcuts
