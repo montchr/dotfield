7d942835f146f4ca4324b4eaeac530ad8b206f45
Chris Montgomery <chris@alley.co>
2021-10-29 17:03:02 -0400

    fix: restore `HodgePodge` host file

---
+++ hosts/HodgePodge.nix
@@ -0,0 +1,28 @@
+{ config, inputs, ... }: {
+  imports = [ ../modules/darwin ];
+
+  my = {
+    username = "cdom";
+    email = "chris@cdom.io";
+    website = "https://github.com/montchr/";
+    hm.accounts.email.accounts.personal.primary = true;
+  };
+
+  networking = {
+    hostName = "HodgePodge";
+    knownNetworkServices =
+      [ "Wi-Fi" "Bluetooth PAN" "iPhone USB" "Thunderbolt Bridge" ];
+  };
+
+  homebrew = {
+    casks = [
+      "adobe-acrobat-reader"
+      "audio-hijack"
+      "soundsource"
+      "tigervnc-viewer"
+      "transmit"
+    ];
+
+    masApps = { "Paprika Recipe Manager 3" = 1303222628; };
+  };
+}
