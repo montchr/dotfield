e25edc11dda0370420e5030766a87426e87da8f9
Chris Montgomery <chris@alley.co>
2021-11-09 23:30:24 -0500

    refactor(espanso): remove for now

--- modules/default.nix
+++ modules/default.nix
@@ -7,13 +7,11 @@
     ./tealdeer.nix
 
     ./editors/vim
-    ./espanso
     ./firefox
     ./php
   ];
 
   my.modules = {
-    espanso.enable = true;
     firefox.enable = true;
     gui.enable = true;
     tealdeer.enable = true;
