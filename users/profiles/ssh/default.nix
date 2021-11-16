a079a5eef5f7b6f50ae85d331078d43835b3d97a
Chris Montgomery <chris@alley.co>
2021-11-16 15:44:51 -0500

    fix(ssh): no more broken ssh configuration

--- users/profiles/ssh/default.nix
+++ users/profiles/ssh/default.nix
@@ -14,6 +14,10 @@ in
     enable = true;
     hashKnownHosts = true;
     forwardAgent = false;
+    serverAliveInterval = 300;
+    serverAliveCountMax = 2;
+
+    includes = [ "${config.my.user.home}/.config/ssh/config.local" ];
 
     matchBlocks = {
 
@@ -23,32 +27,39 @@ in
         extraOptions = {
           "MACs" = "hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,umac-128-etm@openssh.com,hmac-sha2-512,hmac-sha2-256,umac-128@openssh.com";
         };
+        extraOptions = {
+          ControlMaster = "no";
+        };
       };
 
       "*" = {
+        addressFamily = "inet";
         forwardX11 = false;
         forwardX11Trusted = false;
+        identityFile = identityFile;
         serverAliveInterval = 300;
         serverAliveCountMax = 2;
+
+        extraOptions = {
+          AddKeysToAgent = "yes";
+          ChallengeResponseAuthentication = "no";
+          PasswordAuthentication = "no";
+          StrictHostKeyChecking = "ask";
+          VerifyHostKeyDNS = "yes";
+          VisualHostKey = "yes";
+
+          Ciphers = "chacha20-poly1305@openssh.com,aes256-gcm@openssh.com";
+          HostKeyAlgorithms = "ssh-ed25519-cert-v01@openssh.com,ssh-rsa-cert-v01@openssh.com,ssh-ed25519,ssh-rsa";
+          KexAlgorithms = "curve25519-sha256@libssh.org,diffie-hellman-group-exchange-sha256";
+          MACs = "hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com";
+        };
       };
 
     };
-
-    extraConfig = ''
-      Include ${config.my.user.home}/.config/ssh/config.local
-
-      AddKeysToAgent yes
-      AddressFamily inet
-      ChallengeResponseAuthentication no
-      IdentityFile ${identityFile}
-      PasswordAuthentication no
-      StrictHostKeyChecking ask
-      VerifyHostKeyDNS yes
-
-      Ciphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com
-      HostKeyAlgorithms ssh-ed25519-cert-v01@openssh.com,ssh-rsa-cert-v01@openssh.com,ssh-ed25519,ssh-rsa
-      KexAlgorithms curve25519-sha256@libssh.org,diffie-hellman-group-exchange-sha256
-      MACs hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com
-    '';
   };
 }
+
+
+## References:
+# https://github.com/drduh/config/blob/master/ssh_config
+# https://linux.die.net/man/5/ssh_config
