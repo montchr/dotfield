538661d09d3f36149312669b5d93dac27165ac8c
Chris Montgomery <chris@alley.co>
2021-12-01 14:20:39 -0500

    refactor: clean up dotfield module types and variables

--- modules/dotfield.nix
+++ modules/dotfield.nix
@@ -1,26 +1,26 @@
 { config, lib, pkgs, ... }:
 
 let
-  inherit (pkgs.lib.our) mkOpt mkOpt' mkBoolOpt;
-
-  t = with lib.types; either str path;
+  inherit (pkgs.lib.our) mkOpt;
+  dir = (toString ../.);
 in
 
 {
   options = {
-    dotfield = rec {
-      configDir = mkOpt t "${config.dotfield.dir}/config";
-      dir = mkOpt t (toString ../.);
+    dotfield = with lib.types; {
+      configDir = mkOpt path "${dir}/config";
+      dir = mkOpt path dir;
+      binDir = mkOpt path "${dir}/bin";
+      libDir = mkOpt path "${dir}/lib";
+      modulesDir = mkOpt path "${dir}/modules";
+      vendorDir = mkOpt path "${dir}/vendor";
+
       # FIXME: This points to an arbitrary location which may vary per system.
       # Instead, it should be determined programmatically based on the flake's
       # actual location. Note, however, that its currently only useful for
       # out-of-store symlinks, which are generally discouraged as they do not
       # adhere to the Nix principle of immutable configuration.
-      path = mkOpt t "${config.my.user.home}/.config/dotfield";
-      binDir = mkOpt t "${config.dotfield.dir}/bin";
-      libDir = mkOpt t "${config.dotfield.dir}/lib";
-      modulesDir = mkOpt t "${config.dotfield.dir}/modules";
-      vendorDir = mkOpt t "${config.dotfield.dir}/vendor";
+      path = mkOpt path "${config.my.user.home}/.config/dotfield";
     };
   };
 }
