14a25fce37cfc97cbc72a205346282ee95eedc0c
Chris Montgomery <chris@alley.co>
2021-12-02 14:42:02 -0500

    feat(espanso): restore personal and work expansions via age encryption

--- users/profiles/espanso/default.nix
+++ users/profiles/espanso/default.nix
@@ -1,12 +1,28 @@
 { config, lib, pkgs, ... }:
 
 let
-  configDir = "${config.dotfield.configDir}/espanso";
+  configSrc = "${config.dotfield.configDir}/espanso";
+  matchesSrc = "${configSrc}/match";
+  matchesDest = "${config.my.xdg.config}/espanso/match";
+
+  mkMatchesSecret = name: {
+    "espanso-${name}" = {
+      file = "${matchesSrc}/${name}.yml.age";
+      path = "${matchesDest}/${name}.yml";
+      owner = config.my.user.name;
+    };
+  };
 in
 
 {
   my.hm.xdg.configFile."espanso" = {
-    source = configDir;
+    source = configSrc;
     recursive = true;
   };
+
+  age.secrets = lib.mkMerge (builtins.map mkMatchesSecret [
+    "personal"
+    "work"
+  ]);
+
 }
