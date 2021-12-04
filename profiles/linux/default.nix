0c74e68352460667783720e10451f7fa661be276
Chris Montgomery <chris@alley.co>
2021-12-04 01:48:52 -0500

    tweak(linux): enable openssh and earlyoom services

--- profiles/linux/default.nix
+++ profiles/linux/default.nix
@@ -8,4 +8,14 @@
     usbutils
     utillinux
   ];
+
+  # For rage encryption, all hosts need a ssh key pair
+  services.openssh = {
+    enable = true;
+    openFirewall = lib.mkDefault false;
+  };
+
+  # Automatically kill processes when running low on available memory.
+  # https://github.com/rfjakob/earlyoom
+  services.earlyoom.enable = true;
 }
