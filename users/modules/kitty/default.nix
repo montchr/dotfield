42a116b95b96eddd5632a60f8fdc32104fa6e3f4
Chris Montgomery <chris@alley.co>
2021-12-16 22:09:30 -0500

    tweak(kitty): move color module to toplevel `kitty.colors`

--- users/modules/kitty/default.nix
+++ users/modules/kitty/default.nix
@@ -1,52 +1,50 @@
-{ pkgs, lib, config, options, inputs, ... }:
+{ pkgs, lib, config, options, ... }:
+
+with lib;
 
 {
-  options = {
-    # colors = {
-    #   enable = mkOption {
-    #     type = types.bool;
-    #     default = false;
-    #     description = ''
-    #       When enabled, commands <command>term-dark</command> and <command>term-light</command> will
-    #       toggle between dark and light colorschemes.
-    #       <command>term-background</command> which accepts one argument (the value of which should
-    #       be <literal>dark</literal> or <literal>light</literal>) is also avaible.
-    #       (Note that the Kitty setting <literal>allow_remote_control = true</literal> is set to
-    #       enable this functionality.)
-    #     '';
-    #   };
+  # options.kitty.colors = with types; {
+  #   enable = mkEnableOption {
+  #     description = ''
+  #       When enabled, commands <command>term-dark</command> and <command>term-light</command> will
+  #       toggle between dark and light colorschemes.
+  #       <command>term-background</command> which accepts one argument (the value of which should
+  #       be <literal>dark</literal> or <literal>light</literal>) is also avaible.
+  #       (Note that the Kitty setting <literal>allow_remote_control = true</literal> is set to
+  #       enable this functionality.)
+  #     '';
+  #   };
 
-    #   dark = mkOption {
-    #     type = with types; attrsOf str;
-    #     default = { };
-    #     description = ''
-    #       Kitty color settings for dark background colorscheme.
-    #     '';
-    #   };
+  #   dark = mkOption {
+  #     type = attrsOf str;
+  #     default = { };
+  #     description = ''
+  #       Kitty color settings for dark background colorscheme.
+  #     '';
+  #   };
 
-    #   light = mkOption {
-    #     type = with types; attrsOf str;
-    #     default = { };
-    #     description = ''
-    #       Kitty color settings for light background colorscheme.
-    #     '';
-    #   };
+  #   light = mkOption {
+  #     type = attrsOf str;
+  #     default = { };
+  #     description = ''
+  #       Kitty color settings for light background colorscheme.
+  #     '';
+  #   };
 
-    #   common = mkOption {
-    #     type = with types; attrsOf str;
-    #     default = { };
-    #     description = ''
-    #       Kitty color settings that the light and dark background colorschemes share.
-    #     '';
-    #   };
+  #   common = mkOption {
+  #     type = attrsOf str;
+  #     default = { };
+  #     description = ''
+  #       Kitty color settings that the light and dark background colorschemes share.
+  #     '';
+  #   };
 
-    #   default = mkOption {
-    #     type = types.enum [ "dark" "light" ];
-    #     default = "dark";
-    #     description = ''
-    #       The colorscheme Kitty opens with.
-    #     '';
-    #   };
-    # };
-  };
+  #   default = mkOption {
+  #     type = enum [ "dark" "light" ];
+  #     default = "dark";
+  #     description = ''
+  #       The colorscheme Kitty opens with.
+  #     '';
+  #   };
+  # };
 }
