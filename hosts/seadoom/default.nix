e4c7938876bfda1b457783daf88b865f1038e79b
Chris Montgomery <chris@cdom.io>
2022-02-22 00:56:57 -0500

    fix(seadoom): users need to exist oops

--- hosts/seadoom/default.nix
+++ hosts/seadoom/default.nix
@@ -31,7 +31,25 @@
 
   time.timeZone = "America/New_York";
 
-  services.openssh.permitRootLogin = "no";
+  users.users.xtallos = {
+    isNormalUser = true;
+    hashedPassword = "$6$yq7jJybfGyx19QqK$mr1dfKu1fChKkYDUZvQnlcKCmAYywIvWZXw3uT9EjQ/Xi85SGqkPDcsrrQ.7WEYM6InqDPqGZrTGfvoFpuONi1";
+    extraGroups = [ "wheel" "network-manager" ]; # Enable ‘sudo’ for the user.
+  };
+  home-manager.users.xtallos = { pkgs, ... }: {
+    home.packages = with pkgs; [
+      ddate
+    ];
+    programs.fish.enable = true;
+  };
+  home-manager.useUserPackages = true;
+  home-manager.useGlobalPkgs = true;
+
+  users.users.root.hashedPassword = "$6$OZwXf5o.yZUoehTw$29dItBi5.fjd2GZToR6sP2F.xEUn/5TBKZlZ6CHNazEAWz6roIevLATX82QV9rCxlZ21sL9zsW..LmTFhC/dx.";
+
+  users.mutableUsers = false;
+
+  services.openssh.permitRootLogin = "yes";
   services.openssh.openFirewall = true;
 
   system.stateVersion = "21.05";
