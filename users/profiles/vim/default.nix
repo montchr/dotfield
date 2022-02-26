4d8e24e544b5995c996f91b865576e964e80baad
Chris Montgomery <chris@cdom.io>
2022-02-26 15:15:49 -0500

    feat(vim|lunarvim): add lunarvim configuration and deps

--- users/profiles/vim/default.nix
+++ users/profiles/vim/default.nix
@@ -1,10 +1,29 @@
 { config, lib, pkgs, ... }:
 
 {
+  my.env = {
+    LUNARVIM_RUNTIME_DIR = "$XDG_DATA_HOME/lunarvim";
+    LUNARVIM_CONFIG_DIR = "$XDG_CONFIG_HOME/lvim";
+    LUNARVIM_CACHE_DIR = "$XDG_CACHE_HOME/nvim";
+  };
+
   my.user.packages = with pkgs; [
     neovim-unwrapped
 
-    # LunarVim dependencies
+    ##: LunarVim dependencies {{
+
+    #: core
     cargo
+    fd
+    ripgrep
+
+    #: nodejs
+    nodePackages.neovim
+    tree-sitter
+
+    #: python
+    python39Packages.pynvim
+
+    #: }}
   ];
 }
