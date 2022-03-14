54bd754082f5fe99c4e4da0e2f2513f7a121f7c7
Chris Montgomery <chris@cdom.io>
2022-03-14 10:23:11 -0400

    chore: use `git-cliff` v0.6.0 from unstable channel
    no longer necessary to pull from trunk

--- overlays/nixpkgs-darwin-stable/overrides.nix
+++ overlays/nixpkgs-darwin-stable/overrides.nix
@@ -1,13 +1,10 @@
 channels: final: prev: {
   inherit (channels.nixpkgs-unstable)
     direnv
+    git-cliff
     neovim
     neovim-unwrapped
     nix
     nix-direnv
-    ;
-  inherit (channels.nixpkgs-trunk)
-    # TODO: inherit from unstable when v0.6.0 is available there
-    git-cliff
     ;
 }
