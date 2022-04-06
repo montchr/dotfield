75c40a59e6ae973a17d35d79b9166cb01ea0f531
Chris Montgomery <chris@cdom.io>
2022-04-06 10:17:16 -0400

    fix(fish): make profile a noop b/c not currently in use

--- users/profiles/fish/default.nix
+++ users/profiles/fish/default.nix
@@ -1,3 +1,5 @@
+# FIXME: known to be broken, but not tough to fix
+
 {
   config,
   lib,
@@ -5,51 +7,51 @@
   ...
 }: let
 
-  shellCfg = config.shell;
+  # shellCfg = config.shell;
 
-  mkPlugins = plugins: (map
-    (name: {
-      inherit name;
-      inherit (pkgs.sources."fish-${name}") src;
-    })
-    plugins);
+  # mkPlugins = plugins: (map
+  #   (name: {
+  #     inherit name;
+  #     inherit (pkgs.sources."fish-${name}") src;
+  #   })
+  #   plugins);
 in {
-  imports = [
-    ../shell
-  ];
+  # imports = [
+  #   ../shell
+  # ];
 
-  my.hm.programs = {
-    fish = {
-      enable = true;
-      interactiveShellInit = ''
-        set -g fish_greeting
-      '';
-      shellInit = ''
-        for p in /run/current-system/sw/bin
-          if not contains $p $fish_user_paths
-            set -U fish_user_paths $p $fish_user_paths
-          end
-        end
+  # my.hm.programs = {
+  #   fish = {
+  #     enable = true;
+  #     interactiveShellInit = ''
+  #       set -g fish_greeting
+  #     '';
+  #     shellInit = ''
+  #       for p in /run/current-system/sw/bin
+  #         if not contains $p $fish_user_paths
+  #           set -U fish_user_paths $p $fish_user_paths
+  #         end
+  #       end
 
-        set -U fish_user_paths /run/wrappers/bin $fish_user_paths
+  #       set -U fish_user_paths /run/wrappers/bin $fish_user_paths
 
-        function fish_title
-          echo "$PWD | $_" | sed "s|$HOME|~|g"
-        end
-      '';
-      shellAbbrs = shellCfg.abbrs;
-      shellAliases = shellCfg.aliases;
-      plugins = mkPlugins [
-        "abbr-tips"
-        "autopair"
-        "done"
-        "fzf"
-        "nix-env"
-        "nvm"
-        "replay"
-        "z"
-      ];
-    };
-    starship.enableFishIntegration = true;
-  };
+  #       function fish_title
+  #         echo "$PWD | $_" | sed "s|$HOME|~|g"
+  #       end
+  #     '';
+  #     shellAbbrs = shellCfg.abbrs;
+  #     shellAliases = shellCfg.aliases;
+  #     plugins = mkPlugins [
+  #       "abbr-tips"
+  #       "autopair"
+  #       "done"
+  #       "fzf"
+  #       "nix-env"
+  #       "nvm"
+  #       "replay"
+  #       "z"
+  #     ];
+  #   };
+  #   starship.enableFishIntegration = true;
+  # };
 }
