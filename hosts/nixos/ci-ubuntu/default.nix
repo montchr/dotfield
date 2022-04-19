a0274314e26e18c1146a401b02f80f30408fa226
Chris Montgomery <chris@cdom.io>
2022-04-19 19:51:19 -0400

    fix(hosts): set boot and filesystem options for all linux hosts
    checks fail otherwise. but these options shouldn't even work on generic
    linux, right?

--- hosts/nixos/ci-ubuntu/default.nix
+++ hosts/nixos/ci-ubuntu/default.nix
@@ -6,4 +6,11 @@
   ...
 }: {
   imports = suites.minimal;
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # networking.networkmanager.enable = true;
+
+  fileSystems."/" = {device = "/dev/disk/by-label/nixos";};
 }
--- hosts/nixos/onceler/default.nix
+++ hosts/nixos/onceler/default.nix
@@ -7,13 +7,19 @@
   hmUsers,
   ...
 }: {
-  # Note that these are system-level suites, not hm suites.
   imports = with suites;
     gui
     ++ personal
     ++ (with profiles; [
       users.xtallos
     ]);
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.networkmanager.enable = true;
+
+  fileSystems."/" = {device = "/dev/disk/by-label/nixos";};
 
   home-manager.users.xtallos = {suites, ...}: {
     imports = [hmUsers.xtallos] ++ suites.gui;
