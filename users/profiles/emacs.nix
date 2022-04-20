2461ed81bfb3a523b585f9ed9a81d8cda8bc4885
Chris Montgomery <chris@cdom.io>
2022-04-19 23:27:20 -0400

    fix(emacs|gpg): remove pinentry_emacs pkg b/c collision

--- users/profiles/emacs.nix
+++ users/profiles/emacs.nix
@@ -73,7 +73,6 @@ in {
     fd # faster projectile indexing
     imagemagick # for image-dired
     zstd # for undo-fu-session/undo-tree compression
-    (lib.optionals config.services.gpg-agent.enable pinentry_emacs)
 
     ## Module dependencies
     # :checkers spell
