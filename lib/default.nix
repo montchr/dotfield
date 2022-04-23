c44dbb5bdec3013a36f8425b77465a02499c46a0
Chris Montgomery <chris@cdom.io>
2022-04-23 19:35:33 -0400

    chore: remove some unused lib functions

--- lib/default.nix
+++ lib/default.nix
@@ -2,12 +2,6 @@
 lib.makeExtensible (self: {
   dotfieldPath = toString ../.;
 
-  isDarwin = system: (builtins.elem system lib.platforms.darwin);
-
-  homePrefix = system:
-    if self.isDarwin system
-    then "/Users"
-    else "/home";
 
   mkOpt = type: default: lib.mkOption {inherit type default;};
 
@@ -21,7 +15,3 @@ lib.makeExtensible (self: {
       example = true;
     };
 })
-##: sources
-#
-# https://github.com/kclejeune/system/blob/71c65173e7eba8765a3962df5b52c2f2c25a8fac/flake.nix#L59-L60
-
