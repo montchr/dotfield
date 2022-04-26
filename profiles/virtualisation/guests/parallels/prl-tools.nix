31b08692dc12bdd7c7665c21cb55274d48e39cc6
Chris Montgomery <chris@cdom.io>
2022-04-26 00:51:07 -0400

    fix(parallels): syntax errors in prl-tools

--- profiles/virtualisation/guests/parallels/prl-tools.nix
+++ profiles/virtualisation/guests/parallels/prl-tools.nix
@@ -3,17 +3,26 @@
 #
 ## Sources:
 #
+# https://github.com/Builditluc/dotnix/blob/d5cd6906308b55c4e5548779ff4ba86e138f708e/hosts/prlnix/prl-tools.nix
 # https://github.com/NixOS/nixpkgs/pull/153665/files
 # https://github.com/NixOS/nixpkgs/blob/d675eefdb0b4a0ad35caa5bd773b0dc01a3e72d9/pkgs/os-specific/linux/prl-tools/default.nix
-# https://github.com/Builditluc/dotnix/tree/d5cd6906308b55c4e5548779ff4ba86e138f708e/hosts/prlnix
 
-{ config, lib, pkgs, ... }:
-
-{
-{ stdenv, lib, makeWrapper, p7zip
-, gawk, util-linux, xorg, glib, dbus-glib, zlib
-, kernel ? null, libsOnly ? false
-, fetchurl, undmg, perl, autoPatchelfHook
+{ stdenv
+, lib
+, makeWrapper
+, p7zip
+, gawk
+, util-linux
+, xorg
+, glib
+, dbus-glib
+, zlib
+, kernel ? null
+, libsOnly ? false
+, fetchurl
+, undmg
+, perl
+, autoPatchelfHook
 }:
 
 assert (!libsOnly) -> kernel != null;
@@ -22,19 +31,17 @@ let
   aarch64 = stdenv.hostPlatform.system == "aarch64-linux";
   x86_64 = stdenv.hostPlatform.system == "x86_64-linux";
   i686 = stdenv.hostPlatform.system == "i686-linux";
-  _ = if (aarch64 || x86_64 || i686) then true
-      else throw "Parallels Tools for Linux only supports {aarch64,x86_64,i686}-linux targets";
 in
 stdenv.mkDerivation rec {
-  version = "${prl_major}.1.1-51537";
+  version = "${prl_major}.1.2-51548";
   prl_major = "17";
   pname = "prl-tools";
 
   # We download the full distribution to extract prl-tools-lin.iso from
   # => ${dmg}/Parallels\ Desktop.app/Contents/Resources/Tools/prl-tools-lin.iso
   src = fetchurl {
-    url =  "https://download.parallels.com/desktop/v${prl_major}/${version}/ParallelsDesktop-${version}.dmg";
-    sha256 = "1ab5jwbg3jgvfwx8kwxwhhrsdp9wz4g9q7fg4z0hhd8v9pgy4yxx";
+    url = "https://download.parallels.com/desktop/v${prl_major}/${version}/ParallelsDesktop-${version}.dmg";
+    sha256 = "sha256-OyfjFPiaEjFWi3/RxX+lgRoXn1aFH3mEC/jr/pcHsqw=";
   };
 
   hardeningDisable = [ "pic" "format" ];
@@ -49,17 +56,16 @@ stdenv.mkDerivation rec {
 
   inherit libsOnly;
 
-  unpackPhase = ''
+  unpackPhase = assert (aarch64 || x86_64 || i686); ''
     undmg "${src}"
-
     export sourceRoot=prl-tools-build
-    7z x "Parallels Desktop.app/Contents/Resources/Tools/prl-tools-lin${if aarch64 then "-arm" else ""}.iso" -o$sourceRoot
+    7z x "Parallels Desktop.app/Contents/Resources/Tools/prl-tools-lin${lib.optionalString aarch64 "-arm"}.iso" -o$sourceRoot
     if test -z "$libsOnly"; then
       ( cd $sourceRoot/kmods; tar -xaf prl_mod.tar.gz )
     fi
-    # TODO
   '';
-    #( cd $sourceRoot/tools; tar -xaf prltools${if x64 then ".x64" else ""}.tar.gz )
+
+  patches = if lib.versionAtLeast kernel.version "5.17" then [ ./prl-tools.patch ] else [ ];
 
   kernelVersion = if libsOnly then "" else lib.getVersion kernel.name;
   kernelDir = if libsOnly then "" else "${kernel.dev}/lib/modules/${kernelVersion}";
@@ -86,8 +92,9 @@ stdenv.mkDerivation rec {
         mkdir -p $out/lib/modules/${kernelVersion}/extra
         cp prl_fs/SharedFolders/Guest/Linux/prl_fs/prl_fs.ko $out/lib/modules/${kernelVersion}/extra
         cp prl_fs_freeze/Snapshot/Guest/Linux/prl_freeze/prl_fs_freeze.ko $out/lib/modules/${kernelVersion}/extra
-        ${if aarch64 then "cp prl_notifier/Installation/lnx/prl_notifier/prl_notifier.ko $out/lib/modules/${kernelVersion}/extra" else ""}
         cp prl_tg/Toolgate/Guest/Linux/prl_tg/prl_tg.ko $out/lib/modules/${kernelVersion}/extra
+        ${lib.optionalString aarch64
+        "cp prl_notifier/Installation/lnx/prl_notifier/prl_notifier.ko $out/lib/modules/${kernelVersion}/extra"}
       )
     fi
 
@@ -106,8 +113,9 @@ stdenv.mkDerivation rec {
         wrapProgram $out/sbin/prlfsmountd \
           --prefix PATH ':' "$scriptPath"
 
-        for i in lib/*.0.0; do
+        for i in lib/libPrl*.0.0; do
           cp $i $out/lib
+          ln -s $out/$i $out/''${i%.0.0}
         done
 
         mkdir -p $out/share/man/man8
@@ -116,16 +124,6 @@ stdenv.mkDerivation rec {
         mkdir -p $out/etc/pm/sleep.d
         install -Dm644 ../99prltoolsd-hibernate $out/etc/pm/sleep.d
       fi
-
-      cd $out/lib
-      ln -s libPrlWl.so.1.0.0 libPrlWl.so.1
-      ${if aarch64 then "" else "
-      ln -s libGL.so.1.0.0 libGL.so
-      ln -s libGL.so.1.0.0 libGL.so.1
-      ln -s libPrlDRI.so.1.0.0 libPrlDRI.so.1
-      ln -s libEGL.so.1.0.0 libEGL.so.1
-      ln -s libgbm.so.1.0.0 libgbm.so.1
-      "}
     )
   '';
 
@@ -135,5 +133,4 @@ stdenv.mkDerivation rec {
     platforms = [ "aarch64-linux" "i686-linux" "x86_64-linux" ];
     license = licenses.unfree;
   };
-}
 }
