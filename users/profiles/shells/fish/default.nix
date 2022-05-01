67c2c839ad9dbc9a5a5f05b8bcdca00d8ef0174d
Chris Montgomery <chris@cdom.io>
2022-04-30 20:18:50 -0400

    tweak(fish): add some basic plugins from nixpkgs

--- users/profiles/shells/fish/default.nix
+++ users/profiles/shells/fish/default.nix
@@ -6,29 +6,30 @@
 }: let
   shellAbbrs = import ../abbrs.nix;
   shellAliases = import ../aliases.nix;
-  # mkPlugin = name: {
-  #   inherit name;
-  #   inherit (pkgs.sources."fish-${name}") src;
-  # };
-  # mkPlugins = plugins: (map mkPlugin plugins);
+
+  mkPlugin = name: {
+    inherit name;
+    inherit (pkgs.sources."fish-${name}") src;
+  };
+  mkPlugins = plugins: (map mkPlugin plugins);
 in {
   imports = [../common.nix];
+
+  home.packages = with pkgs; [
+    fishPlugins.done
+    fishPlugins.forgit
+    fishPlugins.fzf-fish
+    fishPlugins.pisces
+  ];
 
   programs.fish = {
     inherit shellAbbrs shellAliases;
     # inherit shellAliases;
 
     enable = true;
-
-    # plugins = mkPlugins [
-    #   "abbr-tips"
-    #   "autopair"
-    #   "done"
-    #   "fzf"
-    #   "nix-env"
-    #   "nvm"
-    #   "replay"
-    #   "z"
-    # ];
+    plugins = mkPlugins [
+      # "nvm"
+      # "replay"
+    ];
   };
 }
