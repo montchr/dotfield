d3f4ef4d684c95154097982fbbd76e6eef65525c
Chris Montgomery <chris@cdom.io>
2022-05-01 18:44:32 -0400

    feat(secrets|pass|firefox): enable browserpass extension

--- users/profiles/secrets.nix
+++ users/profiles/secrets.nix
@@ -4,29 +4,34 @@
   pkgs,
   ...
 }: let
-  # inherit (pkgs.lib.our) dotfieldPath;
   inherit (config.lib.dotfield.whoami) pgpPublicKey;
 
   dotfieldPath = "${config.xdg.configHome}/dotfield";
-in {
-  home.packages = with pkgs; [
-    yubikey-manager
-    yubikey-personalization
-  ];
+in lib.mkMerge [
+  {
+    home.packages = with pkgs; [
+      yubikey-manager
+      yubikey-personalization
+    ];
 
-  home.sessionVariables.AGENIX_ROOT = dotfieldPath;
+    home.sessionVariables.AGENIX_ROOT = dotfieldPath;
 
-  programs.password-store = lib.mkIf config.programs.gpg.enable {
-    enable = true;
-    package = pkgs.pass.withExtensions (exts:
-      with exts; [
-        pass-import # https://github.com/roddhjav/pass-import
-        pass-otp # https://github.com/tadfisher/pass-otp
-        pass-update # https://github.com/roddhjav/pass-update
-      ]);
-    settings = {
-      PASSWORD_STORE_DIR = "${config.xdg.dataHome}/pass";
-      PASSWORD_STORE_KEY = "${pgpPublicKey} 0xF0B8FB42A7498482";
+    programs.password-store = lib.mkIf config.programs.gpg.enable {
+      enable = true;
+      package = pkgs.pass.withExtensions (exts:
+        with exts; [
+          pass-import # https://github.com/roddhjav/pass-import
+          pass-otp # https://github.com/tadfisher/pass-otp
+          pass-update # https://github.com/roddhjav/pass-update
+        ]);
+      settings = {
+        PASSWORD_STORE_DIR = "${config.xdg.dataHome}/pass";
+        PASSWORD_STORE_KEY = "${pgpPublicKey} 0xF0B8FB42A7498482";
+      };
     };
-  };
-}
+  }
+  (lib.mkIf (config.programs.password-store.enable && config.programs.firefox.enable) {
+    programs.browserpass.enable = true;
+    programs.browserpass.browsers = ["firefox"];
+  })
+]
