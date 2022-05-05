927b3d4f755a38b33969a55fb85107f3f9a58f83
Chris Montgomery <chris@cdom.io>
2022-05-04 21:58:34 -0400

    chore(HodgePodge): config cleanup

---
+++ hosts/nixos/HodgePodge/broadcom.nix
@@ -0,0 +1,11 @@
+{config, lib, pkgs, ...}:
+{
+  # Required for broadcom driver support.
+  nixpkgs.config.allowUnfree = true;
+  hardware.enableRedistributableFirmware = true;
+  hardware.enableAllFirmware = true;
+
+  environment.systemPackages = with pkgs; [
+    linuxPackages.broadcom_sta
+  ];
+}
--- hosts/nixos/HodgePodge/default.nix
+++ hosts/nixos/HodgePodge/default.nix
@@ -1,8 +1,11 @@
 {
   config,
+  lib,
   inputs,
+  pkgs,
   suites,
   profiles,
+  hmUsers,
   ...
 }: {
   imports =
@@ -13,5 +16,74 @@
     ++ (with profiles; [
       users.xtallos
     ])
-    ++ [./configuration.nix];
+    ++ [
+      ./broadcom.nix
+      ./hardware-configuration.nix
+    ];
+
+  users.mutableUsers = false;
+  users.users.root.hashedPassword = "$6$OZwXf5o.yZUoehTw$29dItBi5.fjd2GZToR6sP2F.xEUn/5TBKZlZ6CHNazEAWz6roIevLATX82QV9rCxlZ21sL9zsW..LmTFhC/dx.";
+  users.users.xtallos = {
+    hashedPassword = "$6$yq7jJybfGyx19QqK$mr1dfKu1fChKkYDUZvQnlcKCmAYywIvWZXw3uT9EjQ/Xi85SGqkPDcsrrQ.7WEYM6InqDPqGZrTGfvoFpuONi1";
+    extraGroups = ["network-manager"];
+  };
+
+  home-manager.users.xtallos = {pkgs, ...}: {
+    imports = [hmUsers.xtallos];
+    home.packages = with pkgs; [
+      ddate
+      _1password
+      kitty
+    ];
+  };
+
+  environment.systemPackages = with pkgs; [
+    firefox
+    glxinfo
+    kate
+    konsole
+    ripgrep
+    tldr
+    vim
+    wget
+    xsel
+    # wirelesstools
+    # networkmanagerapplet
+  ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.supportedFilesystems = ["btrfs"];
+
+  boot.cleanTmpDir = true;
+
+  networking.interfaces = {
+    enp0s20u1.useDHCP = true;
+    wlp3s0.useDHCP = true;
+    # TODO: these are probably invalid
+    ens9.useDHCP = true;
+    ens0.useDHCP = true;
+  };
+  networking.wireless = {
+    enable = true;
+    interfaces = ["wlp3s0"];
+    userControlled.enable = true;
+  };
+  networking.firewall.enable = false;
+
+  console = {
+    # Large font size for use on HiDPI screen
+    font = "ter-i32b";
+    packages = with pkgs; [terminus_font];
+    keyMap = "us";
+  };
+
+  # videoDrivers = [ "nvidia" ];
+
+  hardware.video.hidpi.enable = true;
+
+  # See https://en.wikipedia.org/wiki/Retina_display
+  services.xserver.dpi = 221;
 }
