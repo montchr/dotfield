09dc410153e5e7884263ecc8a6ffd9fdc4b09155
Chris Montgomery <chris@cdom.io>
2022-05-04 21:57:57 -0400

    tweak(ui|fonts): add some common linux fonts

--- profiles/fonts/common.nix
+++ profiles/fonts/common.nix
@@ -6,16 +6,29 @@
 }: {
   fonts = {
     fontDir.enable = true;
-    fonts = with pkgs; [
-      b612
-      barlow
-      fira
-      ibm-plex
-      inter
-      jost
-      public-sans
-      recursive
-      sf-pro
-    ];
+    fonts = with pkgs;
+      [
+        b612
+        barlow
+        fira
+        ibm-plex
+        inter
+        jost
+        public-sans
+        recursive
+      ]
+      ++ (lib.optionals stdenv.hostPlatform.isLinux [
+        corefonts
+        inconsolata
+        liberation_ttf
+        dejavu_fonts
+        bakoma_ttf
+        gentium
+        ubuntu_font_family
+        terminus_font
+      ])
+      ++ (lib.optionals stdenv.hostPlatform.isMacOS [
+        sf-pro
+      ]);
   };
 }
