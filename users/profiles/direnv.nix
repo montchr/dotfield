44fb0fce9c0a651b16c7656465ea02438ccbd466
Chris Montgomery <chris@cdom.io>
2022-05-06 19:29:10 -0400

    fix(zsh|direnv): allow direnv integration

--- users/profiles/direnv.nix
+++ users/profiles/direnv.nix
@@ -7,7 +7,5 @@
   programs.direnv = {
     enable = true;
     nix-direnv.enable = true;
-    # Prevent early loading of direnv integration for a faster shell startup.
-    enableZshIntegration = false;
   };
 }
