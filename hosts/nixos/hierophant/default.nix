8685b6a1e728e2d7af3a2a0944ff451ed61db6c9
Chris Montgomery <chris@cdom.io>
2022-05-10 10:36:26 -0400

    hierophant: move hetzner cloud configuration to profile

--- hosts/nixos/hierophant/default.nix
+++ hosts/nixos/hierophant/default.nix
@@ -17,36 +17,13 @@ in
   imports =
     (with suites; basic)
     ++ (with profiles; [
+      environments.hetzner-cloud
       networking.tailscale
       users.seadoom
-    ])
-    ++ [
-      (modulesPath + "/profiles/qemu-guest.nix")
-    ];
+    ]);
 
-  # Use the GRUB 2 boot loader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.version = 2;
-  boot.loader.grub.devices = ["/dev/sda"];
-
-  networking.useDHCP = false;
-  networking.interfaces.enp1s0.useDHCP = true;
   networking.firewall.enable = false;
   networking.firewall.trustedInterfaces = [ "enp1s0" ];
-
-  boot.initrd.availableKernelModules = [ "ahci" "xhci_pci" "virtio_pci" "sd_mod" "sr_mod" ];
-  boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ ];
-  boot.extraModulePackages = [ ];
-
-  fileSystems."/" = {
-    # N.B. While nixos-generate-config will set this to a UUID by default, the
-    # UUID of a disk on Hetzner Cloud appears to change if the server is
-    # rebuilt. We know the root filesystem should always point to this
-    # partition, so it's safer to point directly there.
-    device = "/dev/sda1";
-    fsType = "ext4";
-  };
 
   fileSystems."/nix" = {
     device = "/dev/disk/by-id/scsi-0HC_Volume_19315958";
@@ -55,17 +32,9 @@ in
     options = [ "noatime" ];
   };
 
-  swapDevices = [ ];
-
-  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-
   environment.variables.DOTFIELD_DIR = "/etc/dotfield";
   programs.mtr.enable = true; 
   programs.gnupg.agent.enable = true;
-
-  services.openssh.enable = true;
-  services.openssh.openFirewall = true;
-  services.openssh.permitRootLogin = "prohibit-password";
 
   security.sudo.enable = true;
   security.sudo.wheelNeedsPassword = false;
