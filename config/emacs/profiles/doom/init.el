ba520ccd2aef78850b5f436b40606d15e503314e
Chris Montgomery <chris@cdom.io>
2022-05-13 11:24:40 -0400

    doom: re-enable file-templates module

--- config/emacs/profiles/doom/init.el
+++ config/emacs/profiles/doom/init.el
@@ -50,7 +50,7 @@
 
   :editor
   (evil +everywhere) ; come to the dark side, we have cookies
-  ;; file-templates     ; auto-snippets for empty files
+  file-templates     ; auto-snippets for empty files
   fold               ; (nigh) universal code folding
   ;; (format)   ; automated prettiness
   ;;god              ; run Emacs commands without modifier keys
