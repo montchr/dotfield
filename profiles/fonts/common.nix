28ca3f0d83f9854a59556425641b4e4f7feaff44
Chris Montgomery <chris@cdom.io>
2022-05-13 21:14:12 -0400

    fonts: set default fonts

--- profiles/fonts/common.nix
+++ profiles/fonts/common.nix
@@ -5,40 +5,65 @@
   ...
 }: let
   inherit (pkgs.stdenv.hostPlatform) isLinux isMacOS;
-in {
-  environment.systemPackages = with pkgs; [
-    (lib.mkIf isLinux font-manager)
-  ];
+  # inherit (config.theme) fonts;
+in
+  lib.mkMerge [
+    {
+      environment.systemPackages = with pkgs; [
+        (lib.mkIf isLinux font-manager)
+      ];
 
-  fonts = {
-    fontDir.enable = true;
-    fonts = with pkgs;
-      [
-        b612
-        barlow
-        fira
-        fira-code
-        fira-code-symbols
-        ibm-plex
-        inter
-        jost
-        material-design-icons
-        material-icons
-        nerdfonts
-        public-sans
-      ]
-      ++ (lib.optionals isLinux [
-        corefonts
-        inconsolata
-        liberation_ttf
-        dejavu_fonts
-        bakoma_ttf
-        gentium
-        ubuntu_font_family
-        terminus_font
-      ])
-      ++ (lib.optionals isMacOS [
+      fonts = {
+        fontDir.enable = true;
+        fonts = with pkgs; [
+          b612
+          barlow
+          fira
+          fira-code
+          fira-code-symbols
+          ibm-plex
+          inter
+          jost
+          material-design-icons
+          material-icons
+          nerdfonts
+          public-sans
+        ];
+      };
+    }
+
+    (lib.mkIf isLinux {
+      fonts = {
+        fonts = with pkgs; [
+          corefonts
+          inconsolata
+          liberation_ttf
+          dejavu_fonts
+          bakoma_ttf
+          gentium
+          ubuntu_font_family
+          terminus_font
+        ];
+        fontconfig = {
+          enable = lib.mkForce true;
+          defaultFonts = {
+            monospace = ["PragmataPro Liga 16"];
+            sansSerif = ["IBM Plex Sans 14"];
+            serif = ["IBM Plex Serif 14"];
+          };
+          # defaultFonts = {
+          #   monospace = [ "${fonts.mono.family} ${toString fonts.mono.size}" ];
+          #   sansSerif = [ "${fonts.main.family} ${toString fonts.main.size}" ];
+          #   serif = [ "${fonts.serif.family} ${toString fonts.serif.size}" ];
+          # };
+        };
+        enableDefaultFonts = true;
+      };
+    })
+
+    (lib.mkIf isMacOS {
+      fonts.fonts = with pkgs; [
         sf-pro
-      ]);
-  };
-}
+      ];
+    })
+  ]
