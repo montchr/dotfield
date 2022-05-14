0291c9155a56c294edad776464e77b67155c8946
Chris Montgomery <chris@cdom.io>
2022-05-14 04:08:07 -0400

    vm: add a 'common' virtualisation profile for nixos

---
+++ profiles/virtualisation/common.nix
@@ -0,0 +1,7 @@
+{ config, pkgs, lib, ... }:
+lib.mkIf pkgs.stdenv.hostPlatform.isLinux {
+  virtualisation.libvirtd.enable = true;
+  environment.systemPackages = with pkgs; [
+    virt-manager
+  ];
+}
