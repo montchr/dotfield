f7ee683bb579bddc0a3c28de6b5efa713afa3589
Chris Montgomery <chris@cdom.io>
2022-05-14 22:16:13 -0400

    chore(bar): remove unused sketchybar configs

--- config/sketchybar/plugins/clock.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-sketchybar -m --set $NAME label="$(date '+%d/%m %H:%M')"
-
--- profiles/os-specific/darwin/gui/sketchybar.nix
+++ profiles/os-specific/darwin/gui/sketchybar.nix
@@ -12,6 +12,4 @@ in {
 
   # The system menu bar must be hidden when using a custom bar.
   system.defaults.NSGlobalDomain._HIHideMenuBar = true;
-
-  # my.hm.xdg.configFile."sketchybar".source = "${pkgs.dotfield-config}/sketchybar";
 }
