f4a84646f7204860c64e709a2a2f95307771d789
Chris Montgomery <chris@cdom.io>
2022-05-16 11:42:09 -0400

    tweak(fish): disable by default

--- profiles/core/default.nix
+++ profiles/core/default.nix
@@ -12,7 +12,6 @@ in {
   nix = {
     package = pkgs.nix;
     gc.automatic = true;
-    # useSandbox = lib.mkDefault true;
     useSandbox = lib.mkDefault (!pkgs.stdenv.hostPlatform.isDarwin);
     allowedUsers = ["*"];
     trustedUsers = ["root" "@wheel"];
@@ -77,7 +76,7 @@ in {
     enableCompletion = true;
   };
 
-  programs.fish.enable = true;
+  programs.fish.enable = lib.mkDefault false;
 
   home-manager = {
     useGlobalPkgs = true;
