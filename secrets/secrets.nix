b894e246831885d15c11a37e53f05cb01ef7d29e
Chris Montgomery <chris@cdom.io>
2022-05-22 17:28:27 -0400

    feat(secrets|boschic): add keys for boschic + rekey secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,7 @@
 let
   machines = {
     alleymon = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJsVn0I6Q0rL94W2V89efhUiffAeJfDtHYcW6czXcPkh";
+    boschic = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJxqOfaPK3FQnBsp7ey3yUEuFnYzAJSF1PdyJLqpKAfK";
     hierophant = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF14I8s1ywGVCmInco+wfCG5C9kJB0Y9HCOA12ZX9cHe";
     hodge = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ8uGFMeCGkqrGiJZU3oVP7h0Xq9jEdACINpjRHqi96r";
     parrothelles = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINBvBCsqtgEdC4J+d1xzrwPIircRYSKbFHR0FulaNV5z";
@@ -10,12 +11,19 @@ let
   trustedUsers = import ./authorized-keys.nix;
 
   alleymonRsa = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDg+L0ma2MoSDA9c+ydaOn+is1UWaMvTJJSdEbTsJ0qn75JyNaVB3c2nIqbu3kTS18z6WDeGkAuMHVwJLZWiL+JKqQuAgvLm3pSH7Wmtfako5T3+3xcw6DKgteVFQC0819Xh4CaOCirDSHBdUk/dqZEi5HuYHxmLmd39crVqAV60csS1LXFNgmPF4QvLj0NUcXJyHEkfSgRHKWwZ2JywN5pURc6/7Vl7b/j+r2nvsWcP4yqtAYjSgnxO+H7QZWZ040eMFNqgcqP+LclI3l0jEMW/SC8ZlqvS9hiICXJ6YLQCZmbRfnFxO1IxZwu97892pV/9ZsdFEiLq/fT2f8rTVZLYe4xLIfSw7BNlV7K9VDyD6PLQ9blKlioser2UjTxB/FH1NdoD7YBDhcMiYg9CfVBM34Q98mB21OwysHHLF7ukZ4Mk5H64y2cuCWRG+ze4CPLu7gK0zTb32eZh9AmUqljZgPs3FL28BJAWwZii4jzV1Hd7KTZ25Pshrk1bCpfAI8=";
-  allMachines = with machines; [
-    alleymon
-    hodge
-    parrothelles
+
+  servers = with machines; [
+    hierophant
     tso-portal
   ];
+  workstations = with machines; [
+    alleymon
+    boschic
+    hodge
+    parrothelles
+  ];
+  allMachines = servers ++ workstations;
+
 in {
   "wireless.env.age".publicKeys = allMachines ++ trustedUsers;
 
