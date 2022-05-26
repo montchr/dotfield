9ec48f2c8250ba37184d209bef27386241b1e586
Chris Montgomery <chris@cdom.io>
2022-05-26 01:05:18 -0400

    fix(doom): use floating point font sizes -- px don't scale

--- config/emacs/profiles/doom/config.el
+++ config/emacs/profiles/doom/config.el
@@ -6,8 +6,8 @@
 (setq! user-full-name "Chris Montgomery"
        user-mail-address "chris@cdom.io")
 
-(setq! doom-font (font-spec :family "PragmataPro Liga" :size 16)
-       doom-variable-pitch-font (font-spec :family "Barlow" :size 18))
+(setq! doom-font (font-spec :family "PragmataPro Liga" :size 14.0)
+       doom-variable-pitch-font (font-spec :family "Barlow" :size 16.0))
 
 ;; Start the emacs server.
 ;; Open a new frame with `emacsclient -cn'.
@@ -78,7 +78,7 @@
   (custom-set-faces!
     '((mode-line mode-line-inactive)
       :family "PragmataPro Mono"
-      :size 10)))
+      :size 10.0)))
 
 ;; === appearance ==============================================================
 
