fdc83424b167fb96658a60378c70b4d535be3f7a
Chris Montgomery <chris@cdom.io>
2022-06-01 18:28:03 -0400

    fix(doom): disable doom ui module to prevent conflicts with modus

--- config/emacs/profiles/doom/init.el
+++ config/emacs/profiles/doom/init.el
@@ -24,7 +24,7 @@
 
   :ui
   ;; deft                      ; notational velocity for Emacs
-  doom                         ; what makes DOOM look the way it does
+  ;; doom                         ; what makes DOOM look the way it does
   ;; doom-dashboard            ; a nifty splash screen for Emacs
   doom-quit                    ; DOOM quit-message prompts when you quit Emacs
   ;; (emoji +unicode)          ; 🙂
