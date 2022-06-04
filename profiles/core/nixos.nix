19739f5db14f46a15bd80159e923334d22e5a119
Chris Montgomery <chris@cdom.io>
2022-06-04 14:01:28 -0400

    fix: disable unnecessary locale and console config

--- profiles/core/nixos.nix
+++ profiles/core/nixos.nix
@@ -28,12 +28,6 @@
     utillinux
   ];
 
-  i18n.defaultLocale = "en_US.UTF-8";
-  console = {
-    font = "Lat2-Terminus16";
-    keyMap = "us";
-  };
-
   users.groups.secrets.members = ["root" "seadoom" "xtallos"];
 
   services.openssh = {
