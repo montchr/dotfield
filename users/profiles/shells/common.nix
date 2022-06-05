2c11db670d896484623777b707e173eda8a5a509
Chris Montgomery <chris@cdom.io>
2022-06-05 15:55:28 -0400

    fix(shell): set atuin sync address

--- users/profiles/shells/common.nix
+++ users/profiles/shells/common.nix
@@ -36,7 +36,8 @@ in {
   programs.atuin.enable = true;
   programs.atuin.settings = {
     auto_sync = true;
-    sync_frequency = "30m";
+    sync_frequency = "10m";
+    sync_address = "https://api.atuin.sh";
     search_mode = "fuzzy"; # 'prefix' | 'fulltext' | 'fuzzy'
     filter_mode = "global"; # 'global' | 'host' | 'session' | 'directory'
   };
