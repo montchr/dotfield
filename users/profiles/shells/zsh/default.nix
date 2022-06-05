7fc0dd636e7d7e1c6a8d0d9723d670527369ca8b
Chris Montgomery <chris@cdom.io>
2022-06-05 15:55:51 -0400

    feat(zsh): enable vte integration

--- users/profiles/shells/zsh/default.nix
+++ users/profiles/shells/zsh/default.nix
@@ -28,6 +28,7 @@ in {
     enableCompletion = true;
     enableSyntaxHighlighting = true;
     enableAutosuggestions = true;
+    enableVteIntegration = true;
     autocd = true;
 
     history.path = "${config.xdg.dataHome}/zsh/history";
