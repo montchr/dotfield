136f1468e47153e351e63112f5219a1c44ddcc28
Chris Montgomery <chris@cdom.io>
2022-06-05 15:38:02 -0400

    fix(ssh): loosen restriction around agent identities

--- users/profiles/ssh.nix
+++ users/profiles/ssh.nix
@@ -23,6 +23,8 @@ in {
       };
 
       "github.com" = {
+        inherit identityFile;
+        identitiesOnly = true;
         user = "git";
         extraOptions = {
           "MACs" = "hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,umac-128-etm@openssh.com,hmac-sha2-512,hmac-sha2-256,umac-128@openssh.com";
@@ -33,12 +35,9 @@ in {
       };
 
       "*" = {
-        inherit identityFile;
-
         addressFamily = "inet";
         forwardX11 = false;
         forwardX11Trusted = false;
-        identitiesOnly = true;
         serverAliveInterval = 300;
         serverAliveCountMax = 2;
 
