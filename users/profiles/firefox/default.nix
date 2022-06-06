a2c8f536a3b261c4af15fe3cb7a2fc5e82f02b1f
Chris Montgomery <chris@cdom.io>
2022-06-06 18:19:56 -0400

    tweak(firefox): remove some unused extensions

--- users/profiles/firefox/default.nix
+++ users/profiles/firefox/default.nix
@@ -163,19 +163,7 @@ in {
       display-_anchors
       firefox-color
       (lib.mkIf config.programs.browserpass.enable browserpass)
-
-      # Flagfox by Dave G
-      #
-      # Displays a country flag depicting the location of the current website's
-      # server and provides a multitude of tools such as site safety checks,
-      # whois, translation, similar sites, validation, URL shortening, and
-      # more...
-      #
-      # https://addons.mozilla.org/en-US/firefox/addon/flagfox/
       flagfox
-
-      floccus
-
       mailvelope
       multi-account-containers
       octolinker
@@ -187,21 +175,7 @@ in {
       reddit-enhancement-suite
       reduxdevtools
       refined-github
-
-      # Return Youtube Dislike by Dmitry Selivanov
-      #
-      # Returns ability to see dislike statistics on youtube
-      #
-      # https://addons.mozilla.org/en-US/firefox/addon/return-youtube-dislikes/
-      search-engines-helper
-
-      # SingleFile by gildas
-      #
-      # Save an entire web page—including images and styling—as a single HTML file.
-      #
-      # https://addons.mozilla.org/en-US/firefox/addon/single-file/
       single-file
-
       tab-session-manager
       tabliss
       temporary-containers
@@ -210,16 +184,9 @@ in {
 
       ##: Themes {{
 
-      # Arctic Nord Theme by christos
-      #
-      # https://addons.mozilla.org/en-US/firefox/addon/arctic-nord-theme/
-      #
       # TODO: add this to upstream repo
       # arctic-nord-theme
 
-      # Nord Polar Night Theme by christos
-      #
-      # https://addons.mozilla.org/en-US/firefox/addon/nord-polar-night-theme/
       theme-nord-polar-night
 
       ##: }}
