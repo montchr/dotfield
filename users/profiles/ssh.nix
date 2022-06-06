96fd1f619f92a2b1145eeb038b62d88edda2b246
Chris Montgomery <chris@cdom.io>
2022-06-06 18:10:10 -0400

    fix(ssh): let github.com use agent identities

--- users/profiles/ssh.nix
+++ users/profiles/ssh.nix
@@ -23,8 +23,8 @@ in {
       };
 
       "github.com" = {
-        inherit identityFile;
-        identitiesOnly = true;
+        # inherit identityFile;
+        # identitiesOnly = true;
         user = "git";
         extraOptions = {
           "MACs" = "hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,umac-128-etm@openssh.com,hmac-sha2-512,hmac-sha2-256,umac-128@openssh.com";
