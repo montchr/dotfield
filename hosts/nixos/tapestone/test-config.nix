0889599e0ba5bd82ad8ce899ca9797edb5622cb2
Chris Montgomery <chris@cdom.io>
2022-06-05 20:43:03 -0400

    fix(tapestone): comment out missing import in test config
    despite the fact that it's not being used, it still results in failing checks

--- hosts/nixos/tapestone/test-config.nix
+++ hosts/nixos/tapestone/test-config.nix
@@ -8,7 +8,7 @@
 }: {
   imports = [
     # Include the results of the hardware scan.
-    ./hardware-configuration.nix
+    # ./hardware-configuration.nix
   ];
 
   # Use the systemd-boot EFI boot loader.
