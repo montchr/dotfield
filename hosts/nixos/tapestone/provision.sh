5527494dab2fba707ec00f89a8a7272873e57d9b
Chris Montgomery <chris@cdom.io>
2022-06-13 11:02:48 -0400

    fix(tapestone): use zfs scheduler

--- hosts/nixos/tapestone/provision.sh
+++ hosts/nixos/tapestone/provision.sh
@@ -428,6 +428,12 @@ in
   services.zfs.autoScrub.enable = true;
   services.zfs.autoScrub.pools = [ "rpool" "spool" ];
 
+  # ZFS already has its own scheduler.
+  # https://nixos.wiki/wiki/ZFS#How_to_use_it
+  services.udev.extraRules = ''
+    ACTION=="add|change", KERNEL=="sd[a-z]*[0-9]*|mmcblk[0-9]*p[0-9]*|nvme[0-9]*n[0-9]*p[0-9]*", ENV{ID_FS_TYPE}=="zfs_member", ATTR{../queue/scheduler}="none"
+  '';
+
   # This value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you
