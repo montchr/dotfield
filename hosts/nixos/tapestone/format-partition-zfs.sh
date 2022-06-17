db925411e6ba2e3b6b1b75ddecad2fea929d9c00
Chris Montgomery <chris@cdom.io>
2022-06-16 21:39:56 -0400

    tweak(tapestone): no source config

--- hosts/nixos/tapestone/format-partition-zfs.sh
+++ hosts/nixos/tapestone/format-partition-zfs.sh
@@ -2,7 +2,88 @@
 
 set -euox pipefail
 
-. ./config.sh
+###: CONFIGURATION =======================================================
+
+export MY_HOSTNAME=tapestone
+
+
+##: --- Networking ---
+
+export IPV4_ADDR="94.130.220.154"
+export IPV4_CIDR="26"
+export IPV4_GATEWAY="94.130.220.129"
+
+export IPV6_ADDR="2a01:4f8:13b:17ac::1"
+export IPV6_SUBNET="2a01:4f8:13b:17ac::/64"
+export IPV6_CIDR="64"
+export IPV6_GATEWAY="fe80::1"
+
+# Cloudflare nameservers.
+export NSV4="1.1.1.1" # also 1.0.0.1
+export NSV6="2606:4700:4700::1111"
+
+
+##: --- Devices ---
+
+# boot/root
+export NVME1="/dev/disk/by-id/nvme-SAMSUNG_MZQL2960HCJR-00A07_S64FNE0R701851"
+export NVME2="/dev/disk/by-id/nvme-SAMSUNG_MZQL2960HCJR-00A07_S64FNE0R701889"
+
+# silo
+export HDD01="/dev/disk/by-id/ata-TOSHIBA_MG08ACA16TEY_X1J0A04SFVNG"
+export HDD02="/dev/disk/by-id/ata-TOSHIBA_MG08ACA16TEY_X1J0A058FVNG"
+export HDD03="/dev/disk/by-id/ata-TOSHIBA_MG08ACA16TEY_X1J0A04YFVNG"
+export HDD04="/dev/disk/by-id/ata-TOSHIBA_MG08ACA16TEY_X1J0A04NFVNG"
+export HDD05="/dev/disk/by-id/ata-TOSHIBA_MG08ACA16TEY_X1J0A04DFVNG"
+export HDD06="/dev/disk/by-id/ata-TOSHIBA_MG08ACA16TEY_X1J0A053FVNG"
+export HDD07="/dev/disk/by-id/ata-TOSHIBA_MG08ACA16TEY_X1J0A04TFVNG"
+export HDD08="/dev/disk/by-id/ata-TOSHIBA_MG08ACA16TEY_X1W0A00HFVNG"
+export HDD09="/dev/disk/by-id/ata-TOSHIBA_MG08ACA16TEY_X1J0A02AFVNG"
+export HDD10="/dev/disk/by-id/ata-TOSHIBA_MG08ACA16TEY_X1J0A05YFVNG"
+
+
+##: --- ZFS Configuration ---
+#
+# Required for OpenZFS. Must be unique across all machines.
+#
+# Contrary to many explanations out there, according to OpenZFS, this does not
+# need to be entirely numeric.
+
+# Generates a random host ID for uniqueness across each run.
+# FIXME: strip spaces! results in bad id
+MY_HOSTID="$(tr -dc 0-9a-f < /dev/urandom | head -c 8)"
+
+# For a consistent host ID, use this alternative.
+# MY_HOSTID="$(head -c 8 /etc/machine-id)"
+
+export MY_HOSTID
+
+export ZFS_ENC_OPTS="-o encryption=aes-256-gcm -o keyformat=passphrase -o keylocation=prompt"
+
+
+##: --- Helper Functions ---
+
+# Wrapper for parted >= 3.3 that does not exit 1 when it cannot inform
+# the kernel of partitions changing (we use partprobe for that).
+parted_nice() {
+  parted "$@" 2> parted-stderr.txt || {
+    grep "unable to inform the kernel of the change" parted-stderr.txt \
+      || echo >&2 "Parted failed; stderr: $(< parted-stderr.txt)"
+  }
+}
+
+# Create and mount a new ZFS pool.
+zup() {
+  local pool=$1
+  local mountpoint=$2
+  shift 2
+  zfs create -p -o canmount=on -o mountpoint=legacy "$@" "$pool"
+  mkdir -p "$mountpoint"
+  mount -t zfs "$pool" "$mountpoint"
+}
+
+export -f parted_nice
+export -f zup
 
 # Inspect existing disks
 lsblk
