b1f925f6c189ed141efa8a5aed946c062f577040
Chris Montgomery <chris@cdom.io>
2022-06-20 09:25:45 -0400

    tweak(doom): disable unused latex module
    no idea why i had this enabled, i'm no scientist

--- config/emacs/profiles/doom/init.el
+++ config/emacs/profiles/doom/init.el
@@ -143,7 +143,7 @@
   (javascript +lsp)    ; all(hope(abandon(ye(who(enter(here))))))
   ;;julia              ; a better, faster MATLAB
   ;;kotlin             ; a better, slicker Java(Script)
-  (latex +fold)              ; writing papers in Emacs has never been so fun
+  ;;(latex +fold)      ; writing papers in Emacs has never been so fun
   ;;lean
   ledger               ; an accounting system in Emacs
   lua                  ; one-based indices? one-based indices
