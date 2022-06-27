86fa78f4d1b175094f86752b432983f9e6c1a744
Chris Montgomery <chris@cdom.io>
2022-06-27 19:12:40 -0400

    fix(secrets|ssh): read host keys from `hosts.toml`

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,31 +1,25 @@
 let
-  machines = {
-    boschic = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJxqOfaPK3FQnBsp7ey3yUEuFnYzAJSF1PdyJLqpKAfK";
-    hierophant = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF14I8s1ywGVCmInco+wfCG5C9kJB0Y9HCOA12ZX9cHe";
-    HodgePodge = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPRS3UkaJeMQm58v+ggR5e0hVeUbFZkhyQJwEC0LK5YS";
-    parrothelles = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINBvBCsqtgEdC4J+d1xzrwPIircRYSKbFHR0FulaNV5z";
-    tso-portal = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINUnCW5QopDKLISa/kRcH+28n9QUV/nFuYadXqUp/ZVq";
-    tapestone = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICdk0EzvFodiLIxxheOWQTAav92uZwfMVR1k/0TS7yal";
-  };
-
+  hosts = (builtins.fromTOML (builtins.readFile ../hosts/hosts.toml)).hosts;
+  hostKeys = builtins.mapAttrs (n: v: v.keys) hosts;
   trustedUsers = import ./authorized-keys.nix;
 
-  servers = with machines; [
+  servers = with hostKeys;
     hierophant
-    tapestone
-    tso-portal
-  ];
-  workstations = with machines; [
+    ++ tapestone
+    ;
+
+  workstations = with hostKeys;
     boschic
-    HodgePodge
-    parrothelles
-  ];
+    ++ hodgepodge
+    ;
+
   allMachines = servers ++ workstations;
 in {
-  "wireless.env.age".publicKeys = allMachines ++ trustedUsers;
+  "wireless.env.age".publicKeys = workstations ++ trustedUsers;
 
-  "aws/aws-cdom-default.pem.age".publicKeys = allMachines ++ trustedUsers;
+  "aws/aws-cdom-default.pem.age".publicKeys = workstations ++ trustedUsers;
 
-  "espanso/personal.yml.age".publicKeys = allMachines ++ trustedUsers;
-  "espanso/work.yml.age".publicKeys = trustedUsers;
+  "espanso/personal.yml.age".publicKeys = workstations ++ trustedUsers;
+  # FIXME
+  # "espanso/work.yml.age".publicKeys = workstations ++ trustedUsers;
 }
