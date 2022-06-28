a55d3f98828800ecede232dd1bf0c295ac62816a
Chris Montgomery <chris@cdom.io>
2022-06-28 16:30:18 -0400

    feat(boschic): btrfs on nvme device

--- hosts/nixos/boschic/default.nix
+++ hosts/nixos/boschic/default.nix
@@ -25,6 +25,8 @@
   # FIXME: does this interfere with rEFInd? if not this, then i blame Windows.
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.timeout = 15;
+  boot.initrd.supportedFilesystems = [ "ext4" "btrfs" ];
+  boot.supportedFilesystems = [ "ext4" "btrfs" ];
 
   system.stateVersion = "21.11";
 
