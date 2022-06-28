cfa891157c182f8eeda6996599b76d8f4927de45
Chris Montgomery <chris@cdom.io>
2022-06-28 01:54:17 -0400

    fix(net): re-enable cloudflare dns

--- profiles/networking/common.nix
+++ profiles/networking/common.nix
@@ -4,14 +4,14 @@
   pkgs,
   ...
 }: {
-  #  networking = {
-  #    # Use Cloudflare DNS
-  #    # https://developers.cloudflare.com/1.1.1.1/
-  #    dns = [
-  #      "1.1.1.1"
-  #      "1.0.0.1"
-  #      "2606:4700:4700::1111"
-  #      "2606:4700:4700::1001"
-  #    ];
-  #  };
+   networking = {
+     # Use Cloudflare DNS
+     # https://developers.cloudflare.com/1.1.1.1/
+     nameservers = [
+       "1.1.1.1"
+       "1.0.0.1"
+       "2606:4700:4700::1111"
+       "2606:4700:4700::1001"
+     ];
+   };
 }
