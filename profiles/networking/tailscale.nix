b63039207c68546207563ad7d44cd4ff4e6f9ad5
Chris Montgomery <chris@cdom.io>
2022-07-03 18:41:26 -0400

    net: set trusted tailscale interface based on its def

--- profiles/networking/tailscale.nix
+++ profiles/networking/tailscale.nix
@@ -5,6 +5,6 @@
   ...
 }: {
   services.tailscale.enable = true;
-  networking.firewall.trustedInterfaces = ["tailscale0"];
+  networking.firewall.trustedInterfaces = [config.services.tailscale.interfaceName];
   networking.firewall.checkReversePath = "loose";
 }
