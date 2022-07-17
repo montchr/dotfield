8046a018f2e76be48c29a038b82d84dcbe937961
Chris Montgomery <chris@cdom.io>
2022-07-17 14:58:26 -0400

    tapestone: luks initrd unlock via tor for btrfs?

--- hosts/nixos/tapestone/boot.nix
+++ hosts/nixos/tapestone/boot.nix
@@ -7,6 +7,14 @@
 }: let
   inherit (builtins) toString;
   inherit (primaryUser) authorizedKeys;
+  port = 22;
+  torRc = pkgs.writeText "tor.rc" ''
+    DataDirectory /etc/tor
+    SOCKSPort 127.0.0.1:9050 IsolateDestAddr
+    SOCKSPort 127.0.0.1:9063
+    HiddenServiceDir /etc/tor/onion/bootup
+    HiddenServicePort ${toString port} 127.0.0.1:${toString port}
+  '';
 in {
   boot.initrd = {
     availableKernelModules = [
@@ -21,6 +29,46 @@ in {
       "igb"
     ];
     kernelModules = [];
+
+    luks.cryptoModules = [ "aes" "sha512" "sha1" "xts" ];
+
+    extraUtilsCommands = ''
+      copy_bin_and_libs ${pkgs.tor}/bin/tor
+
+      # Gather entropy for faster tor startup
+      copy_bin_and_libs ${pkgs.haveged}/bin/haveged
+    '';
+
+    network = {
+      enable = true;
+      ssh = {
+        inherit authorizedKeys port;
+        enable = true;
+        hostKeys = [
+          "/persist/etc/secrets/initrd/ssh_host_rsa_key"
+          "/persist/etc/secrets/initrd/ssh_host_ed25519_key"
+        ];
+      };
+      postCommands = ''
+        echo "[tor]: preparing onion folder..."
+        chmod -R 700 /etc/tor
+
+        echo "[net]: force localhost up..."
+        ip a a 127.0.0.1/8 dev lo
+        ip link set lo up
+
+        echo "[haveged]: gathering entropy..."
+        haveged -F &
+
+        echo "[tor]: starting tor..."
+        tor -f ${torRc} --verify-config
+        tor -f ${torRc} &
+      '';
+    };
+
+    secrets = {
+      "/etc/tor/onion/bootup" = /persist/tor/onion;
+    };
   };
 
   boot.loader.grub = {
--- hosts/nixos/tapestone/filesystems.nix
+++ hosts/nixos/tapestone/filesystems.nix
@@ -4,23 +4,18 @@
   pkgs,
   ...
 }: {
-  # Required for ZFS.
-  networking.hostId = "e2e496d7";
+  boot.supportedFilesystems = ["btrfs"];
 
-  # FIXME: configure mail sending
-  nixpkgs.config.packageOverrides = pkgs: {
-    zfsStable = pkgs.zfsStable.override {enableMail = true;};
-    zfsUnstable = pkgs.zfsUnstable.override {enableMail = true;};
-  };
-
-  boot.zfs.enableUnstable = true;
-  # TODO: the manual recommends setting this to false for improved zfs
-  # safeguards, but it's enabled by default for compatibility
-  # zfs.forceImportRoot = false;
-
-  boot.supportedFilesystems = ["btrfs" "zfs"];
-
-  swapDevices = [];
+  swapDevices = [
+    {
+      device = "/dev/disk/by-uuid/d84100d6-9750-4577-9a6e-c2894cbe11da";
+      randomEncryption.enable = true;
+    }
+    {
+      device = "/dev/disk/by-uuid/bc9eca8b-a87f-4ffb-b21c-4b88b0ab9771";
+      randomEncryption.enable = true;
+    }
+  ];
 
   fileSystems."/" =
     { device = "/dev/disk/by-label/nixos";
@@ -64,6 +59,39 @@
       # options = ["nofail" "X-mount.mkdir"];
     };
 
+  boot.initrd.luks.devices = {
+    "enc01" = {
+      device = "/dev/disk/by-uuid/99b7edb7-e79f-423a-b043-fe3534cf8132";
+    };
+    "enc02" = {
+      device = "/dev/disk/by-uuid/34fd5e9a-be69-4537-97fd-650249e22117";
+    };
+    "enc03" = {
+      device = "/dev/disk/by-uuid/0978aaa3-fabf-4140-b9af-7b6fa2bd2e8a";
+    };
+    "enc04" = {
+      device = "/dev/disk/by-uuid/05a35847-7bf6-446c-8062-a34a4c3814e8";
+    };
+    "enc05" = {
+      device = "/dev/disk/by-uuid/2dcdd2d2-2e3b-44f6-ae08-6163ea93d1a7";
+    };
+    "enc06" = {
+      device = "/dev/disk/by-uuid/b2c88455-d749-4088-aa70-e0010268eccc";
+    };
+    "enc07" = {
+      device = "/dev/disk/by-uuid/e89c848c-c880-414f-a9f1-404fdedecad3";
+    };
+    "enc08" = {
+      device = "/dev/disk/by-uuid/791a538f-0f8d-4cdf-a984-2f785e9bcc72";
+    };
+    "enc09" = {
+      device = "/dev/disk/by-uuid/76b9cd36-8eff-4047-9b05-cc5c0e496bbf";
+    };
+    "enc10" = {
+      device = "/dev/disk/by-uuid/c8a4f871-41b6-46c7-9e6b-ec7cd0cd2869";
+    };
+  };
+
   # fileSystems."/boot-fallback" = {
   #   device = "/dev/disk/by-uuid/FF5D-559E";
   #   fsType = "vfat";
@@ -72,15 +100,15 @@
   #   options = ["nofail" "X-mount.mkdir"];
   # };
 
-  fileSystems."/silo/backup" = {
-    device = "spool/backup";
-    fsType = "zfs";
-    options = ["nofail" "zfsutil"];
-  };
+  fileSystems."/silo/backup" =
+    { device = "/dev/disk/by-label/silo";
+      fsType = "btrfs";
+      options = [ "nofail" "subvol=backup" ];
+    };
 
-  fileSystems."/silo/data" = {
-    device = "spool/data";
-    fsType = "zfs";
-    options = ["nofail" "zfsutil"];
-  };
+  fileSystems."/silo/data" =
+    { device = "/dev/disk/by-label/silo";
+      fsType = "btrfs";
+      options = [ "nofail" "subvol=data/media" ];
+    };
 }
