2ef4ffaae48fc20bdbb4b0807e37a67035bb3e58
Chris Montgomery <chris@cdom.io>
2022-07-21 09:42:33 -0400

    emacs: unicode tweaks

--- users/seadoom/config/emacs/profiles/xtallos/init.el
+++ users/seadoom/config/emacs/profiles/xtallos/init.el
@@ -62,6 +62,20 @@
 ;; No beep!
 (setq visible-bell t)
 
+;;; Unicode
+(set-language-environment   'utf-8)
+(when (fboundp 'set-charset-priority)
+  (set-charset-priority 'unicode))
+(prefer-coding-system          'utf-8)
+(set-buffer-file-coding-system 'utf-8)
+(set-clipboard-coding-system   'utf-8)
+(set-default-coding-systems    'utf-8)
+(set-file-name-coding-system   'utf-8)
+(set-keyboard-coding-system    'utf-8)
+(set-selection-coding-system   'utf-8)
+(set-terminal-coding-system    'utf-8)
+(setq locale-coding-system     'utf-8)
+
 
 ;;
 ;;; Bootstrap
