736219a33e6377507bcf02977df2056981a5f108
Chris Montgomery <chris@cdom.io>
2022-07-21 16:52:05 -0400

    emacs: remove duplicate unicode settings

--- users/seadoom/config/emacs/profiles/xtallos/lisp/init-editor.el
+++ users/seadoom/config/emacs/profiles/xtallos/lisp/init-editor.el
@@ -39,12 +39,6 @@
 ;;
 ;;; Code:
 
-(prefer-coding-system       'utf-8)
-(set-default-coding-systems 'utf-8)
-(set-terminal-coding-system 'utf-8)
-(set-keyboard-coding-system 'utf-8)
-(set-language-environment   'utf-8)
-
 ;; Backup files sound useful but get in the way, experts say.
 (setq make-backup-files nil)
 
