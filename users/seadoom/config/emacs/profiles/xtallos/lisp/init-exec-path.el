c801d914cd97029e7c943cceb944d57a4ab64fef
Chris Montgomery <chris@cdom.io>
2022-07-22 16:18:10 -0400

    emacs: fixme something something

--- users/seadoom/config/emacs/profiles/xtallos/lisp/init-exec-path.el
+++ users/seadoom/config/emacs/profiles/xtallos/lisp/init-exec-path.el
@@ -38,6 +38,7 @@
 ;;
 ;;; Code:
 
+;; FIXME: ensure this runs mac only
 (use-package exec-path-from-shell
   :config
   (dolist (var '("SSH_AUTH_SOCK" "SSH_AGENT_PID" "GPG_AGENT_INFO" "LANG" "LC_CTYPE" "NIX_SSL_CERT_FILE" "NIX_PATH"))
