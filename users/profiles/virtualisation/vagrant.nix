9de65878e4fdd8bbc859612939c3c208658293c4
Chris Montgomery <chris@cdom.io>
2022-07-25 18:33:20 -0400

    dev: add `vagrant` cli to profile

--- users/profiles/virtualisation/vagrant.nix
+++ users/profiles/virtualisation/vagrant.nix
@@ -6,6 +6,8 @@
 }: {
   imports = [../ruby.nix];
 
+  home.packages = with pkgs; [vagrant];
+
   home.sessionVariables = {
     VAGRANT_ALIAS_FILE = "$XDG_DATA_HOME/vagrant/aliases";
     VAGRANT_HOME = "$XDG_DATA_HOME/vagrant";
