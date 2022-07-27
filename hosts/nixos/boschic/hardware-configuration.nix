1efc871b368e537ee4951fc855321ac93b815bd7
Chris Montgomery <chris@cdom.io>
2022-07-26 23:25:05 -0400

    boschic: add `usb_storage` kernel module to available modules

--- hosts/nixos/boschic/hardware-configuration.nix
+++ hosts/nixos/boschic/hardware-configuration.nix
@@ -4,7 +4,7 @@
   pkgs,
   ...
 }: {
-  boot.initrd.availableKernelModules = ["nvme" "xhci_pci" "ahci" "usbhid" "sd_mod"];
+  boot.initrd.availableKernelModules = ["nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod"];
   boot.initrd.kernelModules = ["kvm-amd"];
   boot.extraModulePackages = [];
 
