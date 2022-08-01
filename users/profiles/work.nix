0d50878f3c070608c8892fbb113a27ec8134c971
Chris Montgomery <chris@cdom.io>
2022-08-01 15:53:48 -0400

    firefox: add 'work' nix profile for lastpass addon
    *shudder*

---
+++ users/profiles/work.nix
@@ -0,0 +1,9 @@
+{ config, lib, pkgs, ... }:
+let
+  inherit (pkgs.nur.repos.rycee) firefox-addons;
+in
+{
+  programs.firefox.extensions = with firefox-addons; [
+    lastpass-password-manager
+  ];
+}
