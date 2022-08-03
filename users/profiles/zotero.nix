0c3d80d2ac2a839bd737dc550e2008bd2d4982fa
Chris Montgomery <chris@cdom.io>
2022-08-03 18:20:54 -0400

    darwin|notes: omit `zotero` package on unsupported darwin

--- users/profiles/zotero.nix
+++ users/profiles/zotero.nix
@@ -1,5 +1,10 @@
 { config, lib, pkgs, ... }:
-
+let
+  inherit (pkgs.stdenv.hostPlatform) isDarwin;
+in
 {
-  home.packages = with pkgs; [zotero];
+  home.packages = with pkgs; [
+    # FIXME: how is this unsupported on darwin? prob cos macOS apps don't install well with nix
+    (lib.mkIf (!isDarwin) zotero)
+  ];
 }
