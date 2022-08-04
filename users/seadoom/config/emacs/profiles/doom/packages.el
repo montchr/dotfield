73dcf1e3638d39620b3482d33a4877d60e597bfb
Chris Montgomery <chris@cdom.io>
2022-08-04 17:45:00 -0400

    emacs:doom:completions: fix a slight but nefarious parens typo

--- users/seadoom/config/emacs/profiles/doom/packages.el
+++ users/seadoom/config/emacs/profiles/doom/packages.el
@@ -55,8 +55,8 @@
 ;;; === completions ============================================================
 
 ;; https://github.com/elken/doom/blob/fd381b6837a34bb7b9bc072909bb697c0ac11f70/config.org#disabledunpin
-(when (featurep! :completion corfu
-  (unpin! evil-collection)))
+(when (featurep! :completion corfu)
+  (unpin! evil-collection))
 
 (package! embark-vc)
 
