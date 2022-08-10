58b6c412043d40ee66b2ed9f3c14a1d75e3c5e1e
Chris Montgomery <chris@cdom.io>
2022-08-10 12:36:33 -0400

    emacs: unpin overlay thanks to upstream fix in doom

--- flake.nix
+++ flake.nix
@@ -61,8 +61,7 @@
     rnix-lsp.url = "github:nix-community/rnix-lsp";
 
     # Emacsen.
-    # FIXME: last known working version of `emacsPgtkNativeComp`
-    emacs-overlay.url = "github:nix-community/emacs-overlay?ref=30a3d95bb4d9812e26822260b6ac45efde0d7700";
+    emacs-overlay.url = "github:nix-community/emacs-overlay";
     emacs-overlay.inputs.nixpkgs.follows = "nixpkgs";
     crafted-emacs.url = "github:SystemCrafters/crafted-emacs";
     crafted-emacs.flake = false;
