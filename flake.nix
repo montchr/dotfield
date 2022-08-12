fa7304cf9fcabda4fb5668e7971468d2f2e1f096
Chris Montgomery <chris@cdom.io>
2022-08-12 11:39:04 -0400

    meta: disable extra nix config flake settings to avoid warnings

--- flake.nix
+++ flake.nix
@@ -1,9 +1,10 @@
 {
   description = "Dotfield";
 
-  nixConfig.extra-experimental-features = "nix-command flakes";
-  nixConfig.extra-substituters = "https://dotfield.cachix.org https://nix-community.cachix.org";
-  nixConfig.extra-trusted-public-keys = "dotfield.cachix.org-1:b5H/ucY/9PDARWG9uWA87ZKWUBU+hnfF30amwiXiaNk= nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=";
+  # Enable as needed for bootstrapping. Otherwise these entries cause warnings on every rebuild.
+  # nixConfig.extra-experimental-features = "nix-command flakes";
+  # nixConfig.extra-substituters = "https://dotfield.cachix.org https://nix-community.cachix.org";
+  # nixConfig.extra-trusted-public-keys = "dotfield.cachix.org-1:b5H/ucY/9PDARWG9uWA87ZKWUBU+hnfF30amwiXiaNk= nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=";
 
   inputs = {
     nixos-stable.url = "github:NixOS/nixpkgs/nixos-22.05";
