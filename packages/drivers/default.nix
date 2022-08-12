8eb57370cc520c7c3550cd68e52d93cfb2ba5c70
Chris Montgomery <chris@cdom.io>
2022-08-12 16:29:34 -0400

    workstation: add epson-201212w driver
    still doesn't print...

---
+++ packages/drivers/default.nix
@@ -0,0 +1,3 @@
+final: prev: {
+  epson-201212w = final.callPackage ./epson_201212w {};
+}
--- profiles/printers-scanners.nix
+++ profiles/printers-scanners.nix
@@ -6,4 +6,10 @@
 }: {
   # Enable CUPS to print documents.
   services.printing.enable = true;
+  services.printing.drivers = with pkgs; [
+    # EPSON WF-3520
+    epson-201212w
+    gutenprint
+  ];
+  nixpkgs.config.allowUnfree = lib.mkForce true;
 }
