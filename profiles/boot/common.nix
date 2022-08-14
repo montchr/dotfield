adeee3ce9c9e8fe2bf4878bc826944a3260ae743
Chris Montgomery <chris@cdom.io>
2022-08-14 12:11:32 -0400

    boot: clean up common boot options

--- profiles/boot/common.nix
+++ profiles/boot/common.nix
@@ -5,26 +5,13 @@
   ...
 }: {
   boot = {
-    loader = {
-      # grub.enable = false;
-      # systemd-boot.enable = pkgs.stdenv.hostPlatform.system == "x86_64-linux";
-    };
-    # kernelParams =
-    #   ["modeset" "nofb"]
-    #   ++ lib.optionals (pkgs.stdenv.hostPlatform.system == "x86_64-linux") [
-    #     "pti=off"
-    #     "spectre_v2=off"
-    #   ];
-
-    kernelPackages = pkgs.linuxPackages_latest;
-
-    consoleLogLevel = 3;
+    kernelPackages = lib.mkDefault pkgs.linuxPackages_latest;
+    consoleLogLevel = lib.mkDefault 3;
 
     # TODO: provide reasoning.
-    kernel.sysctl."vm.swappiness" = 0;
-    kernel.sysctl."kernel/sysrq" = 1;
+    # kernel.sysctl."vm.swappiness" = 0;
+    # kernel.sysctl."kernel/sysrq" = 1;
   };
 }
 ## sources:
 # https://github.com/balsoft/nixos-config/tree/b5ed51152f96225c0bb14482bdb3022b9c979679/profiles/boot.nix
-
