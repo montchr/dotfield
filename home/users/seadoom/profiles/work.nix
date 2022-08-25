f5701d5e5e36198c72e5a39cc16cd97bb2391430
Chris Montgomery <chris@cdom.io>
2022-08-25 09:34:08 -0400

    feat(work|ssh): add ssh config for external hosts

--- home/users/seadoom/profiles/work.nix
+++ home/users/seadoom/profiles/work.nix
@@ -9,4 +9,21 @@ in {
   programs.firefox.extensions = with firefox-addons; [
     lastpass-password-manager
   ];
+  programs.ssh.matchBlocks = lib.mkAfter {
+    "kweb-prod-www" = {
+      hostname = "67.225.164.90";
+      # FIXME
+      # port = 522;
+      user = "cdom";
+    };
+    "kweb-prod-db" = {
+      hostname = "67.225.164.91";
+      port = 522;
+      user = "cdom";
+    };
+    "kweb-dev" = {
+      hostname = "67.43.11.196";
+      user = "cdom";
+    };
+  };
 }
