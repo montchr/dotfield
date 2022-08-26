810f17e7a0dabfa50e4da2138eb6cfefbb89c52e
Chris Montgomery <chris@cdom.io>
2022-08-26 12:50:43 -0400

    fix(home): rm `$XDG_DEV_DIR` and set `$XDG_PROJECTS_DIR` to its value
    i don't use `~/Projects`

--- home/profiles/desktop/common.nix
+++ home/profiles/desktop/common.nix
@@ -5,15 +5,15 @@ moduleArgs @ {
   ...
 }: let
   inherit (pkgs.stdenv.hostPlatform) isLinux;
+  inherit (config.home) homeDirectory;
 in
   lib.mkIf isLinux {
     xdg.userDirs = {
       enable = true;
       createDirectories = true;
       extraConfig = {
-        XDG_DEV_DIR = "$HOME/Developer";
+        XDG_PROJECTS_DIR = homeDirectory + "/Developer";
         XDG_MAIL_DIR = config.accounts.email.maildirBasePath or "$HOME/Mail";
-        XDG_PROJECTS_DIR = "$HOME/Projects";
       };
     };
 
