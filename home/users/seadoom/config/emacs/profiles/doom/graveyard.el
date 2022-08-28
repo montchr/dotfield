fb9b645287a8eea19adee61a5312c1ef50059fb5
Chris Montgomery <chris@cdom.io>
2022-08-28 10:21:36 -0400

    chore(doom): move some large unused config sections to `graveyard.el`

---
+++ home/users/seadoom/config/emacs/profiles/doom/graveyard.el
@@ -0,0 +1,31 @@
+;;; config/emacs/profiles/doom/graveyard.el -*- lexical-binding: t; -*-
+
+;; === graveyard ===============================================================
+
+;; TODO: these don't work quite right
+;; https://github.com/konrad1977/emacs/blob/main/init.el
+;; (use-package! svg-tag-mode
+;;   :hook ((prog-mode . svg-tag-mode)
+;;          (org-mode . svg-tag-mode))
+;;   :config
+;;   (setq svg-tag-tags
+;;         '(
+;;           ("DONE\\b" . ((lambda (tag) (svg-tag-make "DONE" :face 'org-done :margin 0))))
+;;           ("FIXME\\b" . ((lambda (tag) (svg-tag-make "FIXME" :face 'org-todo :inverse t :margin 0))))
+;;           ("\\/\\/\\W?MARK\\b:\\|MARK\\b:" . ((lambda (tag) (svg-tag-make "MARK" :face 'font-lock-doc-face :inverse t :margin 0 :crop-right t))))
+;;           ("MARK\\b:\\(.*\\)" . ((lambda (tag) (svg-tag-make tag :face 'font-lock-doc-face :crop-left t))))
+
+;;           ("\\/\\/\\W?eslint-disable" . ((lambda (tag) (svg-tag-make "eslint-disable" :face 'org-level-3 :inverse t :margin 0 :crop-right t))))
+;;           ("eslint-disable\\(.*\\)" . ((lambda (tag) (svg-tag-make tag :face 'org-level-3 :crop-left t))))
+
+;;           ("\\/\\/\\W?TODO\\b\\|TODO\\b" . ((lambda (tag) (svg-tag-make "TODO" :face 'org-todo :inverse t :margin 0 :crop-right t))))
+;;           ("TODO\\b\\(.*\\)" . ((lambda (tag) (svg-tag-make tag :face 'org-todo :crop-left t))))
+;;           )))
+
+
+;; Add a CREATED property to org-mode headings.
+;; (use-package! org-expiry
+;;   :after (org)
+;;   :config
+;;   (setq! org-expiry-inactive-timestamps t)
+;;   (org-expiry-insinuate))
