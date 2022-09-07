2aaf4a7c24a7cca5a7d3af9cc922ab23480995e5
Chris Montgomery <chris@cdom.io>
2022-09-07 16:20:09 -0400

    feat(theme): add `colors.inverseSchemeType` lib function

---
+++ lib/src/colors.nix
@@ -0,0 +1,8 @@
+{lib, ...}: {
+  inverseSchemeType = type:
+    if ("dark" == type)
+    then "light"
+    else if ("light" == type)
+    then "dark"
+    else throw "Unsupported color scheme type '${type}' specified.";
+}
