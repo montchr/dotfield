19b764449df0803fc2815d6cbb210d3286a339b3
Chris Montgomery <chris@cdom.io>
2022-09-20 20:18:51 -0400

    chore(emacs:doom): update `init.el` with upstream updates

--- home/users/seadoom/config/emacs/profiles/doom/init.el
+++ home/users/seadoom/config/emacs/profiles/doom/init.el
@@ -34,10 +34,10 @@
  ;; doom-dashboard            ; a nifty splash screen for Emacs
  doom-quit                    ; DOOM quit-message prompts when you quit Emacs
  ;; (emoji +unicode)          ; 🙂
- ;; fill-column               ; a `fill-column' indicator
  hl-todo                      ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
  hydra
  ;; indent-guides             ; highlighted indent columns
+ ;; ligatures                 ; ligatures and symbols to make your code pretty again
  ;; minimap                   ; show a map of the code on the side
  modeline                     ; snazzy, Atom-inspired modeline, plus API
  nav-flash                    ; blink cursor line after big motions
@@ -47,7 +47,7 @@
  ;; tabs                      ; an tab bar for Emacs
  ;; (treemacs +lsp)           ; a project drawer, like neotree but cooler
  ;; unicode                   ; extended unicode support for various languages
- vc-gutter                    ; vcs diff in the fringe
+ (vc-gutter +pretty)          ; vcs diff in the fringe
  ;; vi-tilde-fringe           ; fringe tildes to mark beyond EOB
  ;; window-select             ; visually switch windows
  workspaces                   ; tab emulation, persistence & separate workspaces
@@ -147,51 +147,50 @@
  (sh +fish +lsp)      ; she sells {ba,z,fi}sh shells on the C xor
  (web +lsp)           ; the tubes
  (yaml +lsp)          ; JSON, but readable
- ;;agda               ; types of types of types of types...
- ;;cc                 ; C/C++/Obj-C madness
- ;;beancount          ; mind the GAAP
- ;;cc                 ; C > C++ == 1
- ;;clojure            ; java with a lisp
- ;;common-lisp        ; if you've seen one lisp, you've seen them all
- ;;coq                ; proofs-as-programs
- ;;crystal            ; ruby at the speed of c
- ;;csharp             ; unity, .NET, and mono shenanigans
- ;;(dart +flutter)    ; paint ui and not much else
- ;;elixir             ; erlang done right
- ;;elm                ; care for a cup of TEA?
- ;;erlang             ; an elegant language for a more civilized age
- ;;ess                ; emacs speaks statistics
+ ;;agda              ; types of types of types of types...
+ ;;beancount         ; mind the GAAP
+ ;;(cc +lsp)         ; C > C++ == 1
+ ;;clojure           ; java with a lisp
+ ;;common-lisp       ; if you've seen one lisp, you've seen them all
+ ;;coq               ; proofs-as-programs
+ ;;crystal           ; ruby at the speed of c
+ ;;csharp            ; unity, .NET, and mono shenanigans
+ ;;(dart +flutter)   ; paint ui and not much else
+ ;;elixir            ; erlang done right
+ ;;elm               ; care for a cup of TEA?
+ ;;erlang            ; an elegant language for a more civilized age
+ ;;ess               ; emacs speaks statistics
  ;;factor
- ;;faust              ; dsp, but you get to keep your soul
- ;;fortran            ; in FORTRAN, GOD is REAL (unless declared INTEGER)
- ;;fsharp             ; ML stands for Microsoft's Language
- ;;fstar              ; (dependent) types and (monadic) effects and Z3
- ;;gdscript           ; the language you waited for
- ;;(go +lsp)          ; the hipster dialect
- ;;(haskell +lsp)     ; a language that's lazier than I am
- ;;hy                 ; readability of scheme w/ speed of python
- ;;idris              ;
- ;;(java +meghanada)  ; the poster child for carpal tunnel syndrome
- ;;julia              ; a better, faster MATLAB
- ;;kotlin             ; a better, slicker Java(Script)
- ;;(latex +fold)      ; writing papers in Emacs has never been so fun
- ;;lean
- ;;nim                ; python + lisp at the speed of c
- ;;ocaml              ; an objective camel
- ;;perl                ; write code no one else can comprehend
- ;;plantuml            ; diagrams for confusing people more
- ;;purescript          ; javascript, but functional
- ;;qt                  ; the 'cutest' gui framework ever
- ;;racket              ; a DSL for DSLs
- ;;raku                ; the artist formerly known as perl6
- ;;rst                 ; ReST in peace
- ;;rust                ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
- ;;scala               ; java, but good
+ ;;faust             ; dsp, but you get to keep your soul
+ ;;fortran           ; in FORTRAN, GOD is REAL (unless declared INTEGER)
+ ;;fsharp            ; ML stands for Microsoft's Language
+ ;;fstar             ; (dependent) types and (monadic) effects and Z3
+ ;;gdscript          ; the language you waited for
+ ;;(go +lsp)         ; the hipster dialect
+ ;;(graphql +lsp)    ; Give queries a REST
+ ;;(haskell +lsp)    ; a language that's lazier than I am
+ ;;hy                ; readability of scheme w/ speed of python
+ ;;idris             ; a language you can depend on
+ ;;(java +lsp)       ; the poster child for carpal tunnel syndrome
+ ;;julia             ; a better, faster MATLAB
+ ;;kotlin            ; a better, slicker Java(Script)
+ ;;latex             ; writing papers in Emacs has never been so fun
+ ;;lean              ; for folks with too much to prove
+ ;;nim               ; python + lisp at the speed of c
+ ;;ocaml             ; an objective camel
+ ;;plantuml          ; diagrams for confusing people more
+ ;;purescript        ; javascript, but functional
+ ;;qt                ; the 'cutest' gui framework ever
+ ;;racket            ; a DSL for DSLs
+ ;;raku              ; the artist formerly known as perl6
+ ;;rst               ; ReST in peace
+ ;;(rust +lsp)       ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
+ ;;scala             ; java, but good
  ;;sml
- ;;solidity            ; do you need a blockchain? No.
- ;;swift               ; who asked for emoji variables?
- ;;terra               ; Earth and Moon in alignment for performance.
- ;;zig                 ; C, but simpler
+ ;;solidity          ; do you need a blockchain? No.
+ ;;swift             ; who asked for emoji variables?
+ ;;terra             ; Earth and Moon in alignment for performance.
+ ;;zig               ; C, but simpler
 
  :email
  (mu4e +org)
@@ -208,4 +207,4 @@
 
  :config
  ;;literate
- (default +smartparens +bindings))
+ (default +bindings +smartparens))
