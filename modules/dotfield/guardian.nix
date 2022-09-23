6227684e7370d4fd9f93a0ccb08b2b5772b5bec4
Chris Montgomery <chris@cdom.io>
2022-09-22 20:35:40 -0400

    tests(users): safety checks for guardian user specification

--- modules/dotfield/guardian.nix
+++ modules/dotfield/guardian.nix
@@ -5,28 +5,50 @@
   pkgs,
   ...
 }: let
-  inherit (lib.types) nullOr str submodule;
-  inherit (lib.dotfield) mkOpt mkOpt' mkBoolOpt;
+  inherit (builtins) hasAttr;
+  inherit
+    (lib)
+    mkAliasDefinitions
+    mkEnableOption
+    mkIf
+    mkOption
+    optional
+    ;
+  inherit (lib.types) nullOr str;
 
   cfg = config.dotfield.guardian;
 in {
   options.dotfield.guardian = {
-    enable = lib.mkEnableOption "Whether to designate a guardian user for this system.";
-    username = lib.mkOption {
+    enable = mkEnableOption "Whether to designate a guardian user for this system.";
+    username = mkOption {
       type = nullOr str;
       default = null;
-      # FIXME: validate that this is an existing user.
       description = ''
         Name of the guardian user. Must be an existing non-system user.
       '';
     };
-    user = lib.mkOption {
+    user = mkOption {
       readOnly = true;
     };
-    autoLogin = lib.mkEnableOption "Whether to log the guardian user in automatically.";
+    autoLogin = mkEnableOption "Whether to log the guardian user in automatically.";
   };
-  config = lib.mkIf cfg.enable {
-    dotfield.guardian.user = lib.mkAliasDefinitions config.users.users.${cfg.username};
+  config = mkIf cfg.enable {
+    assertions = [
+      {
+        assertion = cfg.username != null;
+        message = "Username must be set when Guardian module enabled.";
+      }
+      {
+        assertion = hasAttr cfg.username config.users.users;
+        message = "Specified Guardian user '${cfg.username}' does not exist.";
+      }
+      {
+        assertion = config.users.users.${cfg.username}.isNormalUser;
+        message = "Specified user '${cfg.username}' must be a normal user.";
+      }
+    ];
+
+    dotfield.guardian.user = mkAliasDefinitions config.users.users.${cfg.username};
     users.groups."wheel".members = [cfg.username];
   };
 }
