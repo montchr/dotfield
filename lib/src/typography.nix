6112673b2feb3ec2513e1049f51d955086cfa897
Chris Montgomery <chris@cdom.io>
2022-09-26 15:38:17 -0400

    feat(lib|fonts): add functions for working with font weight names/values

---
+++ lib/src/typography.nix
@@ -0,0 +1,34 @@
+{lib, ...}: let
+  inherit
+    (lib)
+    findFirst
+    mapAttrs'
+    nameValuePair
+    toLower
+    ;
+
+  fontWeightDict = {
+    thin = 100;
+    extraLight = 200;
+    light = 300;
+    regular = 400;
+    medium = 500;
+    semibold = 600;
+    bold = 700;
+    extraBold = 800;
+    heavy = 900;
+  };
+  # Weight names normalised to lowercase.
+  fontWeightDict' = mapAttrs' (n: v: nameValuePair (toLower n) v) fontWeightDict;
+
+  withAliases = set:
+    set
+    // (with fontWeightDict; {
+      normal = regular;
+      black = heavy;
+      demibold = semibold;
+    });
+in {
+  fontWeightValue = name: (withAliases fontWeightDict').name;
+  fontWeightName = value: findFirst (n: v: v == value) fontWeightDict';
+}
