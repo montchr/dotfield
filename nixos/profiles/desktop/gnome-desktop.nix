1bb572643ba05e6a3aee5493b6e334dcc225100c
Chris Montgomery <chris@cdom.io>
2022-09-28 09:24:37 -0400

    feat(desktop:gnome): enable fun

--- nixos/profiles/desktop/gnome-desktop.nix
+++ nixos/profiles/desktop/gnome-desktop.nix
@@ -1,7 +1,9 @@
 {...}: {
   services.xserver.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+  services.gnome.games.enable = true;
   services.gnome.sushi.enable = true;
+  # FIXME: has no effect, prob cos gpg-agent is configured in home-manager?
   programs.gnupg.agent.pinentryFlavor = "gnome3";
   # Required for Firefox integration in home-manager
   services.gnome.chrome-gnome-shell.enable = true;
