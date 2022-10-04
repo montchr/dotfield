af02feaa43efca3b3cac990cfaaf74521241a783
Chris Montgomery <chris@cdom.io>
2022-10-04 17:44:24 -0400

    chore(doom|corfu): minad-capf.el->minad-capfs.el
    for consistency with other out-of-stream modules

--- home/users/seadoom/config/emacs/profiles/doom/modules/completion/corfu/autoload/minad-capfs.el
+++ home/users/seadoom/config/emacs/profiles/doom/modules/completion/corfu/autoload/minad-capfs.el
@@ -1,4 +1,4 @@
-;;; completion/corfu/autoload/minad-capf.el -*- lexical-binding: t; -*-
+;;; completion/corfu/autoload/minad-capfs.el -*- lexical-binding: t; -*-
 ;;
 ;; Copyright 2020 - 2022, Gerry Agbobada
 ;; https://git.sr.ht/~gagbo/doom-config/tree/31d0bd649b3eb97aebc319088e2674b0412e2beb/item/modules/completion/corfu/autoload/minad-capf.el
