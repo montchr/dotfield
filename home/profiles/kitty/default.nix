d1fccc521b3d44d97d08892c04c67f74bb94020f
Chris Montgomery <chris@cdom.io>
2022-10-14 13:43:50 -0400

    feat(kitty|theme): add some themes from kitty-themes + change defaults

--- home/profiles/kitty/default.nix
+++ home/profiles/kitty/default.nix
@@ -88,8 +88,14 @@ in {
   };
 
   xdg.configFile = {
-    "kitty/themes/dark.conf".text = mkColorScheme theme.colors.dark;
-    "kitty/themes/light.conf".text = mkColorScheme theme.colors.light;
+    "kitty/themes".source = ./themes;
+
+    # "kitty/themes/dark.conf".source = ./themes/dark/Substrata.conf;
+    # "kitty/themes/light.conf".source = ./themes/light/Alabaster.conf;
+
+    # FIXME: allow for specifying custom nix-colors-compatible color schemes
+    # "kitty/themes/dark.conf".text = mkColorScheme theme.colors.dark;
+    # "kitty/themes/light.conf".text = mkColorScheme theme.colors.light;
 
     # FIXME: does not appear to have an effect?
     "kitty/session".text = ''
