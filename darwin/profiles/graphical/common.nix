1ef1dffc28ce782a2a94167ec805238a988564f9
Chris Montgomery <chris@cdom.io>
2022-10-20 18:13:01 -0400

    tweak(darwin): adjust casks/mas apps

--- darwin/profiles/graphical/common.nix
+++ darwin/profiles/graphical/common.nix
@@ -17,8 +17,6 @@
   };
 
   homebrew.taps = [
-    # TODO: which cask is this for? sketchybar iirc?
-    # "FelixKratz/formulae"
     "homebrew/cask"
     "homebrew/cask-versions"
   ];
@@ -36,47 +34,31 @@
   ];
 
   homebrew.casks = [
-    # TODO: use creative cloud?
-    # "adobe-acrobat-reader"
     "alfred"
     "appcleaner"
     "bartender"
     "calibre"
-    "eloston-chromium" # aka "ungoogled-chromium" in nixpkgs
+    "eloston-chromium" #          <- aka "ungoogled-chromium" in nixpkgs
     "dropbox"
     "fantastical"
-    "firefox"
-    "firefox-developer-edition"
+    "firefox" #                   <- "home" browser
+    "firefox-developer-edition" # <- "work" browser
     "flameshot"
-    # "istat-menus" # TODO: replace w/some cli tools or scripty things for a bar?
-    "kap" # screen recorder... written w/next.js?!
-    # TODO: there might be a nix-darwin module for karabiner-elements soon
     "karabiner-elements"
     "mpv"
-    # "nextcloud"
     "plexamp"
-    "qlmarkdown"
-    "quicklook-json"
-    "quicklookase"
     "signal"
     "soundsource"
     "spotify"
-    "steermouse" # for input device mapping
+    "steermouse" # <- for input device mapping
     "vscodium"
-    "webpquicklook"
     "zoom"
   ];
 
-  # Disabled because these greatly slow down installation time.
   homebrew.masApps = {
     "DaisyDisk" = 411643860;
-    # "Deliveries" = 924726344;
-    # "Drafts" = 1435957248;
-    # "GoodTask" = 1143437985;
-    # "Keka" = 470158793;
-    # "NepTunes" = 1006739057;
-    # "New File Menu" = 1064959555;
-    # "Reeder" = 1529448980;
+    "Drafts" = 1435957248;
+    "Reeder" = 1529448980;
     # "Tailscale" = 1475387142;
   };
 }
