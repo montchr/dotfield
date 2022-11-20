73948cc1935a213518ccea61759e7d9dcbb677ba
Chris Montgomery <chris@cdom.io>
2022-11-20 18:49:42 -0500

    feat(zsh): add fzf-tab and zsh-autopair

--- home/profiles/shells/zsh/default.nix
+++ home/profiles/shells/zsh/default.nix
@@ -19,9 +19,12 @@ in {
     dotDir = ".config/zsh";
     enableCompletion = true;
     enableSyntaxHighlighting = true;
-    enableAutosuggestions = true;
     # enableVteIntegration = true;
     autocd = true;
+
+    # zsh-autosuggestions is loaded manually below to insure proper load order
+    # after fzf-tab.
+    enableAutosuggestions = false;
 
     defaultKeymap = "viins";
 
@@ -36,7 +39,32 @@ in {
 
     historySubstringSearch.enable = true;
 
-    # plugins = with pkgs; [];
+    plugins = [
+      # FIXME: requires a very-specific load order -- after compinit but before
+      # other plugins defining zle widgets. including it here causes fzf-tab to
+      # load after zsh-autosuggestions, which the fzf-tab readme explicitly
+      # warns against.
+      # https://github.com/Aloxaf/fzf-tab
+      {
+        name = "fzf-tab";
+        inherit (pkgs.zsh-fzf-tab) src;
+      }
+      {
+        name = "zsh-autosuggestions";
+        inherit (pkgs.zsh-autosuggestions) src;
+      }
+      {
+        name = "zsh-autopair";
+        # The `v1.0.0` version in nixpkgs is over a year old, so use a recent
+        # commit instead.
+        src = pkgs.fetchFromGitHub {
+          owner = "hlissner";
+          repo = "zsh-autopair";
+          rev = "396c38a7468458ba29011f2ad4112e4fd35f78e6";
+          sha256 = "sha256-PXHxPxFeoYXYMOC29YQKDdMnqTO0toyA7eJTSCV6PGE=";
+        };
+      }
+    ];
 
     initExtra = ''
       export DOTFIELD_USER_ZDOTDIR="$DOTFIELD_DIR/home/users/$USER/config/zsh"
--- home/users/cdom/config/zsh/functions.zsh
+++ home/users/cdom/config/zsh/functions.zsh
@@ -10,7 +10,8 @@ chpwd_ls() { exa --group-directories-first; }
 add-zsh-hook -Uz chpwd chpwd_ls
 
 # start new sessions from most recent dir
-# FIXME: `cdr` does not seem to exist...? doesn't work, but no visible errors
+# https://wiki.archlinux.org/title/zsh#cdr
+# FIXME: doesn't work, but no visible errors
 autoload -Uz chpwd_recent_dirs cdr add-zsh-hook
 add-zsh-hook chpwd chpwd_recent_dirs
 
