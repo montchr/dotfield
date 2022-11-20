98be7e50fc840741eb7082b4602d29028c311c29
Chris Montgomery <chris@cdom.io>
2022-11-20 14:22:50 -0500

    fix(tealdeer): use hm module with `cache_dir` setting

--- home/profiles/tealdeer.nix
+++ home/profiles/tealdeer.nix
@@ -1,29 +1,16 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}: {
-  home.packages = [pkgs.tealdeer];
-  home.sessionVariables = {
-    # FIXME: open PR on upstream hm to remove hard-coded config path on darwin
-    TEALDEER_CONFIG_DIR = "${config.xdg.configHome}/tealdeer";
-    # FIXME: open PR on upstream hm to add option to set this value (and ensure the directory exists)
-    TEALDEER_CACHE_DIR = "${config.xdg.cacheHome}/tealdeer";
+{config, ...}: let
+  inherit (config) xdg;
+in {
+  programs.tealdeer.enable = true;
+  programs.tealdeer.settings = {
+    display = {
+      use_pager = false;
+      compact = false;
+    };
+    updates = {
+      auto_update = true;
+      auto_update_interval_hours = 24 * 7;
+    };
+    directories.cache_dir = "${xdg.cacheHome}/tealdeer";
   };
-
-  # https://dbrgn.github.io/tealdeer/config.html
-  xdg.configFile."tealdeer/config.toml".text = ''
-    [display]
-    use_pager = false
-    compact = false
-
-    [updates]
-    auto_update = true
-    auto_update_interval_hours = 168 # 1 week
-  '';
-
-  home.activation.ensureTealdeerCacheDir = lib.hm.dag.entryAfter ["writeBoundary"] ''
-    $DRY_RUN_CMD mkdir -p $VERBOSE_ARG "${config.home.sessionVariables.TEALDEER_CACHE_DIR}"
-  '';
 }
