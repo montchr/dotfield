0cd4cf8219b1cd07f7b748f7fa7c49f1e5502909
Chris Montgomery <chris@cdom.io>
2022-11-20 16:41:48 -0500

    fix(zsh|nix): disable interactive shell comments for flake fragment compat

--- home/users/cdom/config/zsh/options.zsh
+++ home/users/cdom/config/zsh/options.zsh
@@ -14,7 +14,6 @@ setopt HASH_LIST_ALL
 setopt GLOBDOTS                 # Glob dotfiles as well.
 setopt COMBINING_CHARS          # Combine zero-length punc chars (accents) with base char
 setopt EXTENDED_GLOB            # Enable more powerful glob features
-setopt INTERACTIVE_COMMENTS     # Enable comments in interactive shell.
 setopt RC_QUOTES                # Allow 'Henry''s Garage' instead of 'Henry'\''s Garage'
 
 unsetopt CASE_GLOB        # Use case-insensitve globbing.
