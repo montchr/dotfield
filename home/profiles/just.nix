4e4c92feb49c4b14a1a0a53405723b9934d32e1e
Chris Montgomery <chris@cdom.io>
2022-11-20 21:15:14 -0500

    fix(tasks): load `just` completions properly

--- home/profiles/just.nix
+++ home/profiles/just.nix
@@ -1,3 +1,7 @@
-{...}: {
-  programs.just.enable = true;
+{pkgs, ...}: {
+  home.packages = [pkgs.just];
+  # FIXME: the upstream zsh completion loading mechanism is broken. installing
+  # the package normally works just fine (the module doesn't really do anything
+  # special if `home.extraOutputsToInstall` is configured properly)
+  programs.just.enable = false;
 }
