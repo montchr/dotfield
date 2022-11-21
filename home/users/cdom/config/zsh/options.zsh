2f69ad3730a2995f4aee96faeaf5159ca7e60915
Chris Montgomery <chris@cdom.io>
2022-11-20 21:52:08 -0500

    tweak(zsh): various shell option tweaks

--- home/users/cdom/config/zsh/options.zsh
+++ home/users/cdom/config/zsh/options.zsh
@@ -1,7 +1,7 @@
 ###: GENERAL ===================================================================
 
 # Treat these characters as part of a word.
-# WORDCHARS='_-*?[]~&.;!#$%^(){}<>'
+WORDCHARS='_-*?[]~&.;!#$%^(){}<>'
 
 # ls et al.
 AUTO_LS_COMMANDS="exa --oneline"
@@ -11,20 +11,26 @@ AUTO_LS_NEWLINE=false
 ###: INTERACTION/FEEDBACK ======================================================
 
 setopt HASH_LIST_ALL
-setopt GLOBDOTS                 # Glob dotfiles as well.
 setopt COMBINING_CHARS          # Combine zero-length punc chars (accents) with base char
-setopt EXTENDED_GLOB            # Enable more powerful glob features
 setopt RC_QUOTES                # Allow 'Henry''s Garage' instead of 'Henry'\''s Garage'
 
-unsetopt CASE_GLOB        # Use case-insensitve globbing.
+unsetopt BEEP             # And I thought I *liked* beeping...
+unsetopt MAIL_WARNING     # Don't print a warning message if a mail file has been accessed.
 
 
 ###: DIRECTORIES ===============================================================
 
+setopt AUTO_PUSHD           # Push the old directory onto the stack on cd.
+setopt CDABLE_VARS          # Change directory to a path stored in a variable.
+setopt EXTENDED_GLOB        # Enable more powerful glob features
+setopt GLOBDOTS             # Glob dotfiles as well.
+setopt MULTIOS              # Write to multiple descriptors.
 setopt PUSHD_IGNORE_DUPS    # Don't push multiple copies of the same directory onto the directory stack.
+setopt PUSHD_SILENT         # Do not print the directory stack after pushd or popd.
 setopt PUSHD_TO_HOME        # Push to home directory when no argument is given.
 
 unsetopt AUTO_NAME_DIRS     # Don't add variable-stored paths to ~ list
+unsetopt CASE_GLOB        # Use case-insensitve globbing.
 
 
 ###: PROCESSES ===============================================================
