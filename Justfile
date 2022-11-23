900cb1d4a9b9124ebbbf6531ff24c0c7be1446ef
Chris Montgomery <chris@cdom.io>
2022-11-22 20:14:29 -0500

    feat(devshells|tasks): offload all tasks into justfile

--- Justfile
+++ Justfile
@@ -1,19 +1,93 @@
 
+##: paths
+firefox-addons-dir := "${PRJ_ROOT}/packages/applications/firefox/firefox-addons"
+sources-dir := "${PRJ_ROOT}/packages/sources"
+
+
+###: LINTING/FORMATTING ========================================================
+
 # Format source files
 fmt *FILES='$PRJ_ROOT':
   treefmt --no-cache {{FILES}}
 
 # Format all changed source files
 fmt-changed:
+  @echo 'Formatting all changed source files in working tree'
   treefmt --no-cache `git diff --name-only --cached`
 
 # Check for Nix syntax errors
 evalnix file:
+  @echo "Checking {{file}} for syntax errors"
   nix-instantiate --parse --quiet {{file}} >/dev/null
 
 # Check Nix files for unused statements
 deadnix *ARGS='$PRJ_ROOT':
+  @echo 'Checking Nix files for dead code...'
   deadnix check --no-underscore --fail \
     --no-lambda-arg \
     --no-lambda-pattern-names \
     {{ARGS}}
+  @echo 'Done'
+
+###: SYSTEM ====================================================================
+
+sys := system
+
+cachix-cache-name := 'dotfield'
+cachix-exec := "cachix watch-exec --jobs 2 " + cachix-cache-name
+sys-rebuild := if os() == "linux" {
+  "nixos-rebuild"
+} else if os() == "macos" {
+  "darwin-rebuild"
+} else { "nix build" }
+
+system subcommand="build" *ARGS='':
+  {{cachix-exec}} {{sys-rebuild}} {{subcommand}} -- \
+    {{ARGS}} --flake $PRJ_ROOT --verbose
+
+
+###: UPDATES ===================================================================
+
+update-all: update-flake-inputs update-sources update-firefox-addons update-doom
+
+update-flake-inputs:
+  @echo 'Updating flake inputs...'
+  nix flake update --verbose
+
+update-doom:
+  @echo 'Updating Doom Emacs...'
+  doom upgrade
+
+update-firefox-addons dir=firefox-addons-dir:
+  @echo 'Updating Nix expressions for Firefox addons...'
+  mozilla-addons-to-nix {{dir}}/addons.json {{dir}}/addons.nix
+  @echo 'Done'
+
+update-sources dir=sources-dir:
+  @echo 'Updating external sources with nvfetcher...'
+  cd {{sources-dir}} && nvfetcher -c ./sources.toml
+  @echo 'Done'
+
+
+###: APPEARANCE ================================================================
+
+# Toggle the current kitty theme between light<->dark
+set-kitty-theme mode='dark':
+  @echo "Setting kitty terminal colors to {{mode}} scheme"
+  kitty @set-colors -a -c $KITTY_CONFIG_DIRECTORY/themes/{{mode}}.conf
+
+# Toggle the current Emacs theme between light<->dark
+toggle-emacs-theme:
+  @echo "Toggling Emacs modus-themes color scheme"
+  emacsclient --no-wait --eval "(modus-themes-toggle)" >/dev/null
+
+# Toggle the current macOS system theme between light<->dark
+[macos]
+toggle-macos-appearance:
+  @echo "Toggling macOS system appearance mode"
+  osascript -e 'tell app "System Events" to tell appearance preferences to set dark mode to not dark mode'
+
+# Toggle all application themes between light<->dark
+[macos]
+toggle-ui-mode: toggle-emacs-theme toggle-macos-appearance
+
--- devShells/default.nix
+++ devShells/default.nix
@@ -68,178 +68,60 @@
     cacheName = "dotfield";
     cachixExec = ''${l.getExe cachix} watch-exec --jobs 2 ${cacheName}'';
 
-    # FIXME: non-functional, needs re-work
-    # do-repl =
-    #   dotfield' {
-    #     name = "do-repl";
-    #     help = "a REPL for the system flake, by flake-utils-plus";
-    #     command = "${getExe fup-repl} $@";
-    #   };
-
-    # TODO: make argument optional to allow true toggling ... maybe symlinks?
-    do-theme-kitty = ui' {
-      name = "do-theme-kitty";
-      help = "toggle the current kitty theme between light<->dark";
-      command = ''
-        ${l.getExe pkgs.kitty} @set-colors -a -c \
-          $KITTY_CONFIG_DIRECTORY/themes/$1.conf
-      '';
-    };
-
-    do-theme-emacs = ui' {
-      name = "do-theme-emacs";
-      help = "toggle the current emacs theme between light<->dark";
-      command = ''
-        emacsclient --no-wait --eval "(modus-themes-toggle)" >/dev/null
-      '';
-    };
-
-    do-theme-macos = ui' {
-      name = "do-theme-macos";
-      help = "toggle the current macOS system theme between light<->dark";
-      command = ''
-        osascript -e 'tell app "System Events" to tell appearance preferences to set dark mode to not dark mode'
-      '';
-    };
-
-    do-theme-toggle = ui' {
-      name = "do-theme-toggle";
-      help = "toggle all application themes between light<->dark";
-      command = ''
-        ${l.optionalString isDarwin do-theme-macos.command}
-        # FIXME: this command requires light/dark input currently
-        # ${do-theme-kitty.command}
-        ${do-theme-emacs.command}
-      '';
-    };
-
-    do-update-nvfetcher-sources = dotfield' {
-      name = "do-update-nvfetcher-sources";
-      help = nvfetcher.meta.description;
-      command = ''
-        pushd $PRJ_ROOT/packages/sources
-        ${l.getExe nvfetcher} -c ./sources.toml $@
-        popd
-      '';
-    };
-
-    do-update-firefox-addons = dotfield' {
-      name = "do-update-firefox-addons";
+    mozilla-addons-to-nix-wrapped = utils' {
+      name = "mozilla-addons-to-nix";
       help = "Generate a Nix package set of Firefox add-ons from a JSON manifest.";
-      # N.B. As a Haskell package, including this flake's default package
-      # directly will break our own `nix flake check` due to IFD. It will also break
-      # CI SaaS (e.g. Garnix) when included as a flake input.
       command = ''
-        cd $PRJ_ROOT/packages/applications/firefox/firefox-addons
-        nix run sourcehut:~rycee/mozilla-addons-to-nix -- addons.json addons.nix
-        cd $PRJ_ROOT
+        nix run sourcehut:~rycee/mozilla-addons-to-nix -- $@
       '';
     };
 
-    do-update-all = dotfield' {
-      name = "do-update-all";
-      help = "update all of the things and build a new system derivation";
-      command = ''
-        set -e
-        nix flake update --verbose
-        ${do-update-nvfetcher-sources.command}
-        ${do-update-firefox-addons.command}
-        doom upgrade
-        ${do-sys-rebuild.command} build
-        ${do-sys-diff-next.command}
-      '';
-    };
+    commonCommands = [
+      ##: --- utils --------------------
 
-    do-sys-rebuild = dotfield' {
-      name = "do-rebuild-sys";
-      help = "run the system's rebuild command with the supplied operation arg";
-      command = ''
-        set -e
-        ${cachixExec} ${rebuildSystem} -- $@ --flake $PRJ_ROOT --verbose
-      '';
-    };
+      (utils cachix)
+      (utils just)
+      (utils manix)
+      (utils nix-diff)
+      (utils nix-tree)
+      mozilla-addons-to-nix-wrapped
 
-    do-sys-diff-next = utils' {
-      name = "do-sys-diff-next";
-      help = "compare the current system derivation with a project-local result";
-      command = ''
-        ${l.getExe nvd} diff /run/current-system $PRJ_ROOT/result
-      '';
-    };
+      ##: --- linters ------------------
 
-    do-sys-next = dotfield' {
-      name = "do-sys-next";
-      help = "switch to a new system generation";
-      command = ''
-        ${do-sys-rebuild.command} build
-        ${do-sys-diff-next.command}
-        sudo ${do-sys-rebuild.command} switch
-      '';
-    };
+      (linters editorconfig-checker)
+      (linters shellcheck)
+      (linters statix)
 
-    commonCommands =
-      [
-        ##: --- sys/hm mgmt --------------
+      ##: --- formatters ---------------
 
-        do-update-firefox-addons
-        do-update-nvfetcher-sources
-        do-update-all
-        do-sys-diff-next
-        do-sys-next
-        do-sys-rebuild
+      (formatters alejandra)
+      (formatters prettier)
+      (formatters treefmt)
 
-        ##: --- ui -----------------------
+      ##: --- secrets ------------------
 
-        do-theme-kitty
-        do-theme-emacs
-        do-theme-toggle
-      ]
-      ++ (l.optional isDarwin do-theme-macos)
-      ++ [
-        ##: --- utils --------------------
-
-        (utils cachix)
-        (utils just)
-        (utils manix)
-        (utils nix-diff)
-        (utils nix-tree)
-
-        ##: --- linters ------------------
-
-        (linters editorconfig-checker)
-        (linters shellcheck)
-        (linters statix)
-
-        ##: --- formatters ---------------
-
-        (formatters alejandra)
-        (formatters prettier)
-        (formatters treefmt)
-
-        ##: --- secrets ------------------
-
-        (secrets agenix)
-        (secrets rage)
-        (secrets sops)
-        (secrets ssh-to-age)
-        (secrets' {
-          name = "install-age-key";
-          help = "copy the age secret key from the password-store into place";
-          command = ''
-            mkdir -p $SOPS_AGE_KEY_DIR
-            ${pkgs.pass}/bin/pass show age--secret-key >> $SOPS_AGE_KEY_FILE
-          '';
-        })
-        (secrets' {
-          name = "convert-ssh-to-age-key";
-          help = "helper to convert the usual ssh ed25519 keys to age keys";
-          command = ''
-            mkdir -p $SOPS_AGE_KEY_DIR
-            ${l.getExe ssh-to-age} -private-key -i ~/.ssh/id_ed25519 > $SOPS_AGE_KEY_DIR/age-key.sec
-            ${l.getExe ssh-to-age} -i ~/.ssh/id_ed25519.pub > $SOPS_AGE_KEY_DIR/age-key.pub
-          '';
-        })
-      ];
+      (secrets agenix)
+      (secrets rage)
+      (secrets sops)
+      (secrets ssh-to-age)
+      (secrets' {
+        name = "install-age-key";
+        help = "copy the age secret key from the password-store into place";
+        command = ''
+          mkdir -p $SOPS_AGE_KEY_DIR
+          ${pkgs.pass}/bin/pass show age--secret-key >> $SOPS_AGE_KEY_FILE
+        '';
+      })
+      (secrets' {
+        name = "convert-ssh-to-age-key";
+        help = "helper to convert the usual ssh ed25519 keys to age keys";
+        command = ''
+          mkdir -p $SOPS_AGE_KEY_DIR
+          ${l.getExe ssh-to-age} -private-key -i ~/.ssh/id_ed25519 > $SOPS_AGE_KEY_DIR/age-key.sec
+          ${l.getExe ssh-to-age} -i ~/.ssh/id_ed25519.pub > $SOPS_AGE_KEY_DIR/age-key.pub
+        '';
+      })
+    ];
 
     linuxCommands = [
       (dotfield deploy-rs)
