98d9fc28c98d8ba56968ed58733a04edcba94d8e
Chris Montgomery <chris@cdom.io>
2022-12-01 15:55:44 -0500

    fix(darwin|cache|vm): disable `macos-builder` cache

--- darwin/profiles/virtualisation/nixos-vm-host.nix
+++ darwin/profiles/virtualisation/nixos-vm-host.nix
@@ -1,10 +1,8 @@
-_: let
-  substituters = ["https://macos-builder.cachix.org"];
-  trusted-substituters = substituters;
-in {
+{
   nix.settings = {
-    inherit substituters trusted-substituters;
-    trusted-public-keys = ["macos-builder.cachix.org-1:HPWcq59/iyqQz6HEtlO/kjD/a7ril0+/XJc+SZ2LgpI="];
+    # substituters = ["https://macos-builder.cachix.org"];
+    # trusted-substituters = ["https://macos-builder.cachix.org"];
+    # trusted-public-keys = ["macos-builder.cachix.org-1:HPWcq59/iyqQz6HEtlO/kjD/a7ril0+/XJc+SZ2LgpI="];
     # FIXME: error: The option `nix.settings.builders` is defined both null and not null, in
     # `/nix/store/2jbm1fsg3bg6jkkss200ryfrafdxd11q-source/darwin/profiles/virtualisation/nixos-vm-host.nix'
     # and `/nix/store/g0wrd3s71gh9ns7vjz6gf1gp6n6w2xf4-source/modules/nix'.
