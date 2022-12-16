eae48da3bbf930258af43f6c5a677bf002c951c5
Chris Montgomery <chris@cdom.io>
2022-12-15 20:53:24 -0500

    fix(fonts): disable excessive iosevka variants i don't use

--- profiles/fonts/iosevka-variants.nix
+++ profiles/fonts/iosevka-variants.nix
@@ -23,20 +23,5 @@ in {
     ##: width variants
     (makeIosevkaSgrVariant "fixed")
     (makeIosevkaSgrVariant "term")
-
-    ##: slab variants
-    (makeIosevkaSgrVariant "slab")
-    (makeIosevkaSgrVariant "fixed-slab")
-    (makeIosevkaSgrVariant "term-slab")
-
-    ##: curly variants
-    (makeIosevkaSgrVariant "curly")
-    (makeIosevkaSgrVariant "fixed-curly")
-    (makeIosevkaSgrVariant "term-curly")
-
-    ##: curly+slab variants
-    (makeIosevkaSgrVariant "curly-slab")
-    (makeIosevkaSgrVariant "fixed-curly-slab")
-    (makeIosevkaSgrVariant "term-curly-slab")
   ];
 }
