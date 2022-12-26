57742a8b4c0b778628a26e3f4f3b46b82339b0da
Chris Montgomery <chris@cdom.io>
2022-12-25 23:06:58 -0500

    feat(lib:strings): add incomplete function to convert to title case

--- lib/src/strings.nix
+++ lib/src/strings.nix
@@ -61,4 +61,17 @@ in rec {
   */
   trim = s: l.head (l.match "^[[:space:]]+|[[:space:]]+$" s);
 
+  # TODO: untested, unfinished, and probably not worth it
+  # simpleTitleCase = s: let
+  #   words = l.splitString (trim s);
+  #   lowerWords = ["of" "the" "as" "for" "but" "and" "a" "an" "or" "nor"];
+  #   maybeCapFirst = s: let
+  #     s' = l.toLower s;
+  #   in
+  #     if (! (l.elem s' lowerWords))
+  #     then (upperFirstChar s)
+  #     else s';
+  #   words' = l.map maybeCapFirst (l.drop 1 words);
+  # in
+  #   l.concatStringsSep " " ([(upperFirstChar (l.head words))] ++ words');
 }
