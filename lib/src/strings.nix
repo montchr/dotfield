60b60e755cccae8ddb289d067acf655894c0d737
Chris Montgomery <chris@cdom.io>
2022-12-25 23:04:25 -0500

    feat(lib:strings): add `lowerFirstChar` via `transformFirstChar`

--- lib/src/strings.nix
+++ lib/src/strings.nix
@@ -1,6 +1,6 @@
 {inputs, ...}: let
   l = inputs.nixpkgs.lib // builtins;
-in {
+in rec {
   /*
   Convert a boolean to a "yes" or "no" string.
 
@@ -12,13 +12,42 @@ in {
     else "no";
 
   /*
+  Apply a function to the first character in a string.
+
+  Type: transformFirstChar :: (string -> string) -> string -> string
+
+  Example:
+    transformFirstChar (s: s + s) "fnord"
+    => "ffnord"
+    transformFirstChar builtins.toUpper "fnord"
+    => "Fnord"
+  */
+  transformFirstChar = f: s: (f (l.substring 0 1 s)) + l.substring 1 (-1) s;
+
+  /*
+  Convert the first character of the given string to lowercase.
+
+  Type: lowerFirstChar :: string -> string
+
+  @partial
+
+  Example:
+    lowerFirstChar "MacOS"
+    => "macOS"
+  */
+  lowerFirstChar = transformFirstChar l.toLower;
+
+  /*
   Capitalise the first letter of the given string.
 
   Type: upperFirstChar :: string -> string
+
+  @partial
 
   Example:
     upperFirstChar "nixOS"
     => "NixOS"
   */
-  upperFirstChar = s: l.toUpper (l.substring 0 1 s) + l.substring 1 (-1) s;
+  upperFirstChar = transformFirstChar l.toUpper;
+
 }
