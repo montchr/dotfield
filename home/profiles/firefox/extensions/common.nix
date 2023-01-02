1da35d85af46522178e2086e63331835856431d2
Chris Montgomery <chris@cdom.io>
2023-01-02 14:57:05 -0500

    chore(firefox:addons): remove unused

--- home/profiles/firefox/extensions/common.nix
+++ home/profiles/firefox/extensions/common.nix
@@ -11,21 +11,15 @@ in {
     (with firefox-addons; [
       onepassword-password-manager
       a11ycss
-      add-custom-search-engine
       anchors-reveal
       auto-tab-discard
       bitwarden
       cookie-autodelete
       copy-selection-as-markdown
       darkreader
-      # display-_anchors
       # firefox-color
-      kagi-search
-      libredirect
       link-cleaner
-      # linkhints
       multi-account-containers
-      # octolinker
       offline-qr-code-generator
       old-reddit-redirect
       org-capture
@@ -34,9 +28,7 @@ in {
       reddit-enhancement-suite
       reduxdevtools
       refined-github
-      # sidebery
       single-file
-      skip-redirect
       tab-session-manager
       temporary-containers
       tree-style-tab
