c6067da44b75134972cd43fc59e79a10bf94fa2c
Chris Montgomery <chris@cdom.io>
2023-01-06 19:59:25 -0500

    feat(lib): add `numbers.mod` function from stockholm

--- lib/src/numbers.nix
+++ lib/src/numbers.nix
@@ -1,4 +1,16 @@
-_: {
+# TODO: rename to `num`
+{
+  l,
+  self,
+  ...
+}: {
+  /*
+  Type: mod :: int -> int
+
+  Source: <https://git.thalheim.io/Mic92/stockholm/src/commit/0b2952f4ed9572521f7c4a21904943ac33c602b0/lib/default.nix#L40>
+  */
+  mod = x: y: x - y * (x / y);
+
   /*
   Convert an integer to a floating-point number.
 
