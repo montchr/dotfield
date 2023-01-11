99303caec6d9193ef35234585502b5cf91b6bea1
Chris Montgomery <chris@cdom.io>
2023-01-11 11:21:16 -0500

    fix(darwin|emacs): install `tree-sitter` via homebrew for `emacs-plus`

--- darwin/profiles/emacs-plus.nix
+++ darwin/profiles/emacs-plus.nix
@@ -2,6 +2,9 @@
 {
   homebrew.taps = ["d12frosted/emacs-plus"];
   homebrew.brews = [
+    # Required to make emacs 29+ build with tree-sitter support.
+    # <https://github.com/d12frosted/homebrew-emacs-plus/issues/527>
+    "tree-sitter"
     {
       name = "emacs-plus@30";
       args = [
