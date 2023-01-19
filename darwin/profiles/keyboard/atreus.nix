3303f14ef40313a5d63967126fccacd3c570d8dd
Chris Montgomery <chris@cdom.io>
2023-01-19 09:52:01 -0500

    feat(keyboard|darwin): add keyboardio atreus profile w/chrysalis

---
+++ darwin/profiles/keyboard/atreus.nix
@@ -0,0 +1,8 @@
+###: Keyboardio Atreus
+#
+# Links:
+# - <https://shop.keyboard.io/pages/atreus>
+# - <https://github.com/keyboardio/Chrysalis>
+{
+  homebrew.casks = ["chrysalis"];
+}
--- darwin/roles.nix
+++ darwin/roles.nix
@@ -14,6 +14,7 @@
       bitwarden
       emacs-plus
       graphical
+      keyboard.atreus
       system-defaults
       virtualisation.containers
       yabai
