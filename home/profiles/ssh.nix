edd2414be14d7d27a9f90751fe3a10c66f6a43b2
Chris Montgomery <chris@cdom.io>
2023-01-28 21:03:28 -0500

    fix(ssh): set hostnames correctly in match blocks

--- home/profiles/ssh.nix
+++ home/profiles/ssh.nix
@@ -11,13 +11,13 @@ in {
     forwardAgent = false;
     serverAliveInterval = 300;
 
-    matchBlocks."hierophant".host = hosts.hierophant.ipv4.address;
+    matchBlocks."hierophant".hostname = hosts.hierophant.ipv4.address;
     matchBlocks."github.com" = {
       # inherit identityFile;
       # identitiesOnly = true;
       user = "git";
     };
-    matchBlocks."tsone".host = hosts.tsone.ipv4.address;
+    matchBlocks."tsone".hostname = hosts.tsone.ipv4.address;
 
     matchBlocks."eu.nixbuild.net" = {
       identityFile = "${homeDirectory}/.ssh/id_ed25519_nixbuildnet";
