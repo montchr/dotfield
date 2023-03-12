f24e0fd9cebd274a2c5748cc45a9659381d6ebbb
Chris Montgomery <chris@cdom.io>
2023-03-12 16:26:59 -0400

    fix(fzf|shell): ditch the broken `fre`, sanify options

--- home/profiles/fzf/default.nix
+++ home/profiles/fzf/default.nix
@@ -9,17 +9,14 @@
   inherit (config) theme;
   l = inputs.nixpkgs.lib // builtins;
 
-  procSub = s: "<(${s})";
+  # procSub = s: "<(${s})";
+
   packageCommand = pkg: args: (l.getExe pkg) + " " + (l.cli.toGNUCommandLineShell {} args);
 
   find = packageCommand pkgs.fd;
   findFiles = args: find (args // {type = "f";});
   findDirs = args: find (args // {type = "d";});
   # list = packageCommand pkgs.exa;
-  freCommand = packageCommand packages.fre {sorted = true;};
-
-  toSourcesCommand = srcs: "command cat " + (l.concatMapStringsSep " " procSub srcs);
-  toSourcesCommand' = srcs: toSourcesCommand ([freCommand] ++ srcs);
 
   dirPreviewCommand = l.getExe pkgs.exa + " --tree {} | head -n 200";
 in {
@@ -27,9 +24,9 @@ in {
 
   programs.fzf = {
     enable = true;
-    defaultCommand = "${findFiles {}} 2>/dev/null";
+    # defaultCommand = "${findFiles {}} 2>/dev/null";
     defaultOptions =
-      ["--ansi" "--reverse"]
+      ["--ansi" "--reverse" "--border" "--inline-info"]
       ++ (l.optional theme.enable "--color=16");
 
     ##: --- files ---
@@ -43,14 +40,9 @@ in {
 
     ##: --- directories ---
 
-    changeDirWidgetCommand = toSourcesCommand' [
-      (findDirs {})
-      ((findDirs {}) + " . ~")
-    ];
+    changeDirWidgetCommand = findDirs {};
     changeDirWidgetOptions = [
       "--tiebreak=index"
-      # `fre` will take care of sorting
-      "--no-sort"
       "--preview '${dirPreviewCommand}'"
     ];
 
@@ -58,46 +50,4 @@ in {
 
     historyWidgetOptions = ["--sort" "--exact"];
   };
-
-  ###: --- `fre` configuration -------------------------------------------------
-
-  programs.bash.initExtra = ''
-    __fre_pwd() {
-      builtin pwd -L
-    }
-    __fre_oldpwd="$(__fre_pwd)"
-    __fre_hook() {
-      \builtin local -r retval="$?"
-      \builtin local pwd_tmp
-      pwd_tmp="$(__fre_pwd)"
-      if [[ $__fre_oldpwd != "$pwd_tmp" ]]; then
-        __fre_oldpwd="$pwd_tmp"
-        \command fre --add "$__fre_oldpwd"
-      fi
-      return "$retval"
-    }
-    if [[ ''${PROMPT_COMMAND:=} != *'__fre_hook'* ]]; then
-      PROMPT_COMMAND="__fre_hook;''${PROMPT_COMMAND#;}"
-    fi
-  '';
-  programs.zsh.initExtra = ''
-    __fre_pwd() {
-      \builtin pwd -L
-    }
-    __fre_hook() {
-      fre --add "$(__fre_pwd)"
-    }
-    if [[ ''${precmd_functions[(Ie)__fre_hook]:-} -eq 0 ]] && [[ ''${chpwd_functions[(Ie)__fre_hook]:-} -eq 0 ]]; then
-      chpwd_functions+=(__fre_hook)
-    fi
-  '';
-  programs.fish.shellInit = ''
-    function __fre_pwd
-      builtin pwd -L
-    end
-    function __fre_hook --on-variable PWD
-      test -z "$fish_private_mode"
-      and command fre --add (__fre_pwd)
-    end
-  '';
 }
