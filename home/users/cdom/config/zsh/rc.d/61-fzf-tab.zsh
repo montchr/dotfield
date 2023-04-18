732fb02f5b8bea848655d6b6a3c0e3fe2f140ca8
Chris Montgomery <chris@cdom.io>
2023-04-17 22:03:50 -0400

    fix(fzf|zsh): add required padding for border ui support

--- home/users/cdom/config/zsh/rc.d/61-fzf-tab.zsh
+++ home/users/cdom/config/zsh/rc.d/61-fzf-tab.zsh
@@ -4,6 +4,12 @@
 
 # Preview directory contents
 zstyle ':fzf-tab:complete:cd:*' fzf-preview 'exa -1 --color=always $realpath'
+# Border requires extra padding to prevent truncated results.
+# <https://github.com/Aloxaf/fzf-tab/wiki/Configuration#fzf-pad>
+if [[ "$FZF_DEFAULT_OPTS" =~ "--border" ]]; then
+  zstyle ':fzf-tab:complete:*' fzf-pad '4'
+fi
+
 
 # Switch completion group using `,` and `.`
 zstyle ':fzf-tab:*' switch-group ',' '.'
