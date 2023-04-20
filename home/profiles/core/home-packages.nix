8ad20e41ce0b10c394d65cc47d0fa46ed44ca7d5
Chris Montgomery <chris@cdom.io>
2023-04-19 22:13:56 -0400

    feat: add some color tools

--- home/profiles/core/home-packages.nix
+++ home/profiles/core/home-packages.nix
@@ -4,20 +4,35 @@
   ...
 }: {
   home.packages = [
-    pkgs.atool #     <- archive command line helper                       => <https://www.nongnu.org/atool/>
-    pkgs.doggo #     <- command-line DNS client for humans                => <https://github.com/mr-karan/doggo>
-    pkgs.duf #       <- better du/df alternative                          => <https://github.com/muesli/duf/>
-    pkgs.entr #      <- run commands when files change
-    pkgs.fortune #   <- display pseudorandom quotations
+    ##: color utils
+    pkgs.colorpanes #  <- print panes in the 8 bright terminal colors with shadows of the respective darker color
+    pkgs.sanctity #    <- ruSt ANsi16 Color Test utIliTY
+    pkgs.pastel #      <- generate, analyze, convert and manipulate colors
+    (pkgs.writeShellApplication {
+      name = "color-panic";
+      runtimeInputs = [pkgs.colorpanes];
+      text = ''
+        colorpanes --captions --height 38 --width 24
+      '';
+    })
+
+    ##: help me...
     pkgs.fx #        <- interactive terminal json viewer                  => <https://github.com/antonmedv/fx>
     pkgs.glow #      <- charmbracelet's markdown cli renderer
-    pkgs.grex #      <- generate regexps from user-provided test cases
-    pkgs.hexyl #     <- command-line hex viewer
-    pkgs.lf #        <- yet another ranger-like file manager tui          => <https://github.com/gokcehan/lf/wiki/>
-    pkgs.ouch #      <- Obvious Unified Compression Helper                => <https://github.com/ouch-org/ouch>
-    pkgs.pastel #    <- generate, analyze, convert and manipulate colors
-    pkgs.treefmt #   <- one cli to format the code tree
     pkgs.moreutils # <- almost core utils
+    pkgs.treefmt #   <- one cli to format the code tree
+    pkgs.watchexec # <- run commands when files change
+
+    ##: [lukewarm]
+    pkgs.grex #      <- generate regexps from user-provided test cases
+    pkgs.lf #        <- yet another ranger-like file manager tui          => <https://github.com/gokcehan/lf/wiki/>
+
+    ##: [TODO]
+    pkgs.doggo #     <- command-line DNS client for humans                => <https://github.com/mr-karan/doggo>
+    pkgs.duf #       <- better du/df alternative                          => <https://github.com/muesli/duf/>
+    pkgs.fortune #   <- display pseudorandom quotations
+    pkgs.hexyl #     <- command-line hex viewer
+    pkgs.ouch #      <- Obvious Unified Compression Helper                => <https://github.com/ouch-org/ouch>
 
     packages.ddi #   <- "nice dd setup for most cases"  => <https://git.sr.ht/~rycee/configurations/tree/1af2ef3d4c8778b0fb2b12934d3a3f1766ce1d9f/item/user/common.nix#L62-66>
   ];
