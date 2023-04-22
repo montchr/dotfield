04be0639ca1fa88b738b88ff8767659dc2a44836
Chris Montgomery <chris@cdom.io>
2023-04-21 20:33:21 -0400

    fix(boot): s/boot.cleanTmpDir/boot.tmp.cleanOnBoot/

--- nixos/profiles/boot/common.nix
+++ nixos/profiles/boot/common.nix
@@ -1,12 +1,12 @@
 {lib, ...}: {
   boot = {
-    # TODO: consider setting this at some higher level... VMs may not want this...
-    cleanTmpDir = lib.mkDefault true;
     consoleLogLevel = lib.mkDefault 3;
 
     # Enable all Magic SysRq functions via ALT-PrtSc-<command key>
     # https://www.kernel.org/doc/html/latest/admin-guide/sysrq.html
     # FIXME: GNOME overrides the default Magic SysRq binding to take a screenshot.
     kernel.sysctl."kernel/sysrq" = 1;
+
+    tmp.cleanOnBoot = lib.mkDefault true;
   };
 }
