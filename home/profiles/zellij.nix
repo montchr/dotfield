5c0a0bd1853dca3fae4206eb8d6597ca3f76897b
Chris Montgomery <chris@cdom.io>
2023-04-30 14:44:32 -0400

    feat(zellij): oos-link configs from user dir

--- home/profiles/zellij.nix
+++ home/profiles/zellij.nix
@@ -1,3 +1,7 @@
-{...}: {
+{config, ...}: let
+  inherit (config.lib.file) mkOutOfStoreSymlink;
+  configsDir = config.dotfield.paths.userDirs.configs;
+in {
   programs.zellij.enable = true;
+  xdg.configFile."zellij".source = mkOutOfStoreSymlink "${configsDir}/zellij";
 }
