e558ccb9b3c778a701e9b3269d4ac8accc418893
Chris Montgomery <chris@cdom.io>
2023-05-10 20:37:58 -0400

    fix(zellij): use non-broken config source

--- home/profiles/zellij.nix
+++ home/profiles/zellij.nix
@@ -1,7 +1,10 @@
 {config, ...}: let
+  userConfigsBase = "${config.xdg.configHome}/dotfield/home/users/cdom/config";
   inherit (config.lib.file) mkOutOfStoreSymlink;
-  inherit (config.dotfield.paths.userDirs) configsPath;
+  # FIXME: broken, of course
+  # inherit (config.dotfield.paths.userDirs) configsPath;
 in {
   programs.zellij.enable = true;
-  xdg.configFile."zellij".source = mkOutOfStoreSymlink "${configsPath}/zellij";
+  xdg.configFile."zellij".source =
+    mkOutOfStoreSymlink "${userConfigsBase}/zellij";
 }
