1af64b93bd762619b467cfab867f5287663b5252
Chris Montgomery <chris@cdom.io>
2023-05-27 17:11:40 -0400

    feat(zsh): add omz plugins

--- home/users/cdom/config/zsh/rc.d/50-plugins.zsh
+++ home/users/cdom/config/zsh/rc.d/50-plugins.zsh
@@ -12,6 +12,12 @@ if ! zgenom saved; then
 
   zgenom compdef
 
+  zgenom ohmyzsh # <- required base lib
+
+  zgenom ohmyzsh plugins/aws
+  zgenom ohmyzsh plugins/docker-compose
+  zgenom ohmyzsh plugins/docker
+
   # NOTE: Be extra careful about plugin load order, or subtle breakage can emerge.
   #       fzf-tab, for example, must be loaded before any widgets.
   zgenom load Aloxaf/fzf-tab
