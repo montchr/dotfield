9fb0fde3caede39a929306e2c26c5b71b28e699f
Chris Montgomery <chris@cdom.io>
2023-06-18 20:40:58 -0400

    fix(firefox:lepton|packages): firefox-ui-fix: 7.3.0 -> 7.3.1

--- packages/data/themes/firefox-ui-fix/default.nix
+++ packages/data/themes/firefox-ui-fix/default.nix
@@ -5,13 +5,13 @@
 }:
 stdenv.mkDerivation rec {
   pname = "firefox-ui-fix";
-  version = "7.3.0";
+  version = "7.3.1";
 
   src = fetchFromGitHub {
     owner = "black7375";
     repo = "Firefox-UI-Fix";
     rev = "v${version}";
-    sha256 = "sha256-hzUlPf9tGpfZ6I4u4W8j0Gp9XATSAUQN61G9c14mOq4=";
+    sha256 = "sha256-zyW/nZPoee/S0f4KLH1sveeYjuWJetlChpwCfeSXomM=";
   };
 
   installPhase = ''
