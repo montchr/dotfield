32488971749098a80129ee31d548eb3b1c55a21c
Chris Montgomery <chris@cdom.io>
2023-06-18 20:37:16 -0400

    fix: remove broken nix-doc tool and module

--- profiles/homeProfiles/development/nix-tools.nix
+++ profiles/homeProfiles/development/nix-tools.nix
@@ -10,9 +10,4 @@
     pkgs.alejandra
     pkgs.nix-init #   <- generate nix package expressions from url
   ];
-
-  ##: cli+repl docs + ctags nix plugin
-  programs.nix-doc.enable = true;
-  # FIXME: see note in module
-  # programs.nix-doc.plugin.enable = true;
 }
