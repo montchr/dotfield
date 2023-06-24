7bd84823da3a297ff255fdaef9085117f3640cb9
Chris Montgomery <chris@cdom.io>
2023-06-24 11:58:44 -0400

    feat(keys): add blink@phione1 to default keys

--- ops/users/cdom/keys/default.nix
+++ ops/users/cdom/keys/default.nix
@@ -5,7 +5,10 @@
 }: let
   inherit (metadata) hosts;
 in
-  [(keys.ssh."0x135EEDD0F71934F3")]
+  [
+    keys.ssh."0x135EEDD0F71934F3"
+    keys.ssh.blink-at-phione1
+  ]
   ++ hosts.boschic.users.seadoom.keys
   ++ hosts.brakhage.users.blink.keys
   ++ hosts.hodgepodge.users.seadoom.keys
