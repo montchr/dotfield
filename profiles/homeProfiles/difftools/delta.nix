e846c5769437841984688c2c965040ad56e50049
Chris Montgomery <chris@cdom.io>
2023-06-29 11:46:09 -0400

    fix(delta|git|theme:color): make delta respect current scheme

--- profiles/homeProfiles/difftools/delta.nix
+++ profiles/homeProfiles/difftools/delta.nix
@@ -1,7 +1,10 @@
-{
+{config, ...}: let
+  themePolarity = config.theme.color.schemes.default.kind;
+in {
   programs.git.delta = {
     enable = true;
     options = {
+      light = themePolarity == "light";
       line-numbers = true;
       navigate = true;
       keep-plus-minus-markers = true;
