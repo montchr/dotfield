69f3ec0dd1cc33d6e8bc1ba946cebe9f8c555217
Chris Montgomery <chris@cdom.io>
2023-07-03 16:20:01 -0400

    feat(hierophant): use cloudflare dns
    Since we're already using its proxy.

--- machines/hierophant/networking.nix
+++ machines/hierophant/networking.nix
@@ -1,5 +1,7 @@
-{lib, ...}: {
-  networking.useDHCP = lib.mkDefault true;
+{flake, ...}: let
+  inherit (flake.inputs.apparat.constants.networking) dns;
+in {
+  networking.nameservers = dns.nameservers.cloudflare;
 
   # Allow this host to function as a Tailscale exit node.
   services.tailscale.useRoutingFeatures = "both";
