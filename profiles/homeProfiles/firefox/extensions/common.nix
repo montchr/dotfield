4057a5d8a1262fb37f3cf12defedf6ff6ee3287d
Chris Montgomery <chris@cdom.io>
2023-07-05 11:08:21 -0400

    feat(firefox:addons): add simple-tab-groups extensions

--- profiles/homeProfiles/firefox/extensions/common.nix
+++ profiles/homeProfiles/firefox/extensions/common.nix
@@ -19,12 +19,21 @@ with addons; [
   reddit-enhancement-suite
   reduxdevtools
   refined-github
-  simple-tab-groups
   stylus
   tab-session-manager
   temporary-containers
   tridactyl
   violentmonkey #                     <- <https://github.com/violentmonkey/violentmonkey>
+
+  ##: Simple Tab Groups + Extensions
+  simple-tab-groups
+  stg-plugin-create-new-group
+  stg-plugin-create-new-tab
+  stg-plugin-create-temp-tab
+  stg-plugin-del-current-group
+  stg-plugin-group-notes
+  stg-plugin-load-custom-group
+  stg-plugin-manage-groups
 
   ##: TST + Extensions
   tree-style-tab
