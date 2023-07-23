20128effd356b59135612235c67908c125ab2e0f
Chris Montgomery <chris@cdom.io>
2023-07-23 18:03:58 -0400

    gnome: enable autologin crash workaround conditionally in profile
    No need for individual hosts to set this.

--- machines/tuvok/users/cdom.nix
+++ machines/tuvok/users/cdom.nix
@@ -13,13 +13,8 @@ in {
     openssh.authorizedKeys.keys = ops.users.cdom.keys.default;
   };
 
-  # Enable automatic login for the user.
   services.xserver.displayManager.autoLogin.enable = true;
   services.xserver.displayManager.autoLogin.user = username;
-
-  # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
-  systemd.services."getty@tty1".enable = false;
-  systemd.services."autovt@tty1".enable = false;
 
   home-manager.users.${username} = hmArgs: {
     imports = with hmArgs.roles; workstation;
--- profiles/nixosProfiles/desktop/gnome-desktop.nix
+++ profiles/nixosProfiles/desktop/gnome-desktop.nix
@@ -1,9 +1,22 @@
-_: {
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}: let
+  isAutoLoginEnabled = config.services.xserver.displayManager.autoLogin.enable;
+in {
   services.xserver.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
   services.gnome.games.enable = true;
   services.gnome.sushi.enable = true;
   # FIXME: has no effect, prob cos gpg-agent is configured in home-manager?
+
+  # Prevent GNOME session crashes when auto-login is enabled.
+  # <https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229>
+  systemd.services."getty@tty1".enable = lib.mkDefault (!isAutoLoginEnabled);
+  systemd.services."autovt@tty1".enable = lib.mkDefault (!isAutoLoginEnabled);
+
   programs.gnupg.agent.pinentryFlavor = "gnome3";
   # Required for browser integrations with GNOME services.
   services.gnome.gnome-browser-connector.enable = true;
