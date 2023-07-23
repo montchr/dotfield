fd126e119d3d2052aef2a7aeb0e292b8388ae4a7
Chris Montgomery <chris@cdom.io>
2023-07-23 18:49:07 -0400

    boot: reduce configuration limit to a more sensible number

--- profiles/nixosProfiles/boot/systemd-boot.nix
+++ profiles/nixosProfiles/boot/systemd-boot.nix
@@ -1,14 +1,8 @@
 {lib, ...}: {
-  boot.loader = {
-    grub.enable = false;
-    systemd-boot = {
-      enable = true;
-      consoleMode = lib.mkDefault "auto";
-      # This number is super low -- it can safely be bumped higher, but be
-      # careful otherwise you may fill up a boot partition pretty quickly in
-      # relation to increased frequency of rebuilds/generations.
-      configurationLimit = lib.mkDefault 24;
-      editor = false;
-    };
+  boot.loader.systemd-boot = {
+    enable = true;
+    consoleMode = lib.mkDefault "auto";
+    configurationLimit = lib.mkDefault 10;
+    editor = false;
   };
 }
