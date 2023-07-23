884b9c22e8f7038e9030285d5c6f03137214b1ae
Chris Montgomery <chris@cdom.io>
2023-07-23 18:55:42 -0400

    gnome: remove redundant settings
    same as defaults

--- profiles/nixosProfiles/desktop/gnome-desktop.nix
+++ profiles/nixosProfiles/desktop/gnome-desktop.nix
@@ -11,8 +11,6 @@ in {
 
   services.gnome.core-developer-tools.enable = true;
   services.gnome.games.enable = true;
-  services.gnome.sushi.enable = true;
-  # FIXME: has no effect, prob cos gpg-agent is configured in home-manager?
 
   # Prevent GNOME session crashes when auto-login is enabled.
   # <https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229>
@@ -20,6 +18,4 @@ in {
   systemd.services."autovt@tty1".enable = lib.mkDefault (!isAutoLoginEnabled);
 
   programs.gnupg.agent.pinentryFlavor = "gnome3";
-  # Required for browser integrations with GNOME services.
-  services.gnome.gnome-browser-connector.enable = true;
 }
