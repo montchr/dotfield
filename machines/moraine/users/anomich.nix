0943a55dcbd279085566e1cfd89339fd4a4b24fa
Chris Montgomery <chris@cdom.io>
2023-07-28 19:32:21 -0400

    feat(moraine): set user anomich shell to fish via bash

--- machines/moraine/users/anomich.nix
+++ machines/moraine/users/anomich.nix
@@ -1,6 +1,7 @@
 {
   config,
   ops,
+  pkgs,
   ...
 }: let
   username = "anomich";
@@ -12,9 +13,14 @@ in {
     extraGroups = ["wheel"];
     passwordFile = config.sops.secrets."user-${username}-password".path;
     openssh.authorizedKeys.keys = ops.users.cdom.keys.default;
+    # Loads fish shell on interactive init.
+    shell = pkgs.bashInteractive;
   };
 
   home-manager.users.${username} = hmArgs: {
+    imports = [
+      hmArgs.profiles.shells.fish.default
+    ];
     home.stateVersion = "23.05";
   };
 }
