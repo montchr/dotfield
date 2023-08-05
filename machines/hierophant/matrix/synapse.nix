bb46a83b97f5909dc14b399969ab4c5fc9bca6d9
Chris Montgomery <chris@cdom.io>
2023-08-05 16:55:39 -0400

    fix(matrix): specify `smtp_user`

--- machines/hierophant/matrix/synapse.nix
+++ machines/hierophant/matrix/synapse.nix
@@ -55,11 +55,9 @@ in {
   };
 
   services.matrix-synapse.extraConfigFiles = [
-    (config.sops.secrets."matrix-synapse/registration-shared-secret-yaml".path)
     (config.sops.secrets."matrix-synapse/email-config-yaml".path)
-
-    # FIXME: needs update after domain change
     # (config.sops.secrets."matrix-synapse/recaptcha-private-key-yaml".path)
+    (config.sops.secrets."matrix-synapse/registration-shared-secret-yaml".path)
   ];
 
   # NOTE: Database created manually.
