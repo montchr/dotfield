cd8a0226988eaabbe535c824ffa9340b92e33206
Chris Montgomery <chris@cdom.io>
2023-08-31 19:40:35 -0400

    feat(shells:nu): add a working basic config

--- profiles/homeProfiles/shells/nushell/default.nix
+++ profiles/homeProfiles/shells/nushell/default.nix
@@ -4,15 +4,23 @@
   lib,
   ...
 }: let
-  inherit (config) home;
+  inherit (config.home) username;
+  # FIXME: hardcoded
+  userConfigDir = "~/.config/dotfield/users/${username}/config/nushell";
 in {
   imports = [../common.nix];
+  # Handle manually.
+  programs.direnv.enableNushellIntegration = false;
   programs.nushell = {
     enable = true;
+    extraConfig = ''
+      source ${userConfigDir}/config.nu
+    '';
+    # extraEnv = ''
+    #   source ${userConfigDir}/env.nu
+    # '';
+    # configFile.source = ./config.nu;
+    # envFile.source = ./env.nu;
+    # extraConfig = builtins.readFile ./config.nu;
   };
-  xdg.configFile."nushell/home.nu".source = pkgs.writeText "${home.username}-home.nu" ''
-    ${
-      lib.concatStringsSep "\n" (lib.mapAttrsToList (name: value: "$env.${name} = \"${value}\"") home.sessionVariables)
-    }
-  '';
 }
