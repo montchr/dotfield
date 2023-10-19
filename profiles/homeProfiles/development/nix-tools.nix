e947a8e96c01304ef0a5ee22877429de9873a44b
Chris Montgomery <chris@cdom.io>
2023-10-19 12:26:34 -0400

    feat: add nixpkgs-review

--- profiles/homeProfiles/development/nix-tools.nix
+++ profiles/homeProfiles/development/nix-tools.nix
@@ -3,6 +3,7 @@
     pkgs.alejandra
     pkgs.nix-init #   <- generate nix package expressions from url
     pkgs.nix-melt #   <- flake.lock explorer
+    pkgs.nixpkgs-review
     pkgs.nvd #        <- diff package changes between versions
   ];
 }
