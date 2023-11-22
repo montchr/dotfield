bfd6b024c64f48923c7c5147a87187c08d7f4b02
Chris Montgomery <chris@cdom.io>
2023-11-22 12:10:02 -0500

    fix(zsh): fall back to running command verbatim if no glob match

--- users/cdom/config/zsh/main.zsh
+++ users/cdom/config/zsh/main.zsh
@@ -1,3 +1,6 @@
 #!/usr/bin/env zsh
 
-# silence is bolden
+# fix for the big caveat when using `extendedglob` where `^` chars break command
+# if the glob doesn't match anything, then just run the thing verbatim, duh come one come on
+# <https://github.com/ohmyzsh/ohmyzsh/issues/449#issuecomment-6973425>
+unsetopt nomatch
