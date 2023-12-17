df9d4a7a04b5cf2b922778bd53d86cf2f6518a25
Chris Montgomery <chris@cdom.io>
2023-12-16 20:54:59 -0500

    fix(git-sync): actually enable the service

--- profiles/homeProfiles/git-sync.nix
+++ profiles/homeProfiles/git-sync.nix
@@ -14,13 +14,14 @@
   gh = path: repo path "git@github.com:${path}.git";
   srht = path: repo path "git@git.sr.ht:~${path}";
 in {
+  services.git-sync.enable = true;
   services.git-sync.repositories = lib.listToAttrs [
     (gh "doomemacs/doomemacs")
     (gh "purcell/emacs.d")
     (gh "bbatsov/prelude")
     (gh "noctuid/general.el")
     # TODO: reduce interval
-    (gh "nixos/nixpkgs")
+    # (gh "nixos/nixpkgs")
     (gh "nix-community/home-manager")
     # TODO: remove (just here as test)
     (srht "montchr/password-store")
