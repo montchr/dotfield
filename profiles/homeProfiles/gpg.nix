7da2f5b6368f6337ab745585464aa6fcbe450ec6
Chris Montgomery <chris@cdom.io>
2024-01-15 02:53:47 -0500

    Revert "fix(gpg): use upstream gpg-agent module again"
    This reverts commit fea19c6238b37ceb0f259532900d7ba0f255d19d.

--- profiles/homeProfiles/gpg.nix
+++ profiles/homeProfiles/gpg.nix
@@ -1,15 +1,20 @@
 {
   config,
-  lib,
   pkgs,
   flake,
   ops,
   ...
 }: let
-  inherit (pkgs.stdenv.hostPlatform) isDarwin;
   key = (config.dotfield.whoami).pgp;
+  inherit (pkgs.stdenv.hostPlatform) isDarwin;
+  l = flake.inputs.nixpkgs.lib // builtins;
 in {
-  config = lib.mkIf ("" != key) {
+  # Use our fork of these modules while still pending upstream changes.
+  # FIXME: https://github.com/nix-community/home-manager/pull/2964
+  imports = [(flake.inputs.home-manager-gpg-agent-darwin + "/modules/services/gpg-agent.nix")];
+  disabledModules = ["services/gpg-agent.nix"];
+
+  config = l.mkIf ("" != key) {
     home.sessionVariables.DOTFIELD_PGP_KEY = key;
 
     home.packages = with pkgs; [
@@ -24,8 +29,7 @@ in {
     services.gpg-agent = {
       enable = true;
       enableSshSupport = false;
-      # TODO: still necessary?
-      pinentryFlavor = lib.mkIf isDarwin "mac";
+      pinentryFlavor = l.mkIf isDarwin "mac";
       sshKeys = [key];
     };
 
