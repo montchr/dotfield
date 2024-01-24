0b5a67abc95450af748f47552ded898dc415e576
Chris Montgomery <chris@cdom.io>
2024-01-23 21:31:36 -0500

    tweak(clipboard|keyboard): add a bunch of comment garbage

--- profiles/homeProfiles/desktop/clipboard.nix
+++ profiles/homeProfiles/desktop/clipboard.nix
@@ -6,22 +6,19 @@
 }: let
   inherit (pkgs.stdenv.hostPlatform) isLinux;
 in {
-  services.cliphist.enable = true;
+  # FIXME: only on wlroots
+  # services.cliphist.enable = true;
 
-  home.packages =
-    [
-      config.services.cliphist.package
-    ]
-    ++ lib.optionals isLinux [
-      pkgs.wl-clipboard
+  home.packages = lib.optionals isLinux [
+    pkgs.wl-clipboard
 
-      # via <https://github.com/sentriz/cliphist#picker-examples>
-      (pkgs.writeShellApplication {
-        name = "clipsel";
-        runtimeInputs = [pkgs.cliphist pkgs.fzf pkgs.wl-clipboard];
-        text = ''
-          cliphist list | fzf | cliphist decode | wl-copy
-        '';
-      })
-    ];
+    # via <https://github.com/sentriz/cliphist#picker-examples>
+    # (pkgs.writeShellApplication {
+    #   name = "clipsel";
+    #   runtimeInputs = [pkgs.cliphist pkgs.fzf pkgs.wl-clipboard];
+    #   text = ''
+    #     cliphist list | fzf | cliphist decode | wl-copy
+    #   '';
+    # })
+  ];
 }
--- profiles/nixosProfiles/hardware/keyboard.nix
+++ profiles/nixosProfiles/hardware/keyboard.nix
@@ -1,7 +1,7 @@
 {pkgs, ...}: {
   hardware.keyboard.zsa.enable = true;
   environment.systemPackages = [
-    pkgs.keyd
+    pkgs.keyd # services.keyd module has no package option
     pkgs.wally-cli
   ];
 
@@ -10,7 +10,8 @@
     ids = ["*"];
     settings = {
       # based on recommended config <https://github.com/rvaiya/keyd#recommended-config>
-      # FIXME: something here causes super to not work?
+      # FIXME: something here causes super to not work? possibly related to
+      # apple keyboard on hodgepodge?
       main = {
         # FIXME: not work?
         # shift = "oneshot(shift)";
@@ -21,6 +22,7 @@
         # rightalt = "oneshot(altgr)"; # TODO: do we want this?
 
         capslock = "overload(control, esc)";
+
         # insert = "S-insert";
       };
     };
