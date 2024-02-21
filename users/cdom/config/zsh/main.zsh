b2f8bb1eae2c5867de6e93e7c7e78917a704b5ac
Chris Montgomery <chris@cdom.io>
2024-02-20 21:38:13 -0500

    feat(zsh): support for OSC-7 escape sequence
    Facilitates spawning of new terminal instances in the current working directory.

--- users/cdom/config/zsh/main.zsh
+++ users/cdom/config/zsh/main.zsh
@@ -15,3 +15,20 @@ add-zsh-hook -Uz chpwd chpwd-list-all
 # <https://codeberg.org/akib/emacs-eat>
 [[ -n "$EAT_SHELL_INTEGRATION_DIR" ]] && \
   source "$EAT_SHELL_INTEGRATION_DIR"
+
+# Support for OSC-7 escape sequence, spawning new terminal instances in the
+# current working directory.
+#
+# <https://codeberg.org/dnkl/foot/wiki#spawning-new-terminal-instances-in-the-current-working-directory>
+
+function osc7-pwd() {
+    emulate -L zsh # also sets localoptions for us
+    setopt extendedglob
+    local LC_ALL=C
+    printf '\e]7;file://%s%s\e\' $HOST ${PWD//(#m)([^@-Za-z&-;_~])/%${(l:2::0:)$(([##16]#MATCH))}}
+}
+
+function chpwd-osc7-pwd() {
+    (( ZSH_SUBSHELL )) || osc7-pwd
+}
+add-zsh-hook -Uz chpwd chpwd-osc7-pwd
