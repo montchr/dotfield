80e2926b01b48640dcec919d4d84020edf360f06
Chris Montgomery <chris@cdom.io>
2024-02-21 18:14:19 -0500

    feat(hardware:razer): add razergenie configuration gui

--- profiles/nixosProfiles/hardware/razer.nix
+++ profiles/nixosProfiles/hardware/razer.nix
@@ -1,7 +1,16 @@
 # Razer Deathadder V3 Pro Wireless Mouse
 # <https://openrazer.github.io/>
 #   NOTE: admin users should be added to `hardware.openrazer.users`!
-{config,...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
+  environment.systemPackages = [
+    pkgs.razergenie # Razer device configuration GUI (Qt)
+    # pkgs.polychromatic # Lighting management GUI for Razer devices
+  ];
+
   hardware.openrazer = {
     enable = true;
     keyStatistics = false;
