b63ece6e8a3d56f4e3622ec8c18cf85eb1c8f2b4
Chris Montgomery <chris@cdom.io>
2024-02-21 18:13:50 -0500

    fix(hardware:razer): disable broken mouse battery notifier

--- profiles/nixosProfiles/hardware/razer.nix
+++ profiles/nixosProfiles/hardware/razer.nix
@@ -1,9 +1,13 @@
-##: <https://openrazer.github.io/>
+# Razer Deathadder V3 Pro Wireless Mouse
+# <https://openrazer.github.io/>
 #   NOTE: admin users should be added to `hardware.openrazer.users`!
 {config,...}: {
   hardware.openrazer = {
     enable = true;
     keyStatistics = false;
-    mouseBatteryNotifier = true;
+    # Notifications are super frequent, repetitive, and sometimes just report 0%.
+    # The upstream issue tracker has quite a few related issues:
+    # <https://github.com/openrazer/openrazer/issues?q=notification+battery>
+    mouseBatteryNotifier = false;
   };
 }
