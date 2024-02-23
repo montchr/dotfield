3b1287ab454b382841a5d5560b8889e4d50c2444
Chris Montgomery <chris@cdom.io>
2024-02-23 13:03:52 -0500

    fix(gnome|gtk): move gnome-specific settings out of gtk settings

--- profiles/homeProfiles/desktop/gnome/common.nix
+++ profiles/homeProfiles/desktop/gnome/common.nix
@@ -3,6 +3,22 @@ with lib.hm.gvariant; {
   imports = [../gtk/dconf-settings.nix];
 
   dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      clock-show-seconds = lib.mkDefault false;
+      clock-show-weekday = lib.mkDefault true;
+
+      # FIXME: too aggressive, need CUA-like also -- macOS keys are actually
+      # pretty nice as a weird combination of Emacs and CUA with cmd/ctrl swap
+      # Emacs because it's so ingrained by both shells and even macOS defaults.
+      # But it's understandable why it's not the default:
+      # e.g. "C-a" is no longer for select all -- macOS can get away with this
+      # because it doesn't use Ctrl as prominently as Super
+      gtk-key-theme = "Emacs";
+
+      # text-scaling-factor = 1.0;
+      # toolkit-accessibility = false;
+    };
+
     "org/gnome/desktop/session" = {
       # TODO: longer for home theater, which are the primary gnome users
       idle-delay = lib.mkDefault (mkUint32 600);
--- profiles/homeProfiles/desktop/gtk/dconf-settings.nix
+++ profiles/homeProfiles/desktop/gtk/dconf-settings.nix
@@ -9,18 +9,12 @@ in
   with lib.hm.gvariant; {
     dconf.settings = {
       "org/gnome/desktop/interface" = {
-        clock-show-seconds = lib.mkDefault false;
-        clock-show-weekday = lib.mkDefault true;
         color-scheme = theme.color.schemes.default.kind;
 
         font-antialiasing = "rgba";
         font-hinting = "slight";
-
-        # Because it's so ingrained by both shells and even macOS defaults.
-        # But it's understandable why it's not the default:
-        # e.g. "C-a" is no longer for select all -- macOS can get away with this
-        # because it doesn't use Ctrl as prominently as Super
-        gtk-key-theme = "Emacs";
+        # TODO:
+        # text-scaling-factor = 1.0;
       };
     };
   }
