d07d093a5758022446baccc472d76d1552c87924
Chris Montgomery <chris@cdom.io>
2024-02-26 16:42:13 -0500

    fix(macbook|keyboard|boot): compensate for apple keyboard

--- profiles/nixosProfiles/hardware/apple/macbook.nix
+++ profiles/nixosProfiles/hardware/apple/macbook.nix
@@ -1,5 +1,10 @@
 {
   imports = [../laptop.nix];
 
-  boot.kernelParams = ["hid_apple.iso_layout=0"];
+  # <https://wiki.archlinux.org/title/Apple_Keyboard#hid_apple_module_options>
+  boot.kernelParams = [
+    "hid_apple.fnmode=2" # normally function keys; media keys when Fn held
+    "hid_apple.iso_layout=0"
+    "hid_apple.swap_opt_cmd=1"
+  ];
 }
