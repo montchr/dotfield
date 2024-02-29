bfe00b0d8813d3ca5be4eae641f646dfe55615f2
Chris Montgomery <chris@cdom.io>
2024-02-29 17:09:43 -0500

    fix(tuvok|gnome|trackpad): disable tap-to-click

--- machines/tuvok/users/cdom.nix
+++ machines/tuvok/users/cdom.nix
@@ -18,8 +18,16 @@ in {
   services.xserver.displayManager.autoLogin.enable = true;
   services.xserver.displayManager.autoLogin.user = username;
 
-  home-manager.users.${username} = hmArgs: {
-    imports = with hmArgs.roles; workstation;
+  home-manager.users.${username} = {roles, ...}: {
+    imports =
+      roles.workstation
+      ++ [
+        {
+          # The trackpad on this device is huge, and I always end up touching
+          # its corner with my palm, which is very disruptive.
+          dconf.settings."org/gnome/desktop/peripherals/touchpad".tap-to-click = true;
+        }
+      ];
     home.stateVersion = "23.05";
   };
 }
