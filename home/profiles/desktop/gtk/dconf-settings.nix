c0c112382ee61386ca16857d7116467476f3da8e
Chris Montgomery <chris@cdom.io>
2024-03-08 18:57:02 -0500

    chore(gtk): cleanup dconf settings file

--- home/profiles/desktop/gtk/dconf-settings.nix
+++ home/profiles/desktop/gtk/dconf-settings.nix
@@ -1,18 +1,12 @@
-# TODO: rename since this is also gtk stuff
 { lib, config, ... }:
-let
-  inherit (config) theme;
-in
-with lib.hm.gvariant;
+
 {
   dconf.settings = {
     "org/gnome/desktop/interface" = {
-      color-scheme = theme.color.schemes.default.kind;
+      color-scheme = config.theme.color.schemes.default.kind;
 
       font-antialiasing = "rgba";
       font-hinting = "slight";
-      # TODO:
-      # text-scaling-factor = 1.0;
     };
   };
 }
