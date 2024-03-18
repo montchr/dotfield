4c175534cafb102ef528487f06264e6203007f34
Chris Montgomery <chris@cdom.io>
2024-03-18 18:18:52 -0400

    spotify: allow spotify-player on aarch64
    It's only the official `pkgs.spotify` application that is broken on `aarch64-linux`.

--- home/profiles/spotify.nix
+++ home/profiles/spotify.nix
@@ -1,19 +1,21 @@
 { lib, pkgs, ... }:
 let
-  inherit (pkgs.stdenv.hostPlatform) isAarch64 isLinux;
+  inherit (pkgs.stdenv.hostPlatform) isLinux system;
   tomlFormat = pkgs.formats.toml { };
 in
 {
-  home.packages = lib.optionals (isLinux && !isAarch64) [
-    pkgs.spotify
-    pkgs.spotify-player
-  ];
+  home.packages =
+    [ pkgs.spotify-player ]
+    # XXX: broken upstream
+    ++ (lib.optional (system != "aarch64-linux") pkgs.spotify);
 
   # <https://github.com/aome510/spotify-player/blob/master/docs/config.md#general>
   xdg.configFile."spotify-player/app.toml".source =
     tomlFormat.generate "spotify-player-app-config" {
-      # <https://developer.spotify.com/dashboard/a51489a80c22465482734244dc626132/settings>
+
+      # <https://developer.spotify.com/dashboard/>
       client_id = "a51489a80c22465482734244dc626132";
+
       # Must match the callback URL specified in Spotify API configuration.
       client_port = 8080;
     }
