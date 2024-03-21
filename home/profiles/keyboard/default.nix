f2b76ed8a6eb65b982ad04a68a96fa239fbce907
Chris Montgomery <chris@cdom.io>
2024-03-21 14:29:14 -0400

    home: keyboard|gnome: set default xkb settings

--- home/profiles/keyboard/default.nix
+++ home/profiles/keyboard/default.nix
@@ -1,1 +1,19 @@
-{ lib, pkgs, ... }: lib.mkMerge [ { home.packages = with pkgs; [ ]; } ]
+{ lib, ... }:
+let
+  inherit (lib.hm.gvariant) mkTuple;
+in
+{
+  home.keyboard.options = lib.mkDefault [ "compose:ralt" ];
+
+  dconf.settings."org/gnome/desktop/input-sources" = {
+    sources = lib.singleton (mkTuple [
+      "xkb"
+      "us"
+    ]);
+    xkb-options = [
+      "caps:ctrl_modifier"
+      "terminate:ctrl_alt_bksp"
+      "compose:ralt"
+    ];
+  };
+}
