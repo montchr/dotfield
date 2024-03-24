1e55456f4f7a41d3583ed667e77e8ca3d8bfdcf8
Chris Montgomery <chris@cdom.io>
2024-03-24 15:33:37 -0400

    keyd: provide tab+meh, semicolon+hyper, enter+ctrl overloads

--- nixos/profiles/hardware/keyboard/keyd.nix
+++ nixos/profiles/hardware/keyboard/keyd.nix
@@ -13,11 +13,32 @@
   ];
 
   services.keyd.enable = true;
+
+  ## NOTES:
+  #
+  # keyd mod names may be different from expectations:
+  #
+  #   C =>	Ctrl
+  #   M =>	Meta	aka "Super" (Emacs, GNOME) or "Command" (macOS) or "Windows" (MSWin)
+  #   A =>	Alt	aka "Meta" (Emacs) or "Option" (macOS)
+  #   S =>	Shift
+  #   G =>	AltGr
+  #
+  # the docs tend to list these in the order C-M-A-S-G.
+  #
+  # and so, the equivalent composite mods:
+  #
+  #   "Hyper"	Ctrl + Meta + Alt + Shift	C-M-A-S
+  #   "Meh"	Ctrl + Alt + Shift		C-A-S
   services.keyd.keyboards.default = {
     ids = [ "*" ];
     settings = {
-
       global = {
+        # Illuminate the capslock light whenever a layer is active.
+        # We don't use capslock normally, ever, but even if we did, we would
+        # still consider it just another layer (effectively a one-shot layer).
+        layer_indicator = 1;
+
         # Ignore the tap behaviour of an overloaded key if it is held for the
         # given number of miliseconds.
         overload_tap_timeout = 500;
@@ -28,7 +49,14 @@
         # control = "oneshot(control)";
 
         capslock = "overload(control, esc)";
+        enter = "overload(control, enter)";
+        semicolon = "overload(hyper, semicolon)";
+        tab = "overload(meh, tab)";
       };
+
+      # Composite modifier aliases should be defined as "layers".
+      "hyper:C-M-A-S" = { };
+      "meh:C-A-S" = { };
     };
   };
 }
