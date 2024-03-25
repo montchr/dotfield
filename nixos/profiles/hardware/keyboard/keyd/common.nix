89310bda02e75eb8039f7c0c96981e7712cc166a
Chris Montgomery <chris@cdom.io>
2024-03-25 14:27:23 -0400

    keyd: configure with extraConfig + conf file

--- nixos/profiles/hardware/keyboard/keyd/common.nix
+++ nixos/profiles/hardware/keyboard/keyd/common.nix
@@ -1,6 +1,7 @@
-{ config, pkgs, ... }:
 # <https://github.com/rvaiya/keyd>
 # <https://github.com/NixOS/nixpkgs/issues?q=is%3Aissue+is%3Aopen+sort%3Aupdated-desc+keyd>
+# <https://github.com/NixOS/nixpkgs/issues/284797>
+{ config, pkgs, ... }:
 {
   assertions = [
     {
@@ -9,11 +10,10 @@
     }
   ];
 
-  environment.systemPackages = [
-    pkgs.keyd # services.keyd module has no package option
-  ];
+  # XXX: services.keyd module has no package option
+  environment.systemPackages = [ pkgs.keyd ];
+
   # XXX: <https://github.com/NixOS/nixpkgs/issues/290161>
-  #      <https://github.com/NixOS/nixpkgs/issues/284797>
   users.groups.keyd = { };
   systemd.services.keyd.serviceConfig.CapabilityBoundingSet = [ "CAP_SETGID" ];
 
@@ -39,6 +39,10 @@
   #   "Meh"	Ctrl + Alt + Shift		C-A-S
   services.keyd.keyboards.default = {
     ids = [ "*" ];
+    # XXX: the module -- which is very broken -- does not produce a valid file
+    # because it has no regard for the ordering of elements within when merging
+    # importing attrs. better just use the conf like normal people.
+    extraConfig = builtins.readFile ./default.extra.conf;
     settings = {
       global = {
         # Illuminate the capslock light whenever a layer is active.
@@ -50,14 +54,6 @@
         # given number of miliseconds.
         overload_tap_timeout = 500;
       };
-
-      main = {
-        capslock = "overload(control, esc)";
-      };
-
-      # Composite modifier aliases should be defined as "layers".
-      "hyper:C-M-A-S" = { };
-      "meh:C-A-S" = { };
     };
   };
 }
