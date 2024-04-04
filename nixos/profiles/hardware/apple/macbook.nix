511eb110e4b4046e8a7c15d4a98450597df12932
Chris Montgomery <chris@cdom.io>
2024-04-03 21:13:48 -0400

    macbook|keyboard: swap fn/lctl with kernel option

--- nixos/profiles/hardware/apple/macbook.nix
+++ nixos/profiles/hardware/apple/macbook.nix
@@ -4,7 +4,8 @@
   # <https://wiki.archlinux.org/title/Apple_Keyboard#hid_apple_module_options>
   boot.kernelParams = [
     "hid_apple.fnmode=2" # normally function keys; media keys when Fn held
-    "hid_apple.iso_layout=0"
-    "hid_apple.swap_opt_cmd=1"
+    "hid_apple.iso_layout=0" # ANSI
+    "hid_apple.swap_opt_cmd=1" # match pc layout
+    "hid_apple.swap_fn_leftctrl=1" # match pc layout
   ];
 }
