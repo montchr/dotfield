f4d85b2b7dbe11d49dd721f2548d7248281fb815
Chris Montgomery <chris@cdom.io>
2024-04-04 01:24:02 -0400

    prj: remove unnecessary eglot config

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,8 +1,4 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((eglot-workspace-configuration . (:nil
-                                           (:nix
-                                            (:flake
-                                             (:autoArchive nil)))))))
- (nix-ts-mode . ((apheleia-formatter . nixfmt))))
+((nix-ts-mode . ((apheleia-formatter . nixfmt))))
