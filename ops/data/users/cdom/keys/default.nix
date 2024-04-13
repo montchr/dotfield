d8b205759a114afc37b64f3b44a4af8e505b760e
Chris Montgomery <chris@cdom.io>
2024-04-13 18:31:56 -0400

    cdom|keys: add ryosuke & tuvok, remove iphone

--- ops/data/users/cdom/keys/default.nix
+++ ops/data/users/cdom/keys/default.nix
@@ -1,13 +1,11 @@
-# FIXME: add tuvok and ryosuke... or make this not so obscure...
 { root, ... }:
 let
   inherit (root) hosts keys;
 in
-[
-  keys.ssh."0x135EEDD0F71934F3"
-  keys.ssh.blink-at-phione1
-]
+[ keys.ssh."0x135EEDD0F71934F3" ]
 ++ hosts.boschic.users.seadoom.keys
 ++ hosts.brakhage.users.blink.keys
 ++ hosts.hodgepodge.users.seadoom.keys
+++ hosts.ryosuke.users.cdom.keys
 ++ hosts.tuvix.users.cdom.keys
+++ hosts.tuvok.users.cdom.keys
