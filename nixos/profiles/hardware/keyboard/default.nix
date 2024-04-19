f6f567708d667ddf51445711c2092dc5e0bae79b
Chris Montgomery <chris@cdom.io>
2024-04-19 14:56:56 -0400

    keyboard: enable qmk option
    I thought I had already done this.

--- nixos/profiles/hardware/keyboard/default.nix
+++ nixos/profiles/hardware/keyboard/default.nix
@@ -15,6 +15,8 @@
   # Required to support flashing firmware.
   dotfield.guardian.extraGroups = [ "plugdev" ];
 
+  hardware.keyboard.qmk.enable = true;
+
   hardware.keyboard.keyboardio.enable = true;
 
   hardware.keyboard.zsa.enable = true;
