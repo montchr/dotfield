38cf3b30c40a1efc24fc25c67c8e4e57fff71490
Chris Montgomery <chris@cdom.io>
2024-04-26 01:16:47 -0400

    nix: clean up upgrade-diff system activation script

--- common/profiles/core/upgrade-diff.nix
+++ common/profiles/core/upgrade-diff.nix
@@ -1,16 +1,10 @@
-# <https://discourse.nixos.org/t/nvd-simple-nix-nixos-version-diff-tool/12397>
-# <https://github.com/Mic92/dotfiles/blob/c6cad4e57016945c4816c8ec6f0a94daaa0c3203/nixos/modules/upgrade-diff.nix>
 # <https://github.com/NixOS/nixpkgs/pull/208902>
 { pkgs, ... }:
 {
-  # NOTE: All linked examples use /run/current-system, probably to preserve
-  #       existing NixOS behavior since profiles still require opt-in as an
-  #       experimental feature. However, nix-darwin AFAIK does not have such a
-  #       path. The system profile, as used below, points to the same derivation
-  #       as /run/current-system, so no platform check is necessary.
-  # FIXME: does not seem to do anything, either during activation or in an interactive shell session
+  # FIXME: no effect
   system.activationScripts.diff = ''
-    ${pkgs.nix}/bin/nix store diff-closures \
-      /nix/var/nix/profiles/system "$systemConfig" || true
+    if [[ -e /run/current-system ]]; then
+      ${pkgs.nix}/bin/nix --extra-experimental-features 'nix-command' store diff-closures /run/current-system "$systemConfig" || true
+    fi
   '';
 }
