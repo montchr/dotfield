6debc89ca33ab2f737ae265013d1a532936b9901
Chris Montgomery <chris@cdom.io>
2024-04-26 10:27:32 -0400

    dev: add nix-inspect

--- home/profiles/development/nix-tools.nix
+++ home/profiles/development/nix-tools.nix
@@ -3,6 +3,7 @@
   home.packages = [
     pkgs.alejandra
     pkgs.nix-init # <- generate nix package expressions from url
+    pkgs.nix-inspect # <- configuration inspector
     pkgs.nix-melt # <- flake.lock explorer
     pkgs.nixfmt-rfc-style
     pkgs.nixpkgs-review
