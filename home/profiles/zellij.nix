56791be8c68959885194070c5ec151fc092c19cb
Chris Montgomery <chris@cdom.io>
2024-05-02 21:32:46 -0400

    zellij: cleanup

--- home/profiles/zellij.nix
+++ home/profiles/zellij.nix
@@ -3,8 +3,6 @@ let
   userConfigsBase = "${config.xdg.configHome}/dotfield/home/users/cdom/config";
   inherit (config.lib.file) mkOutOfStoreSymlink;
 in
-# FIXME: broken, of course
-# inherit (config.dotfield.paths.userDirs) configsPath;
 {
   programs.zellij.enable = true;
   xdg.configFile."zellij".source = mkOutOfStoreSymlink "${userConfigsBase}/zellij";
