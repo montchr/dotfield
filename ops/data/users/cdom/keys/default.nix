c9a021613e72c2dad82e46688de9b86f11277f24
Chris Montgomery <chris@cdom.io>
2024-05-06 22:58:43 -0400

    gpg|ssh|identity: re-extract rsa from yubikey

--- ops/data/users/cdom/keys/default.nix
+++ ops/data/users/cdom/keys/default.nix
@@ -2,7 +2,7 @@
 let
   inherit (root) hosts keys;
 in
-[ keys.ssh."0x135EEDD0F71934F3" ]
+[ keys.ssh.cdom-yubikey-rsa ]
 ++ hosts.boschic.users.seadoom.keys
 ++ hosts.brakhage.users.blink.keys
 ++ hosts.hodgepodge.users.seadoom.keys
