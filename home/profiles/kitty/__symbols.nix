c30a42af81917c0d71517cf44b7e51a32d0d02e7
Chris Montgomery <chris@cdom.io>
2024-05-08 12:19:44 -0400

    kitty|fonts: use glyphsets from apparat

--- home/profiles/kitty/__symbols.nix
+++ home/profiles/kitty/__symbols.nix
@@ -3,15 +3,14 @@
 { lib, flake, ... }:
 let
   inherit (builtins) attrValues map;
-  inherit (flake.inputs.apparat.lib.kitty) makeConf makeSymbolsMap;
-
-  fontName = "Symbols Nerd Font";
-  glyphSets = import ./__nerdfont-glyph-sets.nix;
+  inherit (flake.inputs) apparat;
+  inherit (apparat.lib.kitty) makeConf makeSymbolsMap;
+  inherit (apparat.constants.typography) glyphsets;
 in
 {
   # https://sw.kovidgoyal.net/kitty/conf/#fonts
   # https://old.reddit.com/r/KittyTerminal/comments/r5hssh/kitty_nerd_fonts/
   programs.kitty.extraConfig = makeConf {
-    symbol_map = map (x: makeSymbolsMap x fontName) (attrValues glyphSets);
+    symbol_map = map (x: makeSymbolsMap x "Symbols Nerd Font") (attrValues glyphsets.symbols-nerd-font);
   };
 }
