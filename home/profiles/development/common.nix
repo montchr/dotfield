836f4481dedcc2f8bf4ad3747003902a65561546
Chris Montgomery <chris@cdom.io>
2024-05-14 19:23:51 -0400

    dev: globally install prettier

--- home/profiles/development/common.nix
+++ home/profiles/development/common.nix
@@ -6,8 +6,10 @@
   ];
 
   home.packages = [
-    # TODO: add emacs integration
+    # TODO: add as flycheck/flymake check
     pkgs.dotenv-linter # <https://dotenv-linter.github.io/#/?id=dotenv-linter>
+
+    pkgs.nodePackages.prettier
 
     # TODO: disabled because i don't remember why i added it
     # pkgs.nodePackages.terser
