9a8ddd2d9078ea837d7ec1d049332a91dd665702
Chris Montgomery <chris@cdom.io>
2024-05-14 17:29:04 -0400

    work|ssh: update

--- home/profiles/development/work/ssh.nix
+++ home/profiles/development/work/ssh.nix
@@ -1,19 +1,24 @@
 {
   programs.ssh.matchBlocks = {
-    "kw-ispr" = {
+    "kwg-ispr" = {
       user = "isprsite";
       hostname = "35.236.219.140";
       port = 24919;
     };
-    "kw-www" = {
+    "kwg-tutv" = {
+      user = "templetv";
+      hostname = "35.236.219.140";
+      port = 38736;
+    };
+    "kwg-www" = {
       hostname = "67.225.164.90";
       port = 5623;
     };
-    "kw-db" = {
+    "kwg-db" = {
       hostname = "67.225.164.91";
       port = 522;
     };
-    "kw-dev" = {
+    "kwg-dev" = {
       hostname = "67.43.11.196";
     };
   };
