a4c6fd0efb42159b7195b55e63a797ce65db2a91
Chris Montgomery <chris@cdom.io>
2024-05-24 23:50:19 -0400

    emacs: just moving stuff around playing with nixd

--- home/profiles/emacs/extra-packages.nix
+++ home/profiles/emacs/extra-packages.nix
@@ -1,24 +1,21 @@
-# FIXME: split apart
-# FIXME: add these tools as necessary in other profiles
 { pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    (ripgrep.override { withPCRE2 = true; })
+  home.packages =
+    [
+      (pkgs.ripgrep.override { withPCRE2 = true; })
+      pkgs.imagemagick # for image-dired
+      pkgs.zstd # for compression in supported contexts
 
-    imagemagick # for image-dired
-    zstd # for compression in supported contexts
-
-    editorconfig-core-c
-
-    nodePackages.bash-language-server
-    nodePackages.dockerfile-language-server-nodejs
-    nodePackages.typescript-language-server
-    nodePackages.vscode-css-languageserver-bin
-    nodePackages.vscode-html-languageserver-bin
-    nodePackages.vscode-json-languageserver
-    nodePackages.yaml-language-server
-    taplo-lsp # toml
-
-    # nodePackages.intelephense # php (unfree)
-  ];
+      pkgs.editorconfig-core-c
+      pkgs.taplo-lsp # toml language server
+    ]
+    ++ (with pkgs.nodePackages; [
+      bash-language-server
+      dockerfile-language-server-nodejs
+      typescript-language-server
+      vscode-css-languageserver-bin
+      vscode-html-languageserver-bin
+      vscode-json-languageserver
+      yaml-language-server
+    ]);
 }
