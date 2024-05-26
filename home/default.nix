29558fc91780eef69d55184d0a9baac462695609
Chris Montgomery <chris@cdom.io>
2024-05-26 15:03:52 -0400

    fix(home|perf)!: remove all existing `homeConfigurations` outputs

--- home/default.nix
+++ home/default.nix
@@ -7,16 +7,12 @@
 let
   inherit (self) inputs;
   inherit (self.lib.hm) makeHomeConfiguration;
-  inherit (inputs.apparat.lib.hm) mkHomeConfigurations;
 
   # features = import ./features.nix { homeProfiles = profiles; };
   profiles = import ./profiles.nix { inherit (inputs) haumea; };
 in
 {
   flake = {
-    # TODO: invert this approach -- make system configs import pre-defined home
-    # configs or something like that. see ~misterio77/nixos-config
-    homeConfigurations = (mkHomeConfigurations config.flake.nixosConfigurations);
 
     homeModules = {
       "theme" = import ./modules/theme/default.nix;
@@ -24,22 +20,6 @@ in
       "programs/bash/trampoline" = import ./modules/programs/bash/trampoline.nix;
       "programs/cod" = import ./modules/programs/cod.nix;
       "programs/liquidprompt" = import ./modules/programs/liquidprompt.nix;
-    };
-  };
-
-  perSystem = _perSystem: {
-    homeConfigurations = {
-      traveller = makeHomeConfiguration "cdom" {
-        modules = [
-          profiles.development.work
-
-          {
-            _module.args = {
-              inherit ops;
-            };
-          }
-        ];
-      };
     };
   };
 }
