501711536738a23028d300015af6859f115a7cd8
Chris Montgomery <chris@cdom.io>
2024-06-08 16:35:39 -0400

    feat(dev): add biome formatter

--- home/profiles/development/common.nix
+++ home/profiles/development/common.nix
@@ -7,11 +7,15 @@
 
   home.packages = [
     pkgs.asciinema
+    pkgs.universal-ctags
+
+    # {{{ checkers & formatters
+    pkgs.biome
     pkgs.dotenv-linter
     pkgs.nodePackages.prettier
     pkgs.shfmt
     pkgs.shellcheck
-    pkgs.universal-ctags
     pkgs.yamllint
+    # }}}
   ];
 }
