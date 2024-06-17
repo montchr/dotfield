a5d8429fc02002fcb174c139f3ca70612283d441
Chris Montgomery <chris@cdom.io>
2024-06-16 23:16:57 -0400

    tweak(boot): keep 16 previous configurations in boot menu

--- nixos/profiles/boot/systemd-boot.nix
+++ nixos/profiles/boot/systemd-boot.nix
@@ -2,8 +2,8 @@
 {
   boot.loader.systemd-boot = {
     enable = true;
-    configurationLimit = lib.mkDefault 10;
     consoleMode = "auto";
+    configurationLimit = 16;
     # NixOS manual recommends setting this to false, as it allows gaining root
     # access by passing `init=/bin/sh` as a kernel parameter. It's enabled by
     # default for back-compat.
