a2a7ddfab7654189733cf89cf2cdde75edda407b
Pablo Ovelleiro Corral <mail@pablo.tools>
2024-06-27 21:01:38 +0200

    Update NixOS wiki link

--- home/profiles/graphical/applications/firefox/search/default.nix
+++ home/profiles/graphical/applications/firefox/search/default.nix
@@ -80,8 +80,8 @@ in
       icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
     };
     "NixOS Wiki" = {
-      urls = [ { template = "https://nixos.wiki/index.php?search={searchTerms}"; } ];
-      iconUpdateURL = "https://nixos.wiki/favicon.png";
+      urls = [ { template = "https://wiki.nixos.org/index.php?search={searchTerms}"; } ];
+      iconUpdateURL = "https://wiki.nixos.org/favicon.png";
       # TODO: why?
       updateInterval = 24 * 60 * 60 * 1000; # every day
       definedAliases = [ "@nwiki" ];
