86f5726b37941cac57fc2cc61c62943b884a4ffa
Chris Montgomery <chmont@proton.me>
2024-06-29 19:40:39 -0400

    chore(ryosuke): organize

--- machines/ryosuke/default.nix
+++ machines/ryosuke/default.nix
@@ -25,27 +25,8 @@
   boot.supportedFilesystems = [ "btrfs" ];
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
-  virtualisation.vmVariant = {
-    virtualisation.graphics = true;
-  };
-
-  system.stateVersion = "22.05";
-
-  # Set your time zone.
   time.timeZone = "America/New_York";
-
   networking.firewall.enable = true;
 
-  ##: wake on lan
-  # TODO: no idea if this works
-  # networking.interfaces."eth0".wakeOnLan.enable = true;
-  # # https://wiki.archlinux.org/title/Wake-on-LAN#Enable_WoL_in_TLP
-  # services.tlp.settings.WOL_DISABLE = "N";
-  # environment.systemPackages = with pkgs; [
-  #   # Manually enable WOL:
-  #   #   $ sudo ethtool -s eth0 wol g
-  #   # Check WOL status:
-  #   #   $ sudo ethtool eth0 | grep Wake-on
-  #   ethtool
-  # ];
+  system.stateVersion = "22.05";
 }
