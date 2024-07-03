ea0880a2202f04db50371fab9a640660d9bcb6fc
Chris Montgomery <chmont@proton.me>
2024-07-03 10:14:27 -0400

    feat(tuvok): use experimental asahi mesa
    wow.

--- machines/tuvok/default.nix
+++ machines/tuvok/default.nix
@@ -16,6 +16,10 @@
   dotfield.guardian.username = "cdom";
   users.mutableUsers = false;
 
+  # NOTE: Enabling this will rebuild the world!  But holy shit, it is worth it.
+  hardware.asahi.useExperimentalGPUDriver = true;
+  hardware.asahi.experimentalGPUInstallMode = "overlay";
+
   # Currently required for Asahi monitor support via USB-C.  Asahi does not yet
   # support DP-Alt display output.  DP-Alt output is required for true HDMI or
   # DP output via one of this machine's two USB-C ports and zero HDMI/DP ports.
