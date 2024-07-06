24a5169c2aa45de6075573f626cd90c42cb97d86
Chris Montgomery <chmont@proton.me>
2024-07-06 14:40:27 -0400

    feat(home::ssh|ops::hosts): add match block for synoxyn

--- home/profiles/ssh.nix
+++ home/profiles/ssh.nix
@@ -10,22 +10,27 @@ in
     serverAliveInterval = 300;
     controlMaster = "auto";
     controlPersist = "10m";
+    matchBlocks = {
+      "gabbro".hostname = "${hosts.gabbro.ipv6.address}::1";
+      "hierophant".hostname = "${hosts.hierophant.ipv6.address}::1";
+      "moraine".hostname = "${hosts.moraine.ipv6.address}::1";
+      "synoxyn" = {
+        hostname = hosts.synoxyn.ipv4.address;
+        port = 2367;
+      };
 
-    matchBlocks."gabbro".hostname = "${hosts.gabbro.ipv6.address}::1";
-    matchBlocks."hierophant".hostname = "${hosts.hierophant.ipv6.address}::1";
-    matchBlocks."moraine".hostname = "${hosts.moraine.ipv6.address}::1";
+      "github.com" = {
+        # inherit identityFile;
+        # identitiesOnly = true;
+        user = "git";
+      };
 
-    matchBlocks."github.com" = {
-      # inherit identityFile;
-      # identitiesOnly = true;
-      user = "git";
-    };
-
-    matchBlocks."eu.nixbuild.net" = {
-      identityFile = "${homeDirectory}/.ssh/id_ed25519_seadome_nixbuild_net";
-      identitiesOnly = true;
-      extraOptions = {
-        PubkeyAcceptedKeyTypes = "ssh-ed25519";
+      "eu.nixbuild.net" = {
+        identityFile = "${homeDirectory}/.ssh/id_ed25519_seadome_nixbuild_net";
+        identitiesOnly = true;
+        extraOptions = {
+          PubkeyAcceptedKeyTypes = "ssh-ed25519";
+        };
       };
     };
 
--- ops/data/hosts.nix
+++ ops/data/hosts.nix
@@ -108,6 +108,15 @@ in
   synoxyn = {
     ipv4.address = "192.168.1.197";
     network = "home";
+    networks.ts = {
+      ipv4.address = "100.106.113.40";
+      ipv6.address = "fd7a:115c:a1e0::7601:7129";
+    };
+    users.cdom = {
+      keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGd0Ao5dHAF1+uOanojoMaE6XBkzsa6ooMNe+rBNY5PI cdom@synoxyn"
+      ];
+    };
   };
   tuvix = {
     age = keys.age.tuvix;
