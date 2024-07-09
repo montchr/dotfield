bfa0bbfd45ad270ee7f01d41aaa7edbd7f503a21
Chris Montgomery <chmont@proton.me>
2024-07-09 00:58:03 -0400

    fix(host::platauc): remove duplicate password

--- machines/platauc/default.nix
+++ machines/platauc/default.nix
@@ -13,7 +13,6 @@
   users.users.cdom = {
     uid = 1000;
     isNormalUser = true;
-    initialHashedPassword = "$y$j9T$oXblkpzc3Afple6/j2jZm/$RwdXc7ADcMDBwvpzzw2KXkj/nw5NX2ogbkB0w25OTe8";
     hashedPasswordFile = config.sops.secrets."users/cdom/hashed-password".path;
     openssh.authorizedKeys.keys = ops.users.cdom.keys.default;
   };
