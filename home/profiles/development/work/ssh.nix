74ea76cdcff3feb1291f51ca9f495bbf5ada438e
Chris Montgomery <chmont@proton.me>
2024-07-09 18:30:25 -0400

    feat(home::(work|ssh)): add matchblock for seesaw

--- home/profiles/development/work/ssh.nix
+++ home/profiles/development/work/ssh.nix
@@ -3,19 +3,28 @@ let
     inherit user port;
     hostname = "35.236.219.140";
   };
+  mkKinstaHost2 = user: port: {
+    inherit user port;
+    hostname = "34.162.230.19";
+  };
 in
 {
   programs.ssh.matchBlocks = {
     "kleinweb-ispr-prod" = mkKinstaHost "isprsite" 24919;
+
     "kleinweb-forms-prod" = mkKinstaHost "kleinforms" 46032;
     "kleinweb-forms-dev" = mkKinstaHost "kleinforms" 19154;
+
     "kleinweb-tutv-prod" = mkKinstaHost "templetv" 38736;
     "kleinweb-tutv-dev" = mkKinstaHost "templetv" 59770;
+
+    "kleinweb-seesaw-prod" = mkKinstaHost2 "seesaw" 52637;
 
     "kleinweb-hostone" = {
       hostname = "67.225.164.90";
       port = 5623;
     };
+
     "kleinweb-db" = {
       hostname = "67.225.164.91";
       port = 522;
