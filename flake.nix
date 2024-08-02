0300c2a880913fbd302e07d4102af41140488cd3
Chris Montgomery <chmont@proton.me>
2024-08-01 23:05:10 -0400

    chore: update

--- flake.nix
+++ flake.nix
@@ -87,7 +87,7 @@
       url = "https://git.lix.systems/lix-project/nixos-module/archive/2.90.0.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nixos-apple-silicon.url = "github:tpwrules/nixos-apple-silicon/release-2024-07-19";
+    nixos-apple-silicon.url = "github:tpwrules/nixos-apple-silicon/release-2024-07-31";
     asahi-tuvok-firmware.url = "git+ssh://git@git.sr.ht/~montchr/asahi-tuvok-firmware";
     nixos-hardware.url = "github:NixOS/nixos-hardware";
     # FIXME: update
