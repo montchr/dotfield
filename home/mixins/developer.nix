c2c277bbdc396e8d0248a1956a2ed2973b1be2d1
Chris Montgomery <chmont@proton.me>
2024-08-09 13:40:14 -0400

    feat(home::dev): add git-filter-repo

--- home/mixins/developer.nix
+++ home/mixins/developer.nix
@@ -1,7 +1,10 @@
+{ pkgs, ... }:
 {
   imports = [
     ../profiles/development/difftools/delta.nix
     ../profiles/emacs/default.nix
     ../profiles/just.nix
   ];
+
+  home.packages = [ pkgs.git-filter-repo ];
 }
