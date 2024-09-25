adefe52012c2e1a08f6188eb10de0de7e2e67db8
Chris Montgomery <chmont@proton.me>
2024-09-24 22:21:30 -0400

    feat(home::espanso): expand arrows expansions

--- home/profiles/espanso/matches/base.nix
+++ home/profiles/espanso/matches/base.nix
@@ -20,81 +20,29 @@
       replace = "⎋";
       trigger = ";s;esc";
     }
-    {
-      replace = "←";
-      triggers = [
-        ";s;a;ll"
-        ";s;a;ww"
-      ];
-    }
-    {
-      replace = "→";
-      triggers = [
-        ";s;a;rr"
-        ";s;a;ee"
-      ];
-    }
-    {
-      replace = "⇒";
-      triggers = [ ";s;aa;rr" ];
-    }
-    {
-      replace = "⇐";
-      triggers = [ ";s;aa;ll" ];
-    }
-    {
-      replace = "↑";
-      triggers = [
-        ";s;a;uu"
-        ";s;a;nn"
-      ];
-    }
-    {
-      replace = "↓";
-      triggers = [
-        ";s;a;dd"
-        ";s;a;ss"
-      ];
-    }
-    {
-      replace = "↖";
-      triggers = [
-        ";s;a;ul"
-        ";s;a;nw"
-      ];
-    }
-    {
-      replace = "↗";
-      triggers = [
-        ";s;a;ur"
-        ";s;a;ne"
-      ];
-    }
-    {
-      replace = "↘";
-      triggers = [
-        ";s;a;dr"
-        ";s;a;se"
-      ];
-    }
-    {
-      replace = "↙";
-      triggers = [
-        ";s;a;dl"
-        ";s;a;sw"
-      ];
-    }
+
     {
       replace = "×";
-      trigger = ";s;mult";
+      triggers = [
+        ";s;mult"
+        ":ssmultiply"
+      ];
     }
     {
       replace = "∅";
-      trigger = ";s;null";
+      triggers = [
+        ";s;null"
+        ":ssnull"
+        ":ssnil"
+      ];
     }
     {
       replace = "¯\\_(ツ)_/¯";
-      trigger = ";s;hrug";
+      triggers = [
+        ";s;hrug"
+        ";s;shrug"
+        ":emoshrug"
+      ];
     }
   ];
 }
