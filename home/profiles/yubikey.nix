2a91f5090df02b0c5312107968754996aa675a65
Chris Montgomery <chmont@proton.me>
2024-11-07 22:49:58 -0500

    yubikey: disable `yubikey-manager` due to build failure

--- home/profiles/yubikey.nix
+++ home/profiles/yubikey.nix
@@ -1,5 +1,8 @@
 { pkgs, ... }:
 {
-  home.packages = [ pkgs.yubikey-manager ];
+  home.packages = [
+    # XXX: build failure for dependency python3.12-pyscard-2.1.1
+    #  pkgs.yubikey-manager
+  ];
   services.gpg-agent.enableScDaemon = true;
 }
