9512387129d1468a91cdec7aa8b79200ce4f59af
Chris Montgomery <chmont@proton.me>
2024-11-22 22:50:51 -0500

    feat(espanso): add cryptography snippets

---
+++ home/profiles/espanso/matches/crypt.nix
@@ -0,0 +1,51 @@
+{
+  matches = [
+    {
+      trigger = ";;uuid";
+      replace = "{{output}}";
+      vars = [
+        {
+          name = "output";
+          type = "shell";
+          params = {
+            cmd = "uuidgen -r";
+          };
+        }
+      ];
+    }
+    {
+      trigger = ";e;b64u";
+      replace = "{{output}}";
+      vars = [
+        {
+          name = "clip";
+          type = "clipboard";
+        }
+        {
+          name = "output";
+          type = "shell";
+          params = {
+            cmd = "echo -n \"{{clip}}\" | basenc --base64url";
+          };
+        }
+      ];
+    }
+    {
+      trigger = ";d;b64u";
+      replace = "{{output}}";
+      vars = [
+        {
+          name = "clip";
+          type = "clipboard";
+        }
+        {
+          name = "output";
+          type = "shell";
+          params = {
+            cmd = "echo -n \"{{clip}}\" | basenc --decode --base64url";
+          };
+        }
+      ];
+    }
+  ];
+}
