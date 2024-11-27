e3aaec627e5ae422268268ad6012d9063b0a07f8
Chris Montgomery <chmont@proton.me>
2024-11-26 22:41:19 -0500

    home::dev: import common modules

--- home/mixins/developer.nix
+++ home/mixins/developer.nix
@@ -1,7 +1,12 @@
 { pkgs, ... }:
 {
   imports = [
+    ../profiles/development/common.nix
     ../profiles/development/difftools/delta.nix
+    ../profiles/development/data-wrangling.nix
+    ../profiles/development/nix-tools.nix
+    ../profiles/development/nodejs.nix
+
     ../profiles/emacs/default.nix
     ../profiles/just.nix
   ];
