2fb4b535d46523248f1279b24c0bfdb92304de0a
Chris Montgomery <chmont@proton.me>
2025-02-18 22:34:03 -0500

    home::doom: tweak

--- users/cdom/dot-config/doom/custom.el
+++ users/cdom/dot-config/doom/custom.el
@@ -4,11 +4,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-unlispify-tag-names nil)
  '(safe-local-variable-values
-   '((org-refile-targets (nil :maxlevel . 4))
-     (eval load-file "./ceamx-dev-loader.el")
-     (org-archive-location . "graveyard/%s.archive::datetree/")
-     (magit-todos-exclude-globs ".git/" "lisp/*.el"))))
+    '((org-refile-targets (nil :maxlevel . 4))
+       (eval load-file "./ceamx-dev-loader.el")
+       (org-archive-location . "graveyard/%s.archive::datetree/")
+       (magit-todos-exclude-globs ".git/" "lisp/*.el"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
