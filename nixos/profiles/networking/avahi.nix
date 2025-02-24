ee7cb5fe32ba14ce811631dc2f726b96275c00e4
Chris Montgomery <chmont@proton.me>
2025-02-24 00:42:08 -0500

    fix: disable avahi service publishing by default
    This might be interfering with TU networking.

--- nixos/profiles/networking/avahi.nix
+++ nixos/profiles/networking/avahi.nix
@@ -1,11 +1,12 @@
 # Network service discovery via multicast DNS (mDNS)
 # NOTE: Enable firewall explicitly in other profiles or machine configs.
+{ lib, ... }:
 {
   services.avahi = {
     enable = true;
     nssmdns4 = true;
     publish = {
-      enable = true;
+      enable = lib.mkDefault false;
       domain = true;
       userServices = true;
     };
