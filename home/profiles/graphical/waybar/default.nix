d4f52234433adf5aaf8dc8bad77caaeb4667391b
Chris Montgomery <chmont@proton.me>
2025-03-03 16:08:42 -0500

    feat(bar): styles with color

--- home/profiles/graphical/waybar/default.nix
+++ home/profiles/graphical/waybar/default.nix
@@ -8,14 +8,14 @@ let
   cfg = config.programs.waybar;
 in
 {
-  # FIXME: breaks custom css -- generate colors and families manually
-  # stylix.targets.waybar.enable = false;
   stylix.targets.waybar.addCss = false;
 
   programs.waybar.enable = true;
   programs.waybar.package = flake.perSystem.inputs'.nixos-unstable.legacyPackages.waybar;
   programs.waybar.systemd.enable = true;
-  programs.waybar.style = lib.mkAfter (builtins.readFile ./style.css);
+  programs.waybar.style = ''
+    @import "./custom.css";
+  '';
 
   wayland.windowManager.sway.config.bars = [
     {
