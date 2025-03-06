7a0420e928169e0296338923cfcb47dad3979c59
Chris Montgomery <chmont@proton.me>
2025-03-06 11:30:03 -0500

    feat(chromium): enable widevine

--- home/profiles/graphical/applications/chromium.nix
+++ home/profiles/graphical/applications/chromium.nix
@@ -1,6 +1,8 @@
+{ pkgs, ... }:
 {
   programs.chromium = {
     enable = true;
+    package = pkgs.chromium.override { enableWideVine = true; };
     extensions = [
       { id = "eimadpbcbfnmbkopoojfekhnkhdbieeh"; } # <- dark reader
       { id = "ldgfbffkinooeloadekpmfoklnobpien"; } # <- raindrop.io
