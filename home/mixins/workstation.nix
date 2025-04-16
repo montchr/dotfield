9f7756fa604bf3763efe14f7a9a9c7ee62d2a70c
Chris Montgomery <chmont@protonmail.com>
2025-04-16 19:05:48 -0400

    fix: let vscode manage itself
    VS Code does not like immutable settings files, and neither do I.

--- home/mixins/workstation.nix
+++ home/mixins/workstation.nix
@@ -21,7 +21,6 @@
     ../profiles/ledger.nix
     ../profiles/pandoc.nix
     ../profiles/sync.nix
-    ../profiles/vscode/default.nix
     ../profiles/writing.nix
     ../profiles/yubikey.nix
   ];
