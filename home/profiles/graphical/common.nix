32110725a6411ac755a071abeadae84d40cdcde2
Chris Montgomery <chmont@protonmail.com>
2025-05-04 17:43:52 -0400

    feat(hm|graphical): add `tray` systemd user target

--- home/profiles/graphical/common.nix
+++ home/profiles/graphical/common.nix
@@ -31,6 +31,18 @@ in
     };
   };
 
+  systemd.user.targets.tray = {
+    Unit = {
+      Description = "Home Manager System Tray";
+      Requires = [ "graphical-session-pre.target" ];
+      # Any service starting after tray.target also needs to start
+      # after "graphical-session.target" to prevent cyclic dependency.
+      After = [ "graphical-session.target" ];
+      PartOf = [ "graphical-session.target" ];
+    };
+    Install.WantedBy = [ "graphical-session.target" ];
+  };
+
   home.packages = [
     pkgs.dex # helper for working with xdg desktop entries
     pkgs.mediainfo
