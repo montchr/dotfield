6a13b0d6d1d833512a2f9f2ce80bb8d5d9ee38bb
Chris Montgomery <chmont@protonmail.com>
2025-05-04 22:59:22 -0400

    fix: remove all overlayed packages
    No longer failing builds.

--- overlays/mkWaylandOverlay.nix
+++ overlays/mkWaylandOverlay.nix
@@ -1,8 +1,1 @@
-{ inputs }:
-final: prev: {
-  # XXX: Failing build on nixpkgs-wayland
-  inherit (inputs.nixos-unstable.legacyPackages.${final.system})
-    foot
-    waybar
-    ;
-}
+{ inputs }: final: prev: { }
