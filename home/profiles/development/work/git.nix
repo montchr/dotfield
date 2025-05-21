bbe6a44f56750d146a975bbb46de0f3d8bbb800e
Chris Montgomery <chmont@protonmail.com>
2025-05-21 12:34:58 -0400

    feat(work|git): include local tu gitlab credentials config

--- home/profiles/development/work/git.nix
+++ home/profiles/development/work/git.nix
@@ -1,19 +1,21 @@
+{ config, ... }:
 {
-  # TIP: verify with e.g.:
-  # $ fd -t d '\.git$' -H -x bash -c 'cd {//} && git config --get user.email'
-  programs.git.includes =
-    let
+  programs.git-credential-oauth.enable = true;
+
+  programs.git.includes = [
+    {
+      path = "${config.xdg.configHome}/git/tu-gitlab-credentials";
+    }
+    {
+      # TIP: verify with e.g.:
+      # $ fd -t d '\.git$' -H -x bash -c 'cd {//} && git config --get user.email'
       contents = {
         # FIXME: add this to an identity config -- dotfield's whoami module
         # should support multiple identities
         user.email = "chrismont@temple.edu";
       };
-    in
-    [
-      {
-        inherit contents;
-        condition = "gitdir:~/Projects/work/";
-        contentSuffix = "work-by-gitdir-gitconfig";
-      }
-    ];
+      condition = "gitdir:~/Projects/work/";
+      contentSuffix = "work-by-gitdir-gitconfig";
+    }
+  ];
 }
