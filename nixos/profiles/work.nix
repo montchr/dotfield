f0ac274b7ee811033c0c4f27e9a26a973ec5916c
Chris Montgomery <chmont@protonmail.com>
2025-06-03 11:23:06 -0400

    chore(work|dns): update local hosts

--- nixos/profiles/work.nix
+++ nixos/profiles/work.nix
@@ -6,13 +6,20 @@
       "logancenter.ddev.site"
       "logance.ddev.site"
       "phillyn.ddev.site"
+      "telepresence.ddev.site"
+      "tutv.ddev.site"
+      "whipradio.ddev.site"
+
       "seesaw.ddev.site"
       "pingpong.seesaw.ddev.site"
       "kleinit.seesaw.ddev.site"
-      "telepresence.ddev.site"
+
       "templenews.ddev.site"
-      "tutv.ddev.site"
-      "whipradio.ddev.site"
+      "chopboom.templenews.ddev.site"
+      "longform.templenews.ddev.site"
+      "makingitwork.templenews.ddev.site"
+      "owlery.templenews.ddev.site"
+      "thecherry.templenews.ddev.site"
     ];
   };
 }
