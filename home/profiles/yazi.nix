3705ad3e7998a2d2f710a88b463e16953fcfc3c6
Chris Montgomery <chmont@protonmail.com>
2025-07-18 10:10:28 -0400

    fix(yazi): prevent plugin attrs name warnings

--- home/profiles/yazi.nix
+++ home/profiles/yazi.nix
@@ -1,23 +1,22 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 {
   programs.yazi = {
     enable = true;
     shellWrapperName = "y";
-    plugins = builtins.listToAttrs (
-      builtins.map
-        (pkg: {
-          name = pkg.pname;
-          value = pkg;
-        })
-        [
-          pkgs.yaziPlugins.chmod
-          pkgs.yaziPlugins.git
-          pkgs.yaziPlugins.glow
-          pkgs.yaziPlugins.mediainfo
-          pkgs.yaziPlugins.ouch
-          pkgs.yaziPlugins.projects
-          pkgs.yaziPlugins.rsync
-        ]
-    );
+    plugins =
+      [
+        pkgs.yaziPlugins.chmod
+        pkgs.yaziPlugins.git
+        pkgs.yaziPlugins.glow
+        pkgs.yaziPlugins.mediainfo
+        pkgs.yaziPlugins.ouch
+        pkgs.yaziPlugins.projects
+        pkgs.yaziPlugins.rsync
+      ]
+      |> builtins.map (pkg: {
+        name = lib.removeSuffix ".yazi" pkg.pname;
+        value = pkg;
+      })
+      |> builtins.listToAttrs;
   };
 }
