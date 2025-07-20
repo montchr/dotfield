2865817c52120f3aa8e8199617b5227c4baf845c
Chris Montgomery <chmont@protonmail.com>
2025-07-20 12:40:42 -0400

    perf(jj|gpg): only sign commits upon push

--- home/profiles/jujutsu/with-gpg-signing.nix
+++ home/profiles/jujutsu/with-gpg-signing.nix
@@ -3,6 +3,11 @@ let
   inherit (config.dotfield) whoami;
 in
 {
+  # Defer signing until push to remote.  This provides a very
+  # significant performance boost during iteration due to the
+  # increased ephemerality of git commits using the git backend.
+  programs.jujutsu.settings.git.sign-on-push = true;
+
   programs.jujutsu.settings.signing = {
     behavior = "own";
     backend = "gpg";
