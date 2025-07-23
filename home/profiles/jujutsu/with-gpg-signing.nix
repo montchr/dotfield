5096e1f151ee845bbe2a31bec9c471c0dad1259a
Chris Montgomery <chmont@protonmail.com>
2025-07-23 11:53:26 -0400

    fix(jj|gpg): implement sign-on-push properly

--- home/profiles/jujutsu/with-gpg-signing.nix
+++ home/profiles/jujutsu/with-gpg-signing.nix
@@ -1,15 +1,10 @@
-{ config, ... }:
+{ lib, config, ... }:
 let
   inherit (config.dotfield) whoami;
 in
 {
-  # Defer signing until push to remote.  This provides a very
-  # significant performance boost during iteration due to the
-  # increased ephemerality of git commits using the git backend.
-  programs.jujutsu.settings.git.sign-on-push = true;
-
   programs.jujutsu.settings.signing = {
-    behavior = "own";
+    behavior = lib.mkDefault "own";
     backend = "gpg";
     key = whoami.pgp;
   };
--- home/profiles/jujutsu/with-sign-on-push.nix
+++ home/profiles/jujutsu/with-sign-on-push.nix
@@ -1,1 +1,10 @@
-{lib,...}
+{
+  # Defer signing until push to remote.  This provides a very
+  # significant performance boost during iteration due to the
+  # increased ephemerality of git commits using the git backend.
+  programs.jujutsu.settings.git.sign-on-push = true;
+
+  # Drop signatures on commit modification.  Commits will be re-signed
+  # if necessary upon push.
+  programs.jujutsu.settings.signing.behavior = "drop";
+}
