6b43fcd38afd1a9e80ac0e3393623d4e79a5509e
Chris Montgomery <chmont@protonmail.com>
2025-07-26 17:30:32 -0400

    feat(jj): add "harvest" alias

--- home/profiles/jujutsu/default.nix
+++ home/profiles/jujutsu/default.nix
@@ -19,6 +19,18 @@ in
     enable = true;
     settings = {
       aliases = {
+        # SYNOPSIS: jj harvest <revset>
+        #
+        # NOTE: `--from` revset argument is deliberately omitted a la
+        # partial application so the argument may be provided in the
+        # shell.  Surprisingly, this Just Works™!
+        "harvest" = [
+          "squash"
+          "--interactive"
+          "--to"
+          "@"
+          "--from"
+        ];
         "l" = [
           "log"
           "--no-pager"
