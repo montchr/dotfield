38a3921410fe2c16995a3aad4aa4de553d9d6036
Chris Montgomery <chmont@protonmail.com>
2025-07-26 15:08:54 -0400

    theme: manage fonts

--- nixos/profiles/graphical/fonts/default.nix
+++ nixos/profiles/graphical/fonts/default.nix
@@ -11,25 +11,43 @@
   environment.systemPackages = [ pkgs.font-manager ];
   fonts.fontDir.enable = true;
   fonts.packages = (
-    with pkgs;
-    [
-      aporetic
+    let
+      mkIosevkaSgrVariant =
+        variant:
+        pkgs.iosevka-bin.override {
+          variant = "SGr-Iosevka${variant}";
+        };
+
+      baselineFonts = with pkgs; [
+        corefonts
+        dejavu_fonts
+        ibm-plex
+        iosevka
+        inter
+        jetbrains-mono
+        nerd-fonts.symbols-only
+        terminus_font
+      ];
+
+      preferredFonts = with pkgs; [
+        aporetic
+        (mkIosevkaSgrVariant "Term")
+      ];
+    in
+    baselineFonts
+    ++ preferredFonts
+    ++ (with pkgs; [
       bakoma_ttf
       # <https://software.sil.org/charis/>
       # <https://practicaltypography.com/charter.html>
       charis-sil
-      corefonts
-      dejavu_fonts
+      # very nice website: <https://commitmono.com/>
+      commit-mono
       departure-mono
       fira
       gentium
       ia-writer-duospace
       ia-writer-quattro
-      ibm-plex
-      inter
-      jetbrains-mono
-      nerd-fonts.symbols-only
-      terminus_font
-    ]
+    ])
   );
 }
