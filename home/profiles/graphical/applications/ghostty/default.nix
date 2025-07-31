138f5103fcd323a92a9c31f4e4206af50e9d7999
Chris Montgomery <chmont@protonmail.com>
2025-07-31 18:30:34 -0400

    fix(ghostty): respect working directory setting

--- home/profiles/graphical/applications/ghostty/default.nix
+++ home/profiles/graphical/applications/ghostty/default.nix
@@ -7,8 +7,12 @@
     installBatSyntax = true;
     settings = {
       confirm-close-surface = false;
+      # Incompatible with `window-inherit-working-directory`.
+      # <https://github.com/ghostty-org/ghostty/discussions/4123#discussioncomment-13433453>
+      gtk-single-instance = false;
       window-decoration = false;
       window-inherit-working-directory = true;
+      working-directory = "inherit";
     };
   };
 
