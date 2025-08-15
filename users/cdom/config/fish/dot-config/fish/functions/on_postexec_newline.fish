b2a15e3dbbf2440d2c71bd3c5c2d7df935da7bda
Chris Montgomery <chmont@protonmail.com>
2025-08-15 10:27:38 -0400

    fix: temporarily restore original users dir for stow

---
+++ users/cdom/config/fish/dot-config/fish/functions/on_postexec_newline.fish
@@ -0,0 +1,3 @@
+function on_postexec_newline --on-event fish_postexec
+    echo
+end
--- users/cdom/whoami.nix
+++ users/cdom/whoami.nix
@@ -1,1 +1,9 @@
-{ }
+{
+  name = "Chris Montgomery";
+  firstName = "Chris";
+  lastName = "Montgomery";
+  email = "chmont@protonmail.com";
+  pgp = "0x135EEDD0F71934F3";
+  github = "montchr";
+  mastodon = "@montchr@assemblag.es";
+}
