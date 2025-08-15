8631a807d5dbd7571694ff7981b4060385936e1e
Chris Montgomery <chmont@protonmail.com>
2025-08-15 09:46:40 -0400

    feat(displays): add more metadata

--- src/features/hardware/by-manufacturer/apple/apple-macbook-14-2/default.nix
+++ src/features/hardware/by-manufacturer/apple/apple-macbook-14-2/default.nix
@@ -1,20 +1,31 @@
-{ lib, self, ... }:
+flake@{ lib, ... }:
 {
-  dotfield.features."hardware/apple/macbook-14-2".nixos = {
-    imports = [
-      self.dotfield.features."hardware/apple-silicon".nixos
-      self.dotfield.features."hardware/apple-macbook".nixos
-    ];
-  };
+  dotfield.features."hardware/apple/macbook-14-2" = {
+    nixos =
+      { config, ... }:
+      {
+        imports = [
+          flake.config.dotfield.features."hardware/apple-silicon".nixos
+          flake.config.dotfield.features."hardware/apple-macbook".nixos
+        ];
 
-  dotfield.features.graphical.nixos =
-    { config, ... }:
-    lib.mkIf config.programs.sway.enable {
-      environment.etc."sway/config".text = ''
-        set $laptop eDP-1
+        config = lib.mkIf config.programs.sway.enable {
+          environment.etc."sway/config".text = ''
+            set $laptop eDP-1
 
-        # output $laptop scale 2
-        # output $laptop pos 0 0 res 2560x1600
-      '';
+            # output $laptop scale 2
+            # output $laptop pos 0 0 res 2560x1600
+          '';
+        };
+      };
+
+    home = {
+      services.kanshi.settings = [
+        {
+          output.criteria = "eDP-1";
+          output.scale = 2.0;
+        }
+      ];
     };
+  };
 }
