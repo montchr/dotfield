21596e753244c1fb13a55afc9718970eec02c9e1
Chris Montgomery <chmont@protonmail.com>
2025-09-22 15:18:58 -0400

    ledger: merge duplicate features

--- src/users/cdom/features/ledger.nix
+++ src/users/cdom/features/ledger.nix
@@ -1,6 +1,10 @@
+# - <https://www.gizra.com/content/plain-text-accounting-hledger/>
+# - <https://memo.barrucadu.co.uk/personal-finance.html>
+# - <https://github.com/edkedk99/awesome-hledger?tab=readme-ov-file>
+# - <https://github.com/edkedk99/awesome-hledger/tree/master/contrib-resources>
 {
   users.cdom.aspects.workstation.home =
-    { config, ... }:
+    { config, pkgs, ... }:
     let
       inherit (config.home) homeDirectory;
       ledgerDir = "${homeDirectory}/Documents/ledger";
@@ -12,5 +16,17 @@
         uri = "git@codeberg.org:montchr/ledger";
         path = ledgerDir;
       };
+
+      # TODO:
+      # - <https://github.com/olimorris/hledger-forecast>
+      home.packages = [
+        pkgs.hledger
+        pkgs.hledger-ui
+        pkgs.hledger-web
+
+        pkgs.hledger-utils # <https://gitlab.com/nobodyinperson/hledger-utils>
+        pkgs.ledger-autosync # sync with financial institutions via OFX
+        pkgs.puffin # hledger dashboard
+      ];
     };
 }
