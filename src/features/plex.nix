2d80041514abdb70f91bf3b949a3ba5fb46f24a3
Chris Montgomery <chmont@protonmail.com>
2025-09-22 15:20:54 -0400

    plex: disable on unsupported platform

--- src/features/plex.nix
+++ src/features/plex.nix
@@ -1,7 +1,10 @@
+{ lib, ... }:
 {
   aspects.workstation.home =
     { pkgs, ... }:
     {
-      home.packages = [ pkgs.plex-desktop ];
+      home.packages =
+        # XXX: Unsupported platform.
+        lib.optionals (pkgs.stdenv.hostPlatform.system != "aarch64-linux") [ pkgs.plex-desktop ];
     };
 }
