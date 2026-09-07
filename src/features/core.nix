ee93d54f327ccb66815d4942abc68ff48f139e01
chris montgomery <chmont@protonmail.com>
2026-09-07 16:51:43 -0400

    core: set i18n locale settings

--- src/features/core.nix
+++ src/features/core.nix
@@ -35,6 +35,20 @@ in
           XDG_BIN_HOME = binHome;
         };
 
+        # Select internationalisation properties.
+        i18n.defaultLocale = "en_US.UTF-8";
+        i18n.extraLocaleSettings = {
+          LC_ADDRESS = "en_US.UTF-8";
+          LC_IDENTIFICATION = "en_US.UTF-8";
+          LC_MEASUREMENT = "en_US.UTF-8";
+          LC_MONETARY = "en_US.UTF-8";
+          LC_NAME = "en_US.UTF-8";
+          LC_NUMERIC = "en_US.UTF-8";
+          LC_PAPER = "en_US.UTF-8";
+          LC_TELEPHONE = "en_US.UTF-8";
+          LC_TIME = "en_US.UTF-8";
+        };
+
         programs.htop.enable = true;
         programs.mtr.enable = true;
 
--- src/hosts/riebeck/default.nix
+++ src/hosts/riebeck/default.nix
@@ -88,21 +88,6 @@
       # Set your time zone.
       time.timeZone = "America/New_York";
 
-      # Select internationalisation properties.
-      i18n.defaultLocale = "en_US.UTF-8";
-
-      i18n.extraLocaleSettings = {
-        LC_ADDRESS = "en_US.UTF-8";
-        LC_IDENTIFICATION = "en_US.UTF-8";
-        LC_MEASUREMENT = "en_US.UTF-8";
-        LC_MONETARY = "en_US.UTF-8";
-        LC_NAME = "en_US.UTF-8";
-        LC_NUMERIC = "en_US.UTF-8";
-        LC_PAPER = "en_US.UTF-8";
-        LC_TELEPHONE = "en_US.UTF-8";
-        LC_TIME = "en_US.UTF-8";
-      };
-
       # List packages installed in system profile. To search, run:
       # $ nix search wget
       environment.systemPackages = with pkgs; [
