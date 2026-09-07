725093736ad8a6fed789a5fc9816268b3bd3d5c7
chris montgomery <chmont@protonmail.com>
2026-09-07 16:18:57 -0400

    firefox: update config

--- src/users/cdom/features/firefox/__settings/common.nix
+++ src/users/cdom/features/firefox/__settings/common.nix
@@ -2,6 +2,14 @@
 {
   "browser.aboutConfig.showWarning" = false;
 
+  "browser.ai.control.default" = "blocked";
+  "browser.ai.control.linkPreviewKeyPoints" = "blocked";
+  "browser.ai.control.pdfjsAltText" = "blocked";
+  "browser.ai.control.sidebarChatbot" = "blocked";
+  "browser.ai.control.smartTabGroups" = "blocked";
+  "browser.ai.control.smartWindow" = "blocked";
+  "browser.ai.control.translations" = "blocked";
+
   "browser.bookmarks.editDialog.firstEditField" = "tagsField";
   "browser.bookmarks.restore_default_bookmarks" = false;
   "browser.bookmarks.showMobileBookmarks" = true;
@@ -13,24 +21,35 @@
 
   "browser.proton.enabled" = true;
 
-  # "browser.newtabpage.enabled" = true;
-
+  "browser.newtabpage.activity-stream.feeds.section.topstories" = false;
   # Enable a real search bar on `about:home` instead of diverting focus to the address bar.
   "browser.newtabpage.activity-stream.improvesearch.handoffToAwesomebar" = false;
+  "browser.newtabpage.activity-stream.showSponsored" = false;
+  "browser.newtabpage.activity-stream.showSponsoredCheckboxes" = false;
+  "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
+  "browser.newtabpage.activity-stream.topSitesRows" = 2;
 
   "browser.search.hiddenOneOffs" = "Google,Yahoo,Bing,Amazon.com,Twitter";
   "browser.search.region" = "US";
   "browser.search.suggest.enabled" = true;
 
+  "browser.smartwindow.memories.generateFromConversation" = false;
+  "browser.smartwindow.memories.generateFromHistory" = false;
+
   # TODO: add documentation for values
   "browser.startup.page" = 3;
 
+  "browser.tabs.groups.smart.enabled" = false;
+  "browser.tabs.groups.smart.userEnabled" = false;
   "browser.tabs.insertAfterCurrent" = true;
-
   # Whether we should draw the tabs on top of the titlebar.
   # no (0), yes (1), or default (2), which is true everywhere except Linux.
   # <https://searchfox.org/mozilla-release/rev/b6792379492929d65f5b61a3caa0a9b19bbd7078/modules/libpref/init/StaticPrefList.yaml#1565-1571>
   "browser.tabs.inTitlebar" = 0;
+  # Silence this incessant force-feeding of unwanted UI flourishes.
+  "browser.tabs.splitview.hasUsed" = true;
+  # Warn when closing *multiple* tabs.
+  "browser.tabs.warnOnClose" = true;
 
   # Communicates the toolbar color to platform (for e.g., prefers-color-scheme).
   #
@@ -70,6 +89,7 @@
   # "layout.css.prefers-color-scheme.content-override" = 2;
 
   # Enable picture-in-picture (PiP) mode.
+  "media.videocontrols.picture-in-picture.enable-when-switching-tabs.enabled" = false;
   "media.videocontrols.picture-in-picture.video-toggle.enabled" = false;
   "extensions.pictureinpicture.enable_picture_in_picture_overrides" = true;
 
@@ -86,6 +106,8 @@
   "services.sync.engine.passwords" = false;
   "services.sync.declinedEngines" = "creditcards,addresses,passwords";
 
+  "signon.autofillForms" = false;
+  "signon.generation.enabled" = false;
   "signon.rememberSignons" = false;
 
   # Required for Lepton icon support.
